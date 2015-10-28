f2b83ba2b8a73ebe109c8ffdbefd4cf3f428a1e2
Pavel Seregin <SereginPavel@yandex.ru>
2015-10-28 17:08:06 +0300

    test maven enviroment

---
+++ src/main/java/com/jet/Main.java
@@ -0,0 +1,8 @@
+package com.jet;
+import com.jet.present.*;
+
+public class Main{
+	public static void main(String[] args){
+	Printer.print("Hello, world!!!");	
+	}
+}
---
+++ src/main/java/com/jet/present/Printer.java
@@ -0,0 +1,7 @@
+package com.jet.present;
+
+public class Printer{
+	public static void print(String message){
+		System.out.println(message);
+	}
+}
