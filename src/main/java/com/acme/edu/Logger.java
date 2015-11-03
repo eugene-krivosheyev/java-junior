234e72920183e343b2230b975090d4ae13bbd9a4
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2015-11-03 14:31:20 +0300

    Remove Demo Design

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,20 +1,7 @@
 package com.acme.edu;
 
 public class Logger {
-    private LoggerFilter filter;
-    private Saver saver;
 
-    //DI
-    public Logger(LoggerFilter filter, Saver saver) {
-        this.filter = filter;
-        this.saver = saver;
-    }
-
-    public void log(int message) {
-        if (filter.filter(message)) {
-            saver.save(message);
-        }
-    }
 }
 
 
