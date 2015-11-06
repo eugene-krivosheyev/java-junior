d6818e7c31ba82ee93f6f5e9963783844aca6d03
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2015-11-06 13:13:08 +0300

    Serialization

--- src/main/java/com/acme/edu/App.java
+++ src/main/java/com/acme/edu/App.java
@@ -6,22 +6,60 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         try(
-            FileReader fr = new FileReader("temp.txt");
-            BufferedReader br = new BufferedReader(fr);
-            RandomAccessFile f = new RandomAccessFile("temp.txt", "r");
+//            ObjectOutputStream o =
+//                    new ObjectOutputStream(
+//                            new BufferedOutputStream(
+//                                new FileOutputStream("obj.ser")));
+
+            ObjectInputStream i =
+                    new ObjectInputStream(
+                            new BufferedInputStream(
+                                    new FileInputStream("obj.ser")));
         ) {
 
-            String readLine;
-            while((readLine = br.readLine()) != null) {
-                if(!"".equals(readLine)) System.out.println(">>>>" + readLine);
-            }
+//            o.writeObject(new LogMessage("test2", 15));
+//            o.writeObject(new LogMessage("test3", 1555));
+
+            LogMessage m = (LogMessage) i.readObject();
+            System.out.println(m);
+
+            LogMessage m2 = (LogMessage) i.readObject();
+            System.out.println(m2);
 
 
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
+            Class.forName("LogMessage");
         }
+    }
+}
+
+class LogMessage implements Serializable {
+    public static  int II = 7;
+   private String m;
+   private int i;
+
+    public LogMessage(String m, int i) {
+        this.m = m;
+        this.i = i;
+    }
+
+    public String getM() {
+        return m;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    @Override
+    public String toString() {
+        return "LogMessage{" +
+                "m='" + m + '\'' +
+                ", i=" + i +
+                '}';
     }
 }
