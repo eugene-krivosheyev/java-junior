540b7802a7a4bf156f51d8fcfc41468ce150a903
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-08-29 10:53:22 +0300

    Asswerts demo

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,10 +2,15 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
+        assert message != 0 : "You shall not pass!!!!";
         System.out.println("primitive: " + message);
     }
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+
+    public static void main(String[] args) {
+        Logger.log(0);
     }
 }
