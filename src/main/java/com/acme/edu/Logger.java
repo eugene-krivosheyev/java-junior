fb01e0c44bb2125f13f9e5fec1f3232ced72e1e2
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-08-22 16:32:44 +0300

    Overloading corener case Demo.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -3,41 +3,24 @@ package com.acme.edu;
 import static com.acme.edu.Logger.log;
 
 public class Logger {
-    static int globalState;
-    int instanceState;
-
-    /**
-     * Pass by value: for primitives
-     * Pass by reference: for reference types
-     */
-    public static void log(IntHolder message) { //Formal parameter
-        message.setValue(2);
-        System.out.println("primitive: " + message.getValue());
+    public static void log(String... message) {
+        for (String current : message) {
+            System.out.print(current + " ");
+        }
+        System.out.println();
+        System.out.println(message);
     }
 
-    public static void log(byte message) {
-        System.out.println("primitive: " + message);
+    public static void log(String arg) {
+
+    }
+
+    public static int log(int arg) {
+        return 0;
     }
 
     public static void main(String[] args) {
-        IntHolder intHolder = new IntHolder(1);
-        Logger.log(intHolder); //Factual parameter
-        System.out.println(intHolder.getValue());
-    }
-}
-
-class IntHolder {
-    private int value;
-
-    public IntHolder(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
+        String[] s = null;
+        log(s); //null
     }
 }
