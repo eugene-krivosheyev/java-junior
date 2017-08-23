675d9cb107b6903d3d569f6eecada5893da5f5bf
Anya <boanya5@mail.ru>
2017-08-23 13:15:36 +0300

    output optimisation

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,15 +2,18 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(Object message) {
+        String specification;
 
         if (message instanceof String) {
-            System.out.println("string: " + message);
+            specification = "string";
         } else if (message instanceof Character) {
-            System.out.println("char: " + message);
+            specification = "char";
         } else if (message instanceof Integer || message instanceof Boolean || message instanceof Byte) {
-            System.out.println("primitive: " + message);
+            specification = "primitive";
         } else {
-            System.out.println("reference: " + message);
+            specification = "reference";
         }
+
+        System.out.println(specification + ": " + message);
     }
 }
