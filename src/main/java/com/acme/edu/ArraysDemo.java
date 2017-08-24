0227a079d5cdb0d8babfa107a579b7361cc6aef4
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-08-24 12:51:09 +0300

    Arrays Demo.

---
+++ src/main/java/com/acme/edu/ArraysDemo.java
@@ -0,0 +1,25 @@
+package com.acme.edu;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysDemo {
+    public static void main(String... args) {
+        int[][] array = new int[5][];
+        for (int[] elementL1 : array) {
+            for (int elementL2 : elementL1) {
+                System.out.print(elementL2 + " ");
+            }
+            System.out.println();
+        }
+        array[0] = new int[1];
+
+        int[] array2 = new int[] {1, 3, 5}; //Array literal
+        m(new int[] {1,2,3});
+        m(1,2,3);
+    }
+
+    private static void m(int... array) {
+
+    }
+}
