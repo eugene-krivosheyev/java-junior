929e5af13eb966a70309341f406e784fb4bb0d5e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-08-25 15:06:50 +0300

    OOAD Demo: stateful design + Creator pattern.

--- src/main/java/com/acme/edu/ooad/loggerdemo/Logger.java
+++ src/main/java/com/acme/edu/ooad/loggerdemo/Logger.java
@@ -1,7 +1,7 @@
 package com.acme.edu.ooad.loggerdemo;
 
 public class Logger {
-    private LoggerSaver saver = new ConsoleSaver();
+    private LoggerSaver saver = new ConsoleSaver(); //Creator [GRASP]
 
     public void log(LoggerMessage message) {
         if (!message.filter()) { //Impl: content, severity, ...
