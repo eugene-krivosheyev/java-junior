1d7cc447ca3bdd1f0194308e12006fe2127f4655
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-08-25 15:42:20 +0300

    OOAD Demo: stateful design + DI.

--- src/main/java/com/acme/edu/ooad/loggerdemo/Logger.java
+++ src/main/java/com/acme/edu/ooad/loggerdemo/Logger.java
@@ -1,8 +1,15 @@
 package com.acme.edu.ooad.loggerdemo;
 
 public class Logger {
-    private LoggerSaver saver =
-            new ???LoggerSaverFactory().create(); //Abstract Factory [GoF]
+    private LoggerSaver saver; //field injection
+
+    public Logger(LoggerSaver saver) { //DI (constructor injection)
+        this.saver = saver;
+    }
+
+    public void setSaver(LoggerSaver saver) { //setter injection
+        this.saver = saver;
+    }
 
     public void log(LoggerMessage message) {
         if (!message.filter()) { //Impl: content, severity, ...
