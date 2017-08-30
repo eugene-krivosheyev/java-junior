c8a2c6d0752c0f256b29c444341a6d5a39551e91
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-08-30 17:40:58 +0300

    Anonymous Inner Class demo.

--- src/main/java/com/acme/edu/functionaldemo/FunctionalDemo.java
+++ src/main/java/com/acme/edu/functionaldemo/FunctionalDemo.java
@@ -2,6 +2,7 @@ package com.acme.edu.functionaldemo;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static java.util.Comparator.reverseOrder;
@@ -11,9 +12,13 @@ public class FunctionalDemo {
         int limit = 9;
         Arrays.asList(1, 6, 3, 2, 8, 2, 3, 10).stream()
             .sorted(reverseOrder())
-            .filter((Integer element) -> element < limit)
+            .filter(new Predicate<Integer>() {
+                @Override
+                public boolean test(Integer integer) {
+                    return integer < limit;
+                }
+            })
             .map(e -> e + "0")
                 .forEach(System.out::println);
-//              .reduce() //fold
     }
 }
