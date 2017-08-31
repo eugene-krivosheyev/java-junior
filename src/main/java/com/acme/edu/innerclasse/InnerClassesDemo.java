e923a7f5e827527d42e761f53cffea774b3e4231
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-08-31 10:57:44 +0300

    Inner classes demo.

--- src/main/java/com/acme/edu/innerclasse/InnerClassesDemo.java
+++ src/main/java/com/acme/edu/innerclasse/InnerClassesDemo.java
@@ -2,14 +2,26 @@ package com.acme.edu.innerclasse;
 
 public class InnerClassesDemo {
     public static void main(String[] args) {
-        new Outer.StaticInner();
+        Outer outer = new Outer();
+        outer.new Inner();
+        outer.new Inner().new InnerInnerClass();
     }
 }
 
 
 class Outer {
-    private int i;
-    protected static class StaticInner {
+    private int outerState;
+    protected class Inner {
 
+        class InnerInnerClass {
+            void m() {
+                System.out.println(outerState);
+                System.out.println(Outer.this.outerState);
+            }
+        }
+
+        void m() {
+            System.out.println(outerState);
+        }
     }
 }
