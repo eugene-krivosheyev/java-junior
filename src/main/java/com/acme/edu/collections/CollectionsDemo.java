61d691ac49df833b6f6107263bc91247bc15bd21
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-09-05 11:29:22 +0300

    Collections Demo.

--- src/main/java/com/acme/edu/collections/CollectionsDemo.java
+++ src/main/java/com/acme/edu/collections/CollectionsDemo.java
@@ -1,8 +1,11 @@
 package com.acme.edu.collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+import static java.util.Collections.sort;
+import static java.util.Comparator.comparing;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
@@ -14,9 +17,12 @@ public class CollectionsDemo {
         set.add(chineeseMurzik);
         chineeseMurzik.setName("Murzik");
 
-
-
         set.forEach(System.out::println);
+
+
+        Collection<Cat> treeSet = new TreeSet<>(comparing(Cat::getName));
+        treeSet.add(new Cat("qqq"));
+
 
     }
 }
