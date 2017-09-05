ea9ecd487edfc67d54e8f0364f66c55d19d9a2c1
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-09-05 13:17:15 +0300

    IO Demo.

--- src/main/java/com/acme/edu/io/IODemo.java
+++ src/main/java/com/acme/edu/io/IODemo.java
@@ -1,20 +1,40 @@
 package com.acme.edu.io;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 
 public class IODemo {
     public static void main(String[] args) {
         File file = new File("target", "test.txt");
 
-        try (RandomAccessFile far = new RandomAccessFile(file, "rw")) {
+        try (
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+        ) {
+
+            bw.write("тестовая строка");
+            bw.newLine();
+            bw.write("test string");
 
         } catch (IOException e) {
-            e.printStackTrace();
+
         }
 
 
+        try (
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                    new BufferedInputStream(
+                            new FileInputStream(file)), "UTF-8"))
+        ) {
+
+            String in = null;
+            while ((in = reader.readLine()) != null) {
+                System.out.println(">>>> " + in);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
