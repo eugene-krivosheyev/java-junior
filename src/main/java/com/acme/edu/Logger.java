aa754d0719da1ab645496126dc3507d20a178a1b
Timur @shako Kashapov <timshako@gmail.com>
2017-09-26 13:55:28 +0300

    rechange sources

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,21 @@
 package com.acme.edu;
 
+/** */
 public class Logger {
+
+
+    /** */
     public static void log(int message) {
         System.out.println("primitive: " + message);
     }
 
+    /** */
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+
+    /** */
+    public static void main(String[] args) {
+        System.out.println("I'am is live!");
     }
 }
