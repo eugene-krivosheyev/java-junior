0a4ac06ed5887df7f6e119774198e375718f3211
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-09-26 13:51:58 +0300

    Add demo changes for IDEA

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,10 +2,21 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println("imits " + message);
+        System.out.println("prits " + message);
+        System.out.println("prits " + message);
+        System.out.println("primits " + message);
+        System.out.println("prits " + message);
+        System.out.println("pmits " + message);
+        System.out.println("primits " + message);
+
     }
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HQQQQQQ!!!");
     }
 }
