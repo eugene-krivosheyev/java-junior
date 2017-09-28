dee9d288b2738d9d4078d1cd96ee7cc199bd61ca
TimurKashapov <timshako@gmail.com>
2017-09-28 15:07:23 +0300

    change

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,8 +1,38 @@
 package com.acme.edu;
 
+import java.util.ArrayList;
+
 /** */
 public class Logger {
 
+    /** */
+    private ArrayList<Object> logs = new ArrayList<>();
+
+    /** */
+    private int sumBytes = 0;
+
+    /** */
+    private int sumInts  = 0;
+
+    /** */
+    private int strCounter = 0;
+
+//    public static
+
+    /** */
+    public static void log(char message) {
+        System.out.println("primitive: " + message);
+    }
+
+    /** */
+    public static void log(byte message) {
+        System.out.println("primitive: " + message);
+    }
+
+    /** */
+    public static void log(short message) {
+        System.out.println("primitive: " + message);
+    }
 
     /** */
     public static void log(int message) {
@@ -10,8 +40,28 @@ public class Logger {
     }
 
     /** */
-    public static void log(byte message) {
+    public static void log(long message) {
         System.out.println("primitive: " + message);
+    }
+
+    /** */
+    public static void log(String message) {
+        System.out.println("primitive: " + message);
+    }
+
+    /** */
+    public static void log(Object message) {
+        System.out.println("primitive: " + message);
+    }
+
+    /** */
+    public static void stopInts() {
+
+    }
+
+    /** */
+    public static void flushInts() {
+
     }
 
     /** */
