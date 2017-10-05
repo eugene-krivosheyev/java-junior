e80e2e9e98d2ed15a04432c24ae673d1f48ab4b9
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-10-05 13:35:14 +0300

    Non-Static inner class demo.

--- src/main/java/com/acme/InnerClassDemo.java
+++ src/main/java/com/acme/InnerClassDemo.java
@@ -7,23 +7,24 @@ import java.util.Map;
 
 public class InnerClassDemo {
     public static void main(String[] args) {
-        Outer.StaticInner object = new Outer.StaticInner();
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
 
+        Outer.Inner i = new Outer().new Inner();
     }
 }
 
 class Outer {
-    private static int state;
+    private int state;
 
-    public static class StaticInner {
-//        private int state;
+    public class Inner {
+        private int state;
 
         public void m() {
-            state = 0;
+            Outer.this.state = 0;
         }
     }
 
     void m() {
-        new StaticInner();
     }
 }
