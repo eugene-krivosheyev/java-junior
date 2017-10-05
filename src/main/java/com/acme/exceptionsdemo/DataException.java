ec68defec8d0fac445ee35569f4ea001c3bd4484
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-10-05 18:06:55 +0300

    Custom Exceptions Demo.

--- src/main/java/com/acme/exceptionsdemo/DataException.java
+++ src/main/java/com/acme/exceptionsdemo/DataException.java
@@ -1,7 +1,12 @@
 package com.acme.exceptionsdemo;
 
-public class DataException extends RuntimeException {
+public class DataException extends Exception {
+
     public DataException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public DataException(Throwable cause) {
+        super(cause);
     }
 }
--- src/main/java/com/acme/exceptionsdemo/ExceptionsDemo.java
+++ src/main/java/com/acme/exceptionsdemo/ExceptionsDemo.java
@@ -7,17 +7,21 @@ import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void main(String[] args) {
-        try (
-            FileDao fileDao = new FileDao(null);
-            FileInputStream is = new FileInputStream("")
-        ) {
-            fileDao.getData();
-        } catch (DataException e) {
-            e.printStackTrace();
-            throw new RuntimeException("1", e);
-        } catch (RuntimeException | FileNotFoundException r) {
-            System.out.println("catch");
-        } catch (IOException e) {
+        try {
+            try (
+                FileDao fileDao = new FileDao(null);
+                FileInputStream is = new FileInputStream("")
+            ) {
+                fileDao.getData();
+            } catch (DataException e) {
+                e.printStackTrace();
+                throw new RuntimeException("1", e);
+            } catch (RuntimeException | FileNotFoundException r) {
+                System.out.println("catch");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -26,24 +30,28 @@ public class ExceptionsDemo {
 class FileDao implements Closeable {
     private String path;
 
-    FileDao(String path) {
-        throw new RuntimeException("Error while dao constructor");
+    FileDao(String path) throws Exception {
+        throw new Exception("Error while dao constructor");
 //        this.path = path;
     }
 
-    public String[] getData() {
+    public String[] getData() throws DataException {
         //....
         try {
             new FileInputStream("");
+            is.getByte();
+            if (someFlag != someState) throw new IllegalStateException();
         } catch (FileNotFoundException e) {
             throw new DataException("error while", e);
+        } catch (IOException e) {
+            throw new DataException(e);
         }
 
         return null;
     }
 
     @Override
-    public void close()  {
+    public void close() throws IOException {
 
     }
 }
