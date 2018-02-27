6e60eda6f09ee9fe60cc536f87198e663c9c3068
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-02-27 15:25:58 +0300

    Procedure style

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,44 @@
 package com.acme.edu;
 
+//kdjghdfkgjhdfkgjhdfkgj
+/*
+gkjfhkgjfhg
+лоралопралопр
+лпораплорапл
+ */
+
+import sun.rmi.runtime.Log;
+
+/**
+ * Logs messages.
+ * @author EK
+ * @param
+ * @see
+ */
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
+    public static String state = null; //Global state
+    public static final String MY_CONST = "";
+
+    static {
+        System.out.println(Logger.state);
+        Logger.log(1, 2);
+    }
+
+    public static int log(int i1, int i2) {
+        int localIntVar = 0; //local, auto, temp, stack
+        System.out.println("primitive: " + localIntVar);
+        return 0;
     }
 
     public static void log(byte message) {
+        int localIntVar = 0;
         System.out.println("primitive: " + message);
+        log(0);
     }
+
+    public static void log(int message) {
+
+    }
+
+
 }
