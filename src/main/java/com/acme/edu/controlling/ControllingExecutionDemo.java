6a2ac2ccf6da79837c101c333b309e80493a526e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-03-01 12:30:26 +0300

    Cycles

---
+++ src/main/java/com/acme/edu/controlling/ControllingExecutionDemo.java
@@ -0,0 +1,63 @@
+package com.acme.edu.controlling;
+
+public class ControllingExecutionDemo {
+    public static void мойМетодКакв1С() {
+
+    }
+
+    public static void main(String[] args) {
+        if (false) {
+            System.out.println(".");
+            System.out.println("..");
+        } else if (true) {
+
+        } else {
+
+        }
+
+        String str = null;
+        switch (str) {
+            case "1":
+                System.out.println("1");
+                break;
+            case "2":
+                System.out.println("2");
+                break;
+            case "3":
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("....");
+        }
+
+        for (int i = 0, j = 0; i != 10; i++, j--) {
+
+        }
+//        for(;;) {}
+
+        //WTF
+        outer: while (isaBoolean()) {
+            inner: do {
+//                break outer;
+                continue inner;
+            } while (true);
+        } //finding: bug, issue, WTF
+
+        for (String current : args) {
+            System.out.println(current);
+        }
+    }
+
+    private static boolean isaBoolean() {
+        return 1 == 1;
+    }
+
+    public void m(int param) {
+        //Guard Clauses
+        if (param != 0) return; //throw new IllegalArgExc
+        if (state != expected) return;
+
+        //main flow
+
+    }
+}
