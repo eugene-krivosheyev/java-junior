f5cffcffde7799f6f36043ba947a69fe2b38b0bf
GennadiyO <30900631+GennadiyO@users.noreply.github.com>
2018-03-01 13:45:01 +0300

    Iteration 2, partly 3, need to finish last Junit test for iteration 2

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -8,6 +8,8 @@ gkjfhkgjfhg
  */
 
 
+import java.util.Scanner;
+
 import static com.acme.edu.Logger.*;
 import static java.lang.Math.sin;
 
@@ -22,57 +24,132 @@ public class Logger {
     public static String state = null; //Global state
     public static final String MY_CONST = "";
     //endregion
+    public static int intCaster;
+    public static byte byteCaster;
+    public static String stringCaster = "0";
+    public static int stringCounter=1;
+    public static String preficsPr = new String("primitive: ");
+    public static String preficsCh = new String("char: ");
+    public static String preficsSt = new String("string: ");
+    public static String preficsOb = new String("reference: ");
 
     static {
-        System.out.println(Logger.state);
-        log(1, 2);
-    }
-
-    public static int log(int i1, int i2) {
-        int localIntVar = 0; //local, auto, temp, stack
-        //region output
-        System.out.println("primitive: " + localIntVar);
-        return 0;
-        //endregion
+        //System.out.println(Logger.state);
+        //log(1, 2);
     }
 
     public static void log(byte message) {
-        int localIntVar = 0;
-        System.out.println("primitive: " + message);
-        log(0);
+        if (message==Byte.MAX_VALUE){
+            print(preficsPr,byteCaster);
+            print(preficsPr,message);
+            byteCaster=0;
+        }
+        byteCaster += message;
+        if(message==0){
+            print(preficsPr,message);
+        }
     }
 
     public static void log(int message) {
-        sin(0);
+        if (message==Integer.MAX_VALUE){
+            print(preficsPr,intCaster);
+            print(preficsPr,message);
+            intCaster=0;
+        }
+        intCaster += message;
+        if(message==0){
+            print(preficsPr,message);
+        }
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        if(intCaster != 0){
+            print(preficsPr,intCaster);
+            intCaster=0;
+        }
+        if(byteCaster != 0){
+            print(preficsPr, byteCaster);
+            byteCaster = 0;
+        }
+        print(preficsCh,message);
 
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        if(intCaster != 0){
+            print(preficsPr,intCaster);
+            intCaster=0;
+        }
+        if(byteCaster != 0){
+            print(preficsPr, byteCaster);
+            byteCaster = 0;
+        }
+        print(preficsPr,message);
 
     }
 
-    public static void log(String ...message) {
-        for (String mess: message){
-            System.out.println("string: " + mess);
+    public static void log(String message) {
+        /*if(intCaster != 0){
+            print(preficsPr,intCaster);
+            intCaster=0;
+        }
+        if(byteCaster != 0){
+            print(preficsPr, byteCaster);
+            byteCaster = 0;
+        }
+
+        if(stringCaster.equals("0")){
+            stringCaster=message;
+        } else {
+            if(stringCaster.equals(message)){
+
+            }
+            String newMessage = stringCaster + stringCounter;
+            print(preficsSt,newMessage);
+            stringCaster=message;
+            stringCounter=1;
+        }*/
+        //stringCaster=message;
+        print(preficsSt,message);
+    }
+    public static void log(int[] message){
+        System.out.print("primitives array: {");
+        for(int i=0; i<message.length; i++){
+            if(i==(message.length-1)){
+                System.out.println(message[i]+"}");
+            } else {
+                System.out.print(message[i]+", ");
+            }
         }
     }
 
-    public static void log(Object ...message) {
-        for (Object mess: message){
-            System.out.println("reference: " + mess);
+    public static void log(int[][] message) {
+        
+    }
+
+    public static void log(Object message) {
+        if(intCaster != 0){
+            print(preficsPr,intCaster);
+            intCaster=0;
         }
+        if(byteCaster != 0){
+            print(preficsPr, byteCaster);
+            byteCaster = 0;
+        }
+        print(preficsOb,message);
+    }
+
+    public static void print(String prefics, Object message){
+        System.out.println(prefics+": "+message);
     }
 }
 
 class Demo {
     public static void main(String[] args) {
-        log(1,1);
+        //log(1,1);
         log(0);
         System.out.println(MY_CONST);
+        log((byte)10);
+        log((byte)20);
     }
 }
