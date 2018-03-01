a9c9c132e849523701af1de5e1301bbadaf61248
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-03-01 11:51:32 +0300

    StringBuffer, Fluent API

--- src/main/java/com/acme/edu/strings/StringsDemo.java
+++ src/main/java/com/acme/edu/strings/StringsDemo.java
@@ -10,5 +10,13 @@ public class StringsDemo {
 
         String s = s1.toUpperCase();
         System.out.println(s);
+
+        String longString = "....";
+        String x = longString + i1 + "." + "." + ".";
+        StringBuffer sbf = new StringBuffer(longString);
+        sbf.append(".") //Fluent interface | Fluent API
+            .append(".")
+            .append(".");
+
     }
 }
