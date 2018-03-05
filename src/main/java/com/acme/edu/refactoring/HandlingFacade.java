521b66e79eb45548d86ee899c3be11d57f0ba21d
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-03-05 12:51:43 +0300

    Switch pain demo

--- src/main/java/com/acme/edu/refactoring/HandlingFacade.java
+++ src/main/java/com/acme/edu/refactoring/HandlingFacade.java
@@ -1,7 +1,7 @@
 package com.acme.edu.refactoring;
 
 public class HandlingFacade {
-    private static LoggerHandler intHandler = new IntHandler();
+    private static LoggerHandler intHandler = new IntHandler(); //IS-A
     private static LoggerHandler stringHandler = new StringHandler();
 
     public static void log(int message) {
--- src/main/java/com/acme/edu/refactoring/IntHandler.java
+++ src/main/java/com/acme/edu/refactoring/IntHandler.java
@@ -1,8 +1,23 @@
 package com.acme.edu.refactoring;
 
 public class IntHandler implements LoggerHandler {
+    Command[] commands = new Command[10_000];
+
     @Override
     public void handle(Object message) {
-        (int)message
+        for (Command current : commands) {
+            current.decorate().toUpperCase();
+        }
+
+
+        if (message instanceof String) {
+            //responsibility: switch -> polym
+            ((String)message).toUpperCase();
+        } else {
+
+        }
+
+        message.execute();
+
     }
 }
