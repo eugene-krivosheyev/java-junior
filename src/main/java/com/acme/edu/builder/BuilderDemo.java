f43642a9f2fe9fdfa7edc7e4eae09d385a0f1c6b
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-03-06 15:37:29 +0300

    Builder [GoF] Demo

---
+++ src/main/java/com/acme/edu/builder/BuilderDemo.java
@@ -0,0 +1,45 @@
+package com.acme.edu.builder;
+
+public class BuilderDemo {
+    public static void main(String[] args) {
+        Employee ee = EmployeeFactory.create();
+        Employee e = EmployeeBuilder
+                .withName()
+                .withSalary()
+                .withCompany(
+                        CompanyBuilder
+                            .withName()
+                        .build()
+                )
+            .build();
+
+
+    }
+}
+
+class Employee {
+    private String name;
+    private int salary;
+    private Company company;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
