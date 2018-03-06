f1ff7703088142fb76b1b46dfbff5187ccf3b36b
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-03-06 12:41:28 +0300

    Creational DP: Factory Method [GoF]

--- src/main/java/com/acme/edu/creatation/SaverFactory.java
+++ src/main/java/com/acme/edu/creatation/SaverFactory.java
@@ -1,6 +1,11 @@
 package com.acme.edu.creatation;
 
 public class SaverFactory {
+    /**
+     * [GoF] Factory Method:
+     * Parameter FM
+     * Configurable FM
+     */
     public static Saver create() {
         return new ConsoleSaver();
     }
