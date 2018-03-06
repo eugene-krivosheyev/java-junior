a64d7ef3b7c631cbf0336d79b1addd39860fe34c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-03-06 22:30:26 +0300

    More flexibility need problem statement.

--- src/main/java/com/acme/edu/expressionproblem/LoggerFacade.java
+++ src/main/java/com/acme/edu/expressionproblem/LoggerFacade.java
@@ -1,27 +1,9 @@
 package com.acme.edu.expressionproblem;
 
-import com.acme.edu.expressionproblem.handler.IntPrefixFormatMessageHandler;
-import com.acme.edu.expressionproblem.handler.LoggerHandler;
-import com.acme.edu.expressionproblem.handler.StringPrefixFormatMessageHandler;
-import com.acme.edu.expressionproblem.message.IntMessage;
-import com.acme.edu.expressionproblem.message.StringMessage;
-import com.acme.edu.expressionproblem.saver.ConsoleSaver;
-
 public class LoggerFacade {
-    private static LoggerHandler chain = new IntPrefixFormatMessageHandler(
-        new ConsoleSaver(),
-        new StringPrefixFormatMessageHandler(
-            new ConsoleSaver(),
-            null
-        )
-    );
-
-
     public static void log(int message) {
-        chain.handleMessage(new IntMessage(message));
     }
 
     public static void log(String message) {
-        chain.handleMessage(new StringMessage(message));
     }
 }
---
+++ src/main/java/com/acme/edu/expressionproblem/package-info.java
@@ -0,0 +1,2 @@
+package com.acme.edu.expressionproblem;
+//TODO What if _Postfix_ formatters needed?
