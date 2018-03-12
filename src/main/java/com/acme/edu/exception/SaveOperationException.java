e0ccc0568e710ffb328ca673c4f9c06ccd2aca26
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-03-12 11:41:26 +0300

    Exceptions constructors.

--- src/main/java/com/acme/edu/exception/SaveOperationException.java
+++ src/main/java/com/acme/edu/exception/SaveOperationException.java
@@ -1,7 +1,23 @@
 package com.acme.edu.exception;
 
 public class SaveOperationException extends Exception {
-    public SaveOperationException(SaveOperationException e) {
-        super(e);
+    public SaveOperationException() {
+        super();
+    }
+
+    public SaveOperationException(String message) {
+        super(message);
+    }
+
+    public SaveOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SaveOperationException(Throwable cause) {
+        super(cause);
+    }
+
+    protected SaveOperationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
