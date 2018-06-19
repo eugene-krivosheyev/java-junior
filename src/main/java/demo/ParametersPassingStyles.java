ef7778805b12f774be81039a09b55a8e010a0948
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-06-19 09:48:28 +0300

    References Pass By Reference Demo. Side Effect.

--- src/main/java/demo/ParametersPassingStyles.java
+++ src/main/java/demo/ParametersPassingStyles.java
@@ -2,14 +2,27 @@ package demo;
 
 public class ParametersPassingStyles {
     public static void main(String[] args) {
-        int factualParam = 5;
-        m(factualParam); //Factual param
-        System.out.println(factualParam);
+        Data factualParam = new Data();
+        factualParam.setState(1);
+
+        m(factualParam); //if Factual param instanceof Object -> side effect
+        System.out.println(factualParam.getState());
     }
 
-    private static void m(int formalParam) { //Formal parameter
-        formalParam = 1;
+    private static void m(Data formalParam) { //Formal parameter
+        formalParam.setState(5);
     }
 }
 
+class Data {
+    private int state = 0;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
 
