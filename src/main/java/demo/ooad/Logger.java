19723e8ab538dc9be4dc5743176e63578ecda8aa
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-06-22 11:25:43 +0300

    Creational Patterns: Abstract Factory

--- src/main/java/demo/ooad/Logger.java
+++ src/main/java/demo/ooad/Logger.java
@@ -4,7 +4,8 @@ package demo.ooad;
  * Stateful VS Stateless Design
  */
 public class Logger {
-    private MessageFilter filter = MessageFilterFactory.create(); //Simple Factory OR [GoF] Factory Method
+    private MessageFilter filter
+            = MessageFilterFactory.create(); //Simple Factory -> Abstract Factory [GoF] -> Registry [PoEAA]
     private MessageSaver saver;
 
     public void log(String message) {
