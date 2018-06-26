719cf2ac1bb65de155c3fa6722c7df8061fafef1
maxim <tikhonenkov.maxim@gmail.com>
2018-06-26 09:39:32 +0300

    1st phase

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,10 +2,26 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.print("primitive: " + message + "\n");
     }
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+
+    public static void log(char message) {
+        System.out.print("char: " + message + "\n");
+    }
+
+    public static void log(String message) {
+        System.out.print("string: : " + message + "\n");
+    }
+
+    public static void log(boolean message) {
+        System.out.print("primitive: : " + message + "\n");
+    }
+
+    public static void log(Object object) {
+        System.out.print("reference: : " + object.toString() + "\n");
     }
 }
--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -52,9 +52,6 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
     }
 
-    /*
-    TODO: implement Logger solution to match specification as tests
-
     @Test
     public void shouldLogChar() throws IOException {
         //region when
@@ -68,6 +65,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         assertSysoutContains("b");
         //endregion
     }
+
 
     @Test
     public void shouldLogString() throws IOException {
@@ -108,6 +106,4 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         assertSysoutContains("@");
         //endregion
     }
-
-    */
 }
