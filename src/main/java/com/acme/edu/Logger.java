3637bc3f1aba267c1677b402bae4690e27897707
skalitko <woof.1987@gmail.com>
2018-07-04 22:27:13 +0300

    up to iter3/ test2

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,41 +2,86 @@ package com.acme.edu;
 
 public class Logger {
 
-    static int sum;
-    static {
-        sum = 0;
+    //region Integer and Byte
+    static long sumInt;
+
+
+    public static void log(int message) {
+        output("primitive: " + message);
     }
 
-    public static void log(int message) { System.out.println("primitive: " + message); }
-
-    public static void log(int message, boolean isFlush) {
-        sum += message;
+    public static void log(int message, long maxVal, boolean isFlush) {
+        if (maxVal > sumInt + message) {
+            sumInt += message;
+        } else {
+            log(maxVal);
+            sumInt = sumInt + message - maxVal;
+        }
         if (isFlush) {
-            System.out.println("primitive: " + sum);
-            sum = 0;
+            log(sumInt);
+            sumInt = 0;
         }
     }
 
-    public static void log(byte message) {
-        System.out.println("primitive: " + message);
+/*    public static void log(byte message) {
+        output("primitive: " + message);
+    } */
+
+// endregion
+
+    // region Array
+    public static void log(int[] message, boolean hint) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (hint) {
+            stringBuilder.append("primitives array: ");
+        };
+        stringBuilder.append("{");
+                for (int i = 0; i < message.length; i++) {
+                    stringBuilder.append(message[i]);
+                    if (i < (message.length - 1)) {
+                        stringBuilder.append(", ");
+                    }
+                }
+        stringBuilder.append("}");
+        output(stringBuilder.toString());
     }
 
+    public static void log(int[][] message) {
+        StringBuilder stringBuilder = new StringBuilder();
+        output("primitives matrix: {");
+        for (int element[] : message) {
+            log(element, false);
+        }
+
+        for (int i = 0; i < message.length; i++) {
+            stringBuilder.append(message[i]);
+            if (i < (message.length - 1)) {
+                stringBuilder.append(", ");
+            }
+        }
+        output("}");
+    }
+    // endregion
+
     public static void log(char message) {
-        System.out.println("char: " + message);
+        output("char: " + message);
     }
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        output("string: " + message);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        output("primitive: " + message);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        output("reference: " + message);
     }
 
+    private static void output(String decoratedMessage) {
+        System.out.println(decoratedMessage);
+    }
 
 
 }
