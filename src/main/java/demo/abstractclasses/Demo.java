7937d0697ca56f4d178a1606b7484fc1dbefea72
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-07-06 10:23:26 +0300

    Abstract Class & Polymorphism Demo.

---
+++ src/main/java/demo/abstractclasses/Demo.java
@@ -0,0 +1,60 @@
+package demo.abstractclasses;
+
+public class Demo {
+    public static void main(String[] args) {
+        //Creator Pattern
+        Animal animal = new Cat(); //IS-A: extends || implements
+        // 1Msloc: based on animal
+        animal.sound();
+
+        if (animal instanceof Cat) {
+            ((Cat)animal).mimimi();
+        } else {
+
+        }
+
+//        Cat cat = new Animal();
+        System.out.println(animal.getCount()); //
+    }
+}
+
+  class Animal {
+    /**
+     * Default behavior || abstract
+     */
+    public  void sound() {}
+    public static long getCount() {
+        return 4;
+    }
+}
+
+/**
+ * 1. Polymorphism
+ * 2. Code Reuse with Overriding
+ */
+class Cat extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("MEOW!!!!");
+    }
+
+    public void mimimi() {
+
+    }
+
+    @Override
+    public static long getCount() {
+        return 5;
+    }
+}
+
+class Alian extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("BRKSJFDKFGJDFGDFNG!!");
+    }
+
+    public void killWithExternalJaw() {
+
+    }
+}
---
+++ src/main/java/demo/interfaces/InterfaceDemo.java
@@ -0,0 +1,13 @@
+package demo.interfaces;
+
+import static java.sql.Connection.TRANSACTION_NONE;
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        System.out.println(TRANSACTION_NONE);
+    }
+}
+
+interface My {
+    public static final String reason = "";
+}
