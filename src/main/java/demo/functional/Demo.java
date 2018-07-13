f0c2d5cde0b8a3b0b3f582e9e8000661176ea3a1
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-07-13 11:10:53 +0300

    Lambdas Demo.

--- src/main/java/demo/functional/Demo.java
+++ src/main/java/demo/functional/Demo.java
@@ -6,7 +6,13 @@ public class Demo {
 //        Outer.outerInstanceState ???
         Outer outer = new Outer();
         Saver saver = outer.factoryMethod();
-        saver.save();
+        saver.save("", "");
+
+        new MyLogger(
+            (p, severity) -> outer.doThm(p)
+        );
+
+
     }
 }
 
@@ -17,17 +23,34 @@ class Outer {
     private int outerInstanceState = 0;
 
     public Saver factoryMethod() {
-        class ConsoleSaver implements Saver {
-            @Override
-            public void save() {
-                //????
-            }
-        }
+        int outerMethodLocalVar = 0;
 
-        return new ConsoleSaver();
+       return new Saver() {
+           @Override //Closure
+           public void save(String message, String severity) {
+               Outer.this.outerInstanceState = 1;
+               System.out.println(outerMethodLocalVar);
+//               outerMethodLocalVar = 5;
+           }
+       };
+    }
+
+
+    void doThm(String p) {
+
     }
 }
 
+@FunctionalInterface
 interface Saver {
-    void save();
+    //Closure
+    void save(String message, String severity);
+}
+
+class MyLogger {
+    private Saver saver;
+
+    MyLogger(Saver saver) {
+        this.saver = saver;
+    }
 }
