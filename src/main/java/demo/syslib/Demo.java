258655893e18da709026675d3b4b2472cca0bfa4
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-07-18 12:04:01 +0300

    Syslib Demo.

---
+++ src/main/java/demo/syslib/Demo.java
@@ -0,0 +1,88 @@
+package demo.syslib;
+
+import java.io.File;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.function.BiConsumer;
+
+public class Demo {
+    public static void main(String[] args) {
+//        System.exit(0);
+//        System.arraycopy();
+        System.currentTimeMillis(); System.nanoTime();
+        System.gc();
+        System.getenv();
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+        Properties properties = System.getProperties();//k=v
+//        properties.load();
+//        properties.get
+        properties.forEach((k, v) -> System.out.println(k + " : " + v));
+//        System.lineSeparator();
+//        File.pathSeparator
+//        File.separator
+        //java -Dfile.encoding=cp866 MyApp
+
+//        Runtime.getRuntime().exec("format c:");
+
+        Cat cat1 = new Cat(1, "M");
+        Cat cat2 = new Cat(2, "B");
+        cat1.equals(cat2);
+        Objects.equals(cat1,cat2);
+
+        try {
+            Object cloned = cat1.clone();
+            if (cloned instanceof Cat) {
+                Cat clonedCat = (Cat) cloned;
+                System.out.println(clonedCat.getId());
+            }
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+class Cat implements Cloneable {
+    private int id;
+    private String name;
+
+    public Cat(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        if (id != cat.id) return false;
+        return name != null ? name.equals(cat.name) : cat.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
