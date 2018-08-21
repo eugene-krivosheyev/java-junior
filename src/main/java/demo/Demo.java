f2d5662e558c348802c50a7ff61eab89ad14da53
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-21 17:32:11 +0300

    Add states demo: local, object, class.

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -6,6 +6,9 @@ package demo;
  * @since dfjgfkjhg
  */
 public class Demo {
+    String objectState = new String();
+    static String classState = new String();
+
     static {
         System.out.println("Static init block");
     }
@@ -16,6 +19,11 @@ public class Demo {
      * @see {Cla.m}
      */
     public static void main(String[] args) {
+        int localVar = 0; //stack, temp, auto
+        System.out.println(localVar);
+
+        nestedMethod();
+
         //region MyRegion
         System.out.println("HW!!");
         System.out.println("HW!!");
@@ -23,5 +31,10 @@ public class Demo {
         System.out.println("HW!!");
         System.out.println("HW!!");
         //endregion
+    }
+
+    private static void nestedMethod() {
+        int nestedLocalVar;
+        String localStringVar = new String();
     }
 }
