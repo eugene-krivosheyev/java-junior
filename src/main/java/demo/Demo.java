289a97b4105229f5513c70c83a45d5325155bc3a
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-21 16:58:49 +0300

    Add region and static block demo.

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,18 +1,27 @@
 package demo;
 
 /**
- * kdfjgdkgjhfkgjhgkjhf kdjhdfkjghdf пло
+ * JCSC
  * @author fkgjfbnjk
  * @since dfjgfkjhg
  */
 public class Demo {
+    static {
+        System.out.println("Static init block");
+    }
+
     /**
      * Logs a message.
-     * @param message kgdkfgjdh
+     * @param args kgdkfgjdh
      * @see {Cla.m}
      */
     public static void main(String[] args) {
+        //region MyRegion
         System.out.println("HW!!");
-
+        System.out.println("HW!!");
+        System.out.println("HW!!");
+        System.out.println("HW!!");
+        System.out.println("HW!!");
+        //endregion
     }
 }
