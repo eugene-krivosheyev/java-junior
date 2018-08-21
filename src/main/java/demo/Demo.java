873ae735a248cf32f6585f04356d82dea82ddd85
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-21 16:41:34 +0300

    JavaDoc demo.

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,18 @@
+package demo;
+
+/**
+ * kdfjgdkgjhfkgjhgkjhf kdjhdfkjghdf пло
+ * @author fkgjfbnjk
+ * @since dfjgfkjhg
+ */
+public class Demo {
+    /**
+     * Logs a message.
+     * @param message kgdkfgjdh
+     * @see {Cla.m}
+     */
+    public static void main(String[] args) {
+        System.out.println("HW!!");
+
+    }
+}
