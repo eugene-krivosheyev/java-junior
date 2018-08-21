61b10af057b66bf8adb68b99b070ca77d515cf06
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-21 17:43:51 +0300

    Add Pass By Value Demo.

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,48 +1,16 @@
 package demo;
 
-import static java.lang.Math.PI;
-import static java.lang.Math.abs;
-
-/**
- * JCSC
- * @author fkgjfbnjk
- * @since dfjgfkjhg
- */
 public class Demo {
-    String objectState = new String();
-    static String classState = new String();
-
-    static {
-        System.out.println("Static init block");
+    public static void main(String[] args) {
+        int factualParam = 0;
+        nestedMethod(factualParam);
+        System.out.println(factualParam);
     }
 
     /**
-     * Logs a message.
-     * @param args kgdkfgjdh
-     * @see {Cla.m}
+     * For primitives: Pass By Value
      */
-    public static void main(String[] args) {
-        int localVar = 0; //stack, temp, auto
-        System.out.println(localVar);
-
-        nestedMethod();
-
-        //region MyRegion
-        System.out.println("HW!!");
-        System.out.println("HW!!");
-        System.out.println("HW!!");
-        System.out.println("HW!!");
-        System.out.println("HW!!");
-        //endregion
-    }
-
-    private static void nestedMethod() {
-        int nestedLocalVar;
-        String localStringVar = new String();
-    }
-
-    private void staticImportDemo() {
-        abs(1);
-        System.out.println(PI);
+    private static void nestedMethod(int formalParam) {
+        formalParam = 1;
     }
 }
