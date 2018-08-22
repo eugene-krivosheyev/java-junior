b8c571e132552b42b3132060d78f651f52b1562a
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-22 15:34:07 +0300

    Add Guard Clauses Demo.

---
+++ src/main/java/controllingexecution/CyclesAndBranchesDemo.java
@@ -0,0 +1,13 @@
+package controllingexecution;
+
+public class CyclesAndBranchesDemo {
+    public static void main(String[] args) {
+        //Guard Clauses:
+        if (st1) return;
+        if (st2) throw new IllegalArgumentException();
+        if (st3) return;
+
+        //Main flow:
+
+    }
+}
