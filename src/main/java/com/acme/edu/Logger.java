b16116c811c31213d10ef799aeda661ba5e7f47d
SpringSpark <artaszx@mail.ru>
2018-08-22 12:51:22 +0400

    Refactor iteration 1

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,33 +1,37 @@
 package com.acme.edu;
 
-//TODO To think
 public class Logger {
+    private static final String PRIMITIVE = "primitive";
+    private static final String CHAR = "char";
+    private static final String STRING = "string";
+    private static final String REFERENCE = "reference";
+
     public static void log(int message) {
-        print("primitive", Integer.toString(message));
+        save(PRIMITIVE, Integer.toString(message));
     }
 
     public static void log(byte message) {
-        print("primitive", Byte.toString(message));
+        save(PRIMITIVE, Byte.toString(message));
     }
 
     public static void log(char message) {
-        print("char", Character.toString(message));
+        save(CHAR, Character.toString(message));
     }
 
     public static void log(boolean message) {
-        print("primitive", Boolean.toString(message));
+        save(PRIMITIVE, Boolean.toString(message));
     }
 
     public static void log(String message) {
-       print("string", message);
+        save(STRING, message);
     }
 
     public static void log(Object message) {
-        print("reference", message.toString());
+        save(REFERENCE, message.toString());
     }
 
-    public static void print(String type, String message) {
-        System.out.println(type +": " + message);
+    private static void save(String type, String message) {
+        System.out.println(type + ": " + message);
     }
 
 }
