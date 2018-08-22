5c155f617aa9e079b286828d40112e9c10729b5f
Timur Baev <timurbaev1996@yandex.ru>
2018-08-22 12:09:48 +0300

    iteration01

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,10 +2,23 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-         System.out.println("primitive: " + message);
+        System.out.println("primitive: " + message);
     }
-
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+    public static void log(char message) {
+        System.out.println("char: " + message);
+    }
+    public static void log(String message) {
+        System.out.println("string: " + message);
+    }
+
+    public static void log(boolean message) {
+        System.out.println("primitive: " + message);
+    }
+
+    public static void log(Object message) {
+        System.out.println("reference: " + message);
     }
 }
