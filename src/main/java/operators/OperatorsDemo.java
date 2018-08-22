ad07134f55c23290b5baf3f0bf89626c9df0821e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-22 15:02:11 +0300

    Logical operators demo.

--- src/main/java/operators/OperatorsDemo.java
+++ src/main/java/operators/OperatorsDemo.java
@@ -1,7 +1,17 @@
 package operators;
 
+import java.util.Objects;
+
 public class OperatorsDemo {
     public static strictfp void main(String[] args) {
-        System.out.println(.1 + .2) ; //IEEE 756
+        System.out.println(b1() || b2());
+    }
+
+    private static boolean b1() {
+        return false;
+    }
+
+    private static boolean b2() {
+        return false;
     }
 }
