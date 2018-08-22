beff394deeef2c90de1f0a898328239923ad7f03
ElenaRozhkova <rse.elena.rus@gmail.com>
2018-08-22 12:16:44 +0300

    Add tests passings

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,33 @@
 package com.acme.edu;
 
 public class Logger {
+    private static final String PRIMITIVE = "primitive";
+    private static final String CHAR = "char";
+    private static final String STRING = "string";
+    private static final String REFERENCE = "reference";
+    private static final String SEPARATOR = ": ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE + SEPARATOR + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE + SEPARATOR + message);
+    }
+
+    public static void log(char message) {
+        System.out.println(CHAR + SEPARATOR + message);
+    }
+
+    public static void log(String message) {
+        System.out.println(STRING + SEPARATOR + message);
+    }
+
+    public static void log(boolean message) {
+        System.out.println(PRIMITIVE + SEPARATOR + message);
+    }
+
+    public static void log(Object message) {
+        System.out.println(REFERENCE + SEPARATOR + message);
     }
 }
---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class Demo {
+        public static void main(String[] args) {
+
+        System.out.println("OSI/ISO");
+    }
+}
