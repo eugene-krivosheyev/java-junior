fe503cf50d3a5af3d9f7aa9dab85b7561a8b2285
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-22 15:09:25 +0300

    Ternary operator.

--- src/main/java/operators/OperatorsDemo.java
+++ src/main/java/operators/OperatorsDemo.java
@@ -4,14 +4,6 @@ import java.util.Objects;
 
 public class OperatorsDemo {
     public static strictfp void main(String[] args) {
-        System.out.println(b1() || b2());
-    }
-
-    private static boolean b1() {
-        return false;
-    }
-
-    private static boolean b2() {
-        return false;
+        System.out.println( 1 == 1 ? 2 : "3" );
     }
 }
