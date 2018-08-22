a46b7350d75bee3ed3dfdf5277b52671826dd9c7
Timur Baev <timurbaev1996@yandex.ru>
2018-08-22 14:05:52 +0300

    Extracted

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,23 +2,42 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        String decoratedMessage = PRIMITIVE + message;
+        save(decoratedMessage);
     }
+
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        String decoratedMessage = PRIMITIVE + message;
+        save(decoratedMessage);
     }
+
     public static void log(char message) {
-        System.out.println("char: " + message);
+        String decoratedMessage = CHAR + message;
+        save(decoratedMessage);
     }
+
     public static void log(String message) {
-        System.out.println("string: " + message);
+        String decoratedMessage = STRING + message;
+        save(decoratedMessage);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        String decoratedMessage = PRIMITIVE + message;
+        save(decoratedMessage);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        String decoratedMessage = REFERENCE + message;
+        save(decoratedMessage);
     }
+
+    private static final String PRIMITIVE = "primitive: ";
+    private static final String CHAR = "char: ";
+    private static final String STRING = "string: ";
+    private static final String REFERENCE = "reference: ";
+
+    private static void save(String decoratedMessage) {
+        System.out.println(decoratedMessage);
+    }
+
 }
