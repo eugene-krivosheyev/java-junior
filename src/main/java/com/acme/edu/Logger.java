e656a395184d5588ea7cba5b73aa70a6e183a034
Ignat Melnikov <ignatmelnikov1@gmail.com>
2018-08-22 12:05:17 +0300

    Implement iteration 1.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -8,4 +8,19 @@ public class Logger {
     public static void log(byte message) {
         System.out.println("primitive: " + message);
     }
+
+    public static void log(char message) {
+        System.out.println("char: " + message);
+    }
+
+    public static void log(String message){
+        System.out.println("string: " + message);
+    }
+
+    public static void log(boolean message) {
+        System.out.println("primitive: " + message);
+    }
+    public static void log(Object message) {
+        System.out.println("reference: " + message);
+    }
 }
