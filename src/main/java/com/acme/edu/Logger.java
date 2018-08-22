151aa17d6856133e795d999baddb499dcce4d865
SpringSpark <artaszx@mail.ru>
2018-08-22 12:05:54 +0400

    Add Course Project Iteration 01: The Rise of Logger Library

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,10 +2,31 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        print("primitive", Integer.toString(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        print("primitive", Byte.toString(message));
     }
+
+    public static void log(char message) {
+        print("char", Character.toString(message));
+    }
+
+    public static void log(boolean message) {
+        print("primitive", Boolean.toString(message));
+    }
+
+    public static void log(String message) {
+       print("string", message);
+    }
+
+    public static void log(Object message) {
+        print("reference", message.toString());
+    }
+
+    public static void print(String type, String message) {
+        System.out.println(type +": " + message);
+    }
+
 }
