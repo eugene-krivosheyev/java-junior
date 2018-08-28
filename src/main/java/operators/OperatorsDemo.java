1a914a9751a83f866c90997e53f513dd66033b00
Ignat Melnikov <ignatmelnikov1@gmail.com>
2018-08-28 16:48:08 +0300

    Delete demos code.

--- src/main/java/operators/OperatorsDemo.java
+++
@@ -1,9 +0,0 @@
-package operators;
-
-import java.util.Objects;
-
-public class OperatorsDemo {
-    public static strictfp void main(String[] args) {
-        System.out.println( 1 == 1 ? 2 : "3" );
-    }
-}
