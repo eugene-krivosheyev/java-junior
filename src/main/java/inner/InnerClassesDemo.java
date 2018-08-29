04925e0d3929491369138447c0fb46383b4c2e67
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-08-29 14:17:20 +0300

    Inner Classes Demo.

--- src/main/java/inner/InnerClassesDemo.java
+++ src/main/java/inner/InnerClassesDemo.java
@@ -2,17 +2,26 @@ package inner;
 
 public class InnerClassesDemo {
     public static void main(String[] args) {
-        Outer.Inner obj = new Outer.Inner();
-        obj.m();
+        Outer outer = new Outer();
+        Outer.Inner inner1 = outer.new Inner();
+        Outer.Inner inner2 = outer.new Inner();
+        inner1.m();
+        inner2.m();
+
+        System.out.println(outer.getI());
     }
 }
 
 class Outer {
-    private static int i = 0;
+    private int i = 0;
 
-    static class Inner {
+    public int getI() {
+        return i;
+    }
+
+    class Inner {
         void m() {
-            i = 1;
+            Outer.this.i = 1;
         }
     }
 }
