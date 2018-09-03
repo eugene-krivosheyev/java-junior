3f425ad12c7c394299762f31d5e73b6818dc771c
SpringSpark <artaszx@mail.ru>
2018-09-03 13:03:08 +0400

    Add lambdas to IntMessage accumulator.

--- src/main/java/com/acme/edu/message/IntMessage.java
+++ src/main/java/com/acme/edu/message/IntMessage.java
@@ -3,12 +3,17 @@ package com.acme.edu.message;
 import com.acme.edu.loggerexceptions.LoggerDecoratorException;
 import com.acme.edu.message.decorator.Decorator;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class IntMessage extends Message {
     private int message;
+    private ArrayList<IntMessage> integerArrayList;
 
     public IntMessage(int message, Decorator decorator) {
         super (decorator);
         this.message = message;
+        this.integerArrayList = new ArrayList<IntMessage>();
     }
 
     public int getMessage() {
@@ -18,12 +23,14 @@ public class IntMessage extends Message {
     @Override
     public Message accumulate(Message nextMessage) {
         IntMessage downcastedMessage = (IntMessage) nextMessage;
-        return new IntMessage(this.message + downcastedMessage.message, this.getDecorator());
+        integerArrayList.add(downcastedMessage);
+        return this;
     }
 
     @Override
     public String getDecoratedMessage() throws LoggerDecoratorException {
-        getDecorator().setMessage(Integer.toString(message));
+        Optional<Integer> result = integerArrayList.stream().map(e ->e.getMessage()).reduce((a, b) -> a + b);
+        getDecorator().setMessage(result.toString());
         return getDecorator().getDecoratedMessage();
     }
 
