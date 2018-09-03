f08644a77ce5bfac5548bf49b154b13da0121358
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-03 15:47:29 +0300

    Generics Demo.

--- src/main/java/collectionsdemo/CollectionsDemo.java
+++ src/main/java/collectionsdemo/CollectionsDemo.java
@@ -1,27 +1,80 @@
 package collectionsdemo;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.function.BinaryOperator;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class CollectionsDemo {
     public static void main(String[] args) {
-        Collection<Integer> list = Arrays.asList(1,6,2,5,3,4);
-        list.parallelStream()
-            .filter(e -> e >= 3)
-            .map(CollectionsDemo::dec)
-            .sorted(Comparator.reverseOrder())
-            .skip(1)
-            .limit(2)
-                .forEach(System.out::println);
-//            .reduce((e1, e2) -> e1 + e2)
-//                .ifPresent(System.out::println);
+        Maybe<String> boxedValue =
+                Maybe.<String>empty();
+        boxedValue.get();
+        /*
+        if (unboxedValue instanceof Integer) {
+            (Integer)unboxedValue;
+        }
+        */
 
+        ArrayList<? extends Object> cats = new ArrayList<String>();
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        Collections.sort(numbers, (o1, o2) -> 0);
     }
 
-    private static int dec(int param) {
-        return param - 1;
+}
+
+class Maybe<T> {
+    private T maybeObject;
+
+    public Maybe(T toWrap) {
+        this.maybeObject = toWrap;
+    }
+
+    public static <U> Maybe<U> of(U toWrap) {
+        return new Maybe<>(toWrap);
+    }
+
+    public static <V> Maybe<V> empty() {
+        return new Maybe<>(null);
+    }
+
+    public T get() {
+        return maybeObject;
+    }
+
+    public boolean isPresent() {
+        return maybeObject != null;
+    }
+
+    public void ifPresent(Consumer todo) {
+        if (maybeObject != null) {
+            todo.accept(maybeObject);
+        }
+    }
+}
+
+interface MyComparator<T> {
+    void compareTo(T t1, T t2);
+}
+
+class MyComporatorImpl1<T extends String> implements MyComparator<T> {
+    @Override
+    public void compareTo(T t1, T t2) {
+
+    }
+}
+
+/*
+class MyComporatorImpl2<String> implements MyComparator<T> {
+    @Override
+    public void compareTo(T t1, T t2) {
+
+    }
+}
+*/
+
+class MyComporatorImpl3 implements MyComparator<String> {
+    @Override
+    public void compareTo(String t1, String t2) {
+
     }
 }
