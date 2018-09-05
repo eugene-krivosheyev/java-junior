918b651dd634da3f01bc749a7638c5283bd3138c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-05 12:46:56 +0300

    Thread API.

--- src/main/java/paralldemo/ParallelDemo.java
+++ src/main/java/paralldemo/ParallelDemo.java
@@ -1,16 +1,36 @@
 package paralldemo;
 
-public class ParallelDemo {
-    public static void main(String[] args) {
-        Thread controller = new Thread(() -> {
-            while (true) {
-                System.out.println("thread");
-            }
-        });
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.interrupted;
+import static java.lang.Thread.sleep;
 
+public class ParallelDemo {
+    public static void main(String[] args) throws InterruptedException {
+       final Thread controlled = new Thread(() -> {
+           while (!interrupted()) {
+               try {
+    //               this != currentThread()
+                    System.out.println(currentThread().getName());
+                   sleep(100);
+               } catch (InterruptedException e) {
+
+               }
+           }
+       });
+       controlled.setDaemon(true);
+       controlled.start();
+       controlled.stop(); controlled.suspend(); controlled.resume();
+       controlled.getName();
+       controlled.setPriority(Thread.MAX_PRIORITY); controlled.getPriority();
+
+        controlled.join(10_000);
+
+        controlled.interrupt();
         while (true) {
-            System.out.println("main");
+            sleep(100);
+            System.out.println(Thread.currentThread().getName());
         }
+
     }
 }
 
