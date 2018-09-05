c5a07305c7cefad5380788f1b1a78c16dfff8a6b
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-05 16:44:26 +0300

    Data Race win Demo.

---
+++ src/main/java/concurrencydemo/ConcurrencyDemo.java
@@ -0,0 +1,62 @@
+package concurrencydemo;
+
+import java.util.Collection;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ConcurrencyDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter.increment();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter.increment();
+            }
+        });
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        System.out.println(counter.getValue());
+    }
+}
+
+class Counter {
+    private int value;
+    private Collection history; //Collections.synchronizedCollection(?)
+
+    /**
+     * Amdal Law: P = 1/%sync
+     * -fairness-
+     */
+    public void increment() {
+        synchronized(this) {
+            value++;
+            history.add();
+        }
+    }
+
+    public void decrement() {
+//        Lock lock = new ReentrantLock();
+        ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+//        lock.tryLock();
+        lock.readLock().lock();
+        lock.writeLock().lock();
+        try {
+            value--;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
