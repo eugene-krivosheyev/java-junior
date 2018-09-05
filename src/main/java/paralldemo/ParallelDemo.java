ee62d6f20224e0aa7a6622f9989fab200b354848
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-05 11:17:57 +0300

    1st way to create Thread.

--- src/main/java/paralldemo/ParallelDemo.java
+++ src/main/java/paralldemo/ParallelDemo.java
@@ -1,4 +1,20 @@
 package paralldemo;
 
 public class ParallelDemo {
+    public static void main(String[] args) {
+        MyThread controller = new MyThread();
+
+        while (true) {
+            System.out.println("main");
+        }
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("thread");
+        }
+    }
 }
