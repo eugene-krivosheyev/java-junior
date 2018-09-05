e4a58b85f6a635b21c050be224dbe2f42cc6ef11
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-09-05 11:23:15 +0300

    4th way to create Thread.

--- src/main/java/paralldemo/ParallelDemo.java
+++ src/main/java/paralldemo/ParallelDemo.java
@@ -2,18 +2,15 @@ package paralldemo;
 
 public class ParallelDemo {
     public static void main(String[] args) {
-        Thread controller = new Thread(new Worker());
+        Thread controller = new Thread(() -> {
+            while (true) {
+                System.out.println("thread");
+            }
+        });
 
         while (true) {
             System.out.println("main");
         }
-    }
-}
-
-class Worker implements Runnable {
-    @Override
-    public void run() {
-
     }
 }
 
