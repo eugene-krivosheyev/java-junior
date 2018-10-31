71713610119aa77891706abb2e72b376dd19efa2
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-10-31 14:41:05 +0300

    Add comments, javadoc and contract demo.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,16 +1,58 @@
 package com.acme.edu;
 
+import java.io.IOException;
+
+/**
+ * Text
+ * <strong>flgjhdfkgjdhfgkjh</strong>
+ * @see String#toString
+ * @author
+ * @since 16.18
+ */
 public class Logger {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        printToConsole("primitive: " + message);
     }
 
-    //TODO dflkjdfkgjhdfkgjdhgkjdfhgkdfjhg
-    public static void log(byte message) {
-        System.out.println("primitive: " + message);
+    /**
+     * 1. Contract:
+     * Pre-conditions: param values, state
+     * Post-conditions: side effect
+     * Invariants: business rules
+     *
+     * 2. Architecture comments & whys?
+     * - JDBC -> ORM, faster
+     */
+    public static int findMinimalElementInCollection(byte message) throws IOException {
+        //region checks
+        boolean a;
+        boolean b;
+        boolean c;
+        boolean d;
+        boolean isUserLoggedIn = a != b;
+        //c == d -> is user registered?
+        if (isUserLoggedIn && c == d) {
+
+        }
+        //endregion
+
+        /*
+        lfgkjdflkgjdlg
+        lfkgjdlfgkjdlfkgj
+        lfgkjdflgkjdflgk
+         */
+        printToConsole("primitive: " + message);
+        printToConsole("primitive: " + message);
+        printToConsole("primitive: " + message);
     }
 
     public static void main(String[] args) {
-        System.out.println("HW!!!");
+        printToConsole("HW!!!");
+    }
+
+
+
+    private static void printToConsole(String s) {
+        System.out.println(s);
     }
 }
