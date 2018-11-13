ec774b3729219424afe92b3bb01a424009585e7e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-11-13 12:55:37 +0300

    Add string concat optimization demo.

--- src/main/java/demo/StringDemo.java
+++ src/main/java/demo/StringDemo.java
@@ -7,6 +7,9 @@ public class StringDemo {
         String s1 = "abc"; //new() + intern()
         String s2 = "abc"; // s2 -> s1
 
+        String longString = "aaaaaaaa";
+        String internedString = longString.intern();
+
         System.out.println(s1.toUpperCase()); //StringBuffer, StringBuilder
 
         System.out.println(s1.equals(s2));
@@ -14,5 +17,17 @@ public class StringDemo {
 
         System.out.println(s1.equals("abc"));
         System.out.println("abc".equals(s1));
+
+        System.out.println(1 + 2 + "3" + 4 + 5); //3345
+
+        System.out.println("a" + "b" + "c" + "d" + "e");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder //Fluent API | interface
+            .append("a")
+            .append("b")
+            .append("c")
+            .append("d")
+            .append("e");
+        stringBuilder.toString();
     }
 }
