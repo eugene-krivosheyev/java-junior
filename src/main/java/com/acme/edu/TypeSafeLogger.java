16c96f0f687ca087f9b7fcc0a65be405a95feeb6
light <eshev4enko@gmail.com>
2018-11-23 00:58:12 +0300

    logger with state

--- src/main/java/com/acme/edu/TypeSafeLogger.java
+++ src/main/java/com/acme/edu/TypeSafeLogger.java
@@ -3,9 +3,16 @@ package com.acme.edu;
 
 public class TypeSafeLogger {
     private static final String PRIMITIVE_PREFIX = "primitive: ";
-    public static final String CHAR_PREFIX = "char: ";
-    public static final String REFERENCE_PREFIX = "reference: ";
-    public static final String STRING_PREFIX = "string: ";
+    private static final String CHAR_PREFIX = "char: ";
+    private static final String REFERENCE_PREFIX = "reference: ";
+    private static final String STRING_PREFIX = "string: ";
+
+    private static final String INT_TYPE = "int";
+    private static final String BYTE_TYPE = "byte";
+    private static final String CHAR_TYPE = "char";
+    private static final String BOOLEAN_TYPE = "boolean";
+    private static final String REFERENCE_TYPE = "reference";
+    private static final String STRING_TYPE = "string";
 
 
     private static String currentString = "";
@@ -22,93 +29,137 @@ public class TypeSafeLogger {
     /**
      * OCP
      */
+
     public static void log(int message) {
-        if (lastType != "int" && lastType != "") {
-            flush();
-            currentInt = message;
-            decoratedMessage = PRIMITIVE_PREFIX + message;
-        } else if (message == Integer.MAX_VALUE) {
-            flush();
-            decoratedMessage = "Integer.MAX_VALUE";
-        }
-        else {
-            currentInt = currentInt + message;
-            decoratedMessage = PRIMITIVE_PREFIX + currentInt;
-        }
-        lastType = "int";
+        flushIfTypeChanged(INT_TYPE);
+        flushIfOverflow(message);
+        setCurrentState(message);
+        updateDecoratedMessage();
     }
 
     public static void log(byte message) {
-        if (lastType != "byte" && lastType != "") {
-            flush();
-            currentByte = message;
-            decoratedMessage = PRIMITIVE_PREFIX + message;
-        } else if (message == Byte.MAX_VALUE) {
-            flush();
-            decoratedMessage = "Byte.MAX_VALUE";
-        }
-        else {
-            currentByte = (byte) (currentByte + message);
-            decoratedMessage = PRIMITIVE_PREFIX + currentByte;
-        }
-        lastType = "byte";
-    }
-
-    private static void updateDecoratedMessage(String prefix, String message) {
-        decoratedMessage = prefix + message;
+        flushIfTypeChanged(BYTE_TYPE);
+        flushIfOverflow(message);
+        setCurrentState(message);
+        updateDecoratedMessage();
     }
 
     public static void log(char message) {
-        if (lastType != "char" && lastType != "") {
-            flush();
-            currentChar = message;
-        } else {
-            currentChar = message;
-        }
-        updateDecoratedMessage(CHAR_PREFIX, Character.toString(currentChar));
-        lastType = "char";
+        flushIfTypeChanged(CHAR_TYPE);
+        setCurrentState(message);
+        updateDecoratedMessage();
     }
 
     public static void log(boolean message) {
-        if (lastType != "boolean" && lastType != "") {
-            flush();
-            currentBoolean = message;
-        } else {
-            currentBoolean = message;
-        }
-        updateDecoratedMessage(PRIMITIVE_PREFIX, Boolean.toString(currentBoolean));
-        lastType = "boolean";
+        flushIfTypeChanged(BOOLEAN_TYPE);
+        setCurrentState(message);
+        updateDecoratedMessage();
     }
 
     public static void log(String message) {
-        if (lastType != "string" && lastType != "") {
-            flush();
-            currentString = message;
-        } else {
-            if (currentString.equals(message)) {
-                currentStringCount++;
-            } else {
-                flush();
-                currentString = message;
-            }
-        }
-        if (currentStringCount > 0) {
-            decoratedMessage = STRING_PREFIX + currentString + " (x" + (currentStringCount + 1) + ")";
-        } else {
-            decoratedMessage = STRING_PREFIX + currentString;
-        }
-        lastType = "string";
+        flushIfTypeChanged(STRING_TYPE);
+        flushIfStringChanged(message);
+        setCurrentState(message);
+        updateDecoratedMessage();
     }
 
     public static void log(Object message) {
-        if (lastType != "reference" && lastType != "") {
+        flushIfTypeChanged(REFERENCE_TYPE);
+        setCurrentState(message);
+        updateDecoratedMessage();
+    }
+
+    private static void flushIfOverflow(int message) {
+        if (message == Integer.MAX_VALUE) {
             flush();
-            currentReference = message;
-        } else {
-            currentReference = message;
         }
-        updateDecoratedMessage(REFERENCE_PREFIX, currentReference.toString());
-        lastType = "reference";
+    }
+
+    private static void flushIfOverflow(byte message) {
+        if (message == Byte.MAX_VALUE) {
+            flush();
+        }
+    }
+
+    private static void flushIfStringChanged(String message) {
+        if (!currentString.equals(message)) {
+            flush();
+        }
+    }
+
+    private static void updateDecoratedMessage() {
+        switch (lastType) {
+            case INT_TYPE:
+                decoratedMessage = PRIMITIVE_PREFIX + (currentInt == Integer.MAX_VALUE ? "Integer.MAX_VALUE" : Integer.toString(currentInt));
+                break;
+            case BYTE_TYPE:
+                decoratedMessage = PRIMITIVE_PREFIX + (currentByte == Byte.MAX_VALUE ? "Byte.MAX_VALUE" : Byte.toString(currentByte));
+                break;
+            case CHAR_TYPE:
+                decoratedMessage = CHAR_PREFIX + Character.toString(currentChar);
+                break;
+            case BOOLEAN_TYPE:
+                decoratedMessage = PRIMITIVE_PREFIX + currentBoolean;
+                break;
+            case STRING_TYPE:
+                if (currentStringCount > 0) {
+                    decoratedMessage = STRING_PREFIX + currentString + " (x" + (currentStringCount + 1) + ")";
+                } else {
+                    decoratedMessage = STRING_PREFIX + currentString;
+                }
+                break;
+            case REFERENCE_TYPE:
+                decoratedMessage = REFERENCE_PREFIX + currentReference;
+        }
+    }
+
+    private static void setCurrentState(int message) {
+        if (lastType != INT_TYPE) {
+            currentInt = message;
+            lastType = INT_TYPE;
+        } else {
+            currentInt = (message == Integer.MAX_VALUE ? Integer.MAX_VALUE : currentInt + message);
+        }
+    }
+
+    private static void setCurrentState(byte message) {
+        if (lastType != BYTE_TYPE) {
+            currentByte = message;
+            lastType = BYTE_TYPE;
+        } else {
+            currentByte = (message == Byte.MAX_VALUE ? Byte.MAX_VALUE : (byte)(currentByte + message));
+        }
+    }
+
+    private static void setCurrentState(char message) {
+        currentChar = message;
+        lastType = CHAR_TYPE;
+    }
+
+    private static void setCurrentState(boolean message) {
+        currentBoolean = message;
+        lastType = BOOLEAN_TYPE;
+    }
+
+    private static void setCurrentState(Object message) {
+        currentReference = message;
+        lastType = REFERENCE_TYPE;
+    }
+
+    private static void flushIfTypeChanged(String newType) {
+        if (lastType != newType && lastType != "") {
+            flush();
+        }
+    }
+
+    private static void setCurrentState(String message) {
+        if (lastType != STRING_TYPE) {
+            currentString = message;
+            lastType = STRING_TYPE;
+            currentStringCount = 0;
+        } else if (currentString.equals(message)) {
+            currentStringCount++;
+        }
     }
 
     public static void flush(){
@@ -124,7 +175,7 @@ public class TypeSafeLogger {
         currentStringCount = 0;
         currentReference = null;
         currentBoolean = false;
-
+        lastType = "";
     }
     /**
      * 1. Reuse
