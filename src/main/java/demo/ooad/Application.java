ff79b587fee71e5199a89e68f4409050bc796548
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-12-04 13:06:31 +0300

    Visibility modifiers and [GoF] Template Method Demo.

--- src/main/java/demo/ooad/Application.java
+++ src/main/java/demo/ooad/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         //DI Frameworks: Spring, Google Juice
         Logger logger = new Logger(
-            new MessageLengthLogFilter(10)
+            new MessageLogFilter(10)
         );
     }
 }
---
+++ src/main/java/demo/ooad/MessageLogFilter.java
@@ -0,0 +1,90 @@
+package demo.ooad;
+
+//Default || package-friendly:
+//[GoF] Template Method
+abstract class MessageLogFilter extends LoggerFilter {
+    @Override
+    //REUSE!!!!!
+    public final boolean filter(String message, int severity) {
+        if (message != null) {
+            return checkMessageLength(message);
+        } else {
+            return false;
+        }
+    }
+
+    protected abstract boolean checkMessageLength(String message);
+}
+
+class MessageLengthLogFilter extends MessageLogFilter {
+    private int maxLength;
+
+    MessageLengthLogFilter(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    private boolean checkMessageLength(String message) {
+        return message.length() > maxLength;
+    }
+}
+
+class MyDate {
+    //Data hiding
+    private int year; //Constraints
+    private int mo; //Constr
+    private int day; //Con
+
+    public int getYear() {
+        return year;
+    }
+
+    //Property: set + get
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMo() {
+        return mo;
+    }
+
+    public void setMo(int mo) {
+        this.mo = mo;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+}
+
+class Woman {
+    private int age;
+
+    public int getAge() {
+        return 18;
+    }
+}
+
+class Man {
+    private int beardSize;
+
+    public int getBeardSize() {
+        return beardSize*2;
+    }
+}
+
+//OCP: incapsulate early
+class Cat {
+    public String name;
+
+
+}
+
+class SuperCat extends Cat {
+    public String name;
+}
+
