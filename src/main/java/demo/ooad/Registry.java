d87fddaa64ad2a9b165143f2b74f352335fe57d9
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-12-04 09:58:17 +0300

    OOAD: Add registry behavior.

--- src/main/java/demo/ooad/Registry.java
+++ src/main/java/demo/ooad/Registry.java
@@ -2,6 +2,6 @@ package demo.ooad;
 
 public class Registry {
     public static SaverFactory createLogSaverFactory() {
-        return null;
+        return new XmlConfigLogSaverFactory();
     }
 }
