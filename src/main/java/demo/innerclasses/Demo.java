b9539f779fc8fa6161123d887bc333928402a1e0
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-12-12 12:27:53 +0300

    Named Nested class demo.

--- src/main/java/demo/innerclasses/Demo.java
+++ src/main/java/demo/innerclasses/Demo.java
@@ -1,20 +1,38 @@
 package demo.innerclasses;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 public class Demo {
     public static void main(String[] args) {
         Outer outer = new Outer();
         Outer.Inner obj1 = outer.new Inner();
     }
+
+
 }
 
 class Outer {
     private int instanceState = 0;
     private static int classState;
 
-    private class Inner {
-        private int instanceState = 0;
-        void im() {
-            System.out.println(Outer.this.instanceState);
+    Externalizable om(Externalizable p) {
+        class Inner implements Externalizable {
+            @Override
+            public void writeExternal(ObjectOutput out) throws IOException {
+                //....
+            }
+
+            @Override
+            public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+            }
+
+            public void m() {}
         }
+
+        return new Inner();
     }
 }
