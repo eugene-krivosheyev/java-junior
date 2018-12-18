9978e5679f25efd2c88d86860fb968ebe633438c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-12-18 12:33:58 +0300

    Add exception cases.

--- src/main/java/demo/exceptionsdemo/ExceptionsDemo.java
+++ src/main/java/demo/exceptionsdemo/ExceptionsDemo.java
@@ -1,36 +1,25 @@
 package demo.exceptionsdemo;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class ExceptionsDemo {
-    public static void main(String[] args) throws SQLException {
-        Connection c = null;
-        RuntimeException ble = null;
+    public static void main(String[] args) {
         try {
-            c = null;
-            bl(); // e ->
-        } catch (IllegalArgumentException | ArithmeticException e) {
-            //1. log
-            //2. отказ пользователю
-            ble = new RuntimeException(e);
-            throw ble;
-        } finally {
-            NullPointerException npe = new NullPointerException();
-            if (ble != null) npe.addSuppressed(ble);
-            throw npe;
+            bl();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static void bl() {
+    private static void bl() throws Exception {
         dal();
         // nop
     }
 
-    private static void dal() {
+    private static void dal() throws Exception {
         boolean isOpened = false;
         if (!isOpened) {
-            throw new ArithmeticException("Can't open file!!!");
+            throw new Exception("file.error");
         }
         //
     }
