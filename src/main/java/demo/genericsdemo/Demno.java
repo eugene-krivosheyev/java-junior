ebafcaa6565356dfcf9034c501f858a34ef09153
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-12-21 11:30:07 +0300

    Generics demo.

--- src/main/java/demo/genericsdemo/Demno.java
+++ src/main/java/demo/genericsdemo/Demno.java
@@ -1,5 +1,8 @@
 package demo.genericsdemo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static demo.genericsdemo.MySuperComparator.compareTo;
 
 public class Demno {
@@ -7,10 +10,43 @@ public class Demno {
         String s = compareTo("", "");
         Integer m = MySuperComparator.<Integer>m();
 
-        MySuperComparator<Object> comp
-                = new MySuperComparator<String>();
+//        MySuperComparator<Object> comp =
+//                new MySuperComparator<String>();
+//
+        ArrayList<String> c = new ArrayList<>();
+
     }
 }
+
+interface SuperPuperComparator<T> {
+    int compareTo(T o1, T o2);
+}
+
+class SPC1 implements SuperPuperComparator<String> {
+    @Override
+    public int compareTo(String o1, String o2) {
+        return 0;
+    }
+}
+
+class SPC2<T extends String> implements SuperPuperComparator<T> {
+    @Override
+    public int compareTo(T o1, T o2) {
+        o1.toUpperCase();
+        Collections.sort(new ArrayList<>());
+        return 0;
+    }
+}
+
+class SPC3 implements SuperPuperComparator {
+    @Override
+    public int compareTo(Object o1, Object o2) {
+        return 0;
+    }
+}
+
+
+
 
 class MySuperComparator<V> {
     private V field;
