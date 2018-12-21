d35f5b20c0ef2e7274c25300ecbf46691286a8d7
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2018-12-21 13:15:03 +0300

    Annotations demo.

--- src/main/java/demo/syslib/SysLibDemo.java
+++ src/main/java/demo/syslib/SysLibDemo.java
@@ -3,7 +3,7 @@ package demo.syslib;
 import java.util.Properties;
 
 public class SysLibDemo {
-    public static void main(String[] args) throws CloneNotSupportedException {
+    public static void main(String[] args) throws CloneNotSupportedException, ClassNotFoundException {
         //region system & runtime
         Properties properties = System.getProperties();
         for (String pn: properties.stringPropertyNames()) {
@@ -23,11 +23,18 @@ public class SysLibDemo {
         Cat clone = (Cat)cat.clone();
         System.out.println(clone.getName());
 
+        Class.forName("Cat");
+        cat.getClass().getAnnotations();
+        //ByteBuddy | CodeAssist | ASM
+
         System.exit(-1);
         //endregion
     }
 }
 
+
+@MyAnnotation()
+@SuppressWarnings({"unchecked"})
 class Cat implements Cloneable {
     private Cat friend;
     private String name;
@@ -36,12 +43,34 @@ class Cat implements Cloneable {
         this.name = name;
     }
 
+    @Retry(kjghdkfgjh)
     public String getName() {
         return name;
     }
 
+    @Transaction
+    @RolewAllowes
     @Override
+    @Deprecated
     public Object clone() throws CloneNotSupportedException {
         return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        if (friend != null ? !friend.equals(cat.friend) : cat.friend != null) return false;
+        return name != null ? name.equals(cat.name) : cat.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = friend != null ? friend.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
