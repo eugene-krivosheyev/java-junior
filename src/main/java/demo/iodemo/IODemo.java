e30edc6443b08da74bf492b4d10fd8517aff80e7
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-01-15 13:03:57 +0300

    Sync I/O demo.

---
+++ src/main/java/demo/iodemo/IODemo.java
@@ -0,0 +1,49 @@
+package demo.iodemo;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class IODemo {
+    public static void main(String[] args) {
+        File file = new File("test.txt");//Path
+
+        try (PrintWriter out =
+                    new PrintWriter(
+                            new FileWriter(file, true))) {
+
+            out.println("тест 1");
+            out.println("тест 2");
+            out.println("тест 3");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //-----
+
+        try (BufferedReader in =
+                     new BufferedReader(new InputStreamReader(
+                        new BufferedInputStream(
+                            new FileInputStream(file), 8000),
+                             "UTF-8"))) {
+
+            String readLine = null;
+            while((readLine = in.readLine()) != null) {
+                System.out.println(">>> " + readLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            Files.lines(Paths.get("test.txt"))
+                    .forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
