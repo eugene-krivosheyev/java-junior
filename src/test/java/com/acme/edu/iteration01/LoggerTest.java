ec91ca5aa09f78d5bb01db7887d0e4976e25acb6
Maria <tereshinams@rambler.ru>
2019-07-11 22:05:47 +0300

    Refactor:ExtractVariable lineSeparator

--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -32,7 +32,8 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
 
         //region then
         assertSysoutContains("primitive: ");
-        assertSysoutEquals("primitive: 1" + System.lineSeparator() + "primitive: 0" + System.lineSeparator() + "primitive: -1" + System.lineSeparator());
+        String newLn = System.lineSeparator();
+        assertSysoutEquals("primitive: 1" + newLn + "primitive: 0" + newLn + "primitive: -1" + newLn);
         //endregion
     }
 
