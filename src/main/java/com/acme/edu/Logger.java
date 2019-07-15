698a06207ec4de28f70cce5d676f1004c1e9157f
kirillabaskin <kirillabaskin99@gmail.com>
2019-07-15 14:18:54 +0300

    Make second iteration

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,35 +1,33 @@
 package com.acme.edu;
 
-import com.sun.istack.internal.NotNull;
-
 public class Logger {
-
-    private static void log(String type,String message){
-        System.out.println(type+": "+ message);
+    private static void mainLog(String type, String message){
+        System.out.println(type + ": " + message);
     }
 
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        mainLog("primitive", String.valueOf(message));
     }
 
     public static void log(char message) {
-        log("char",message+"");
+        mainLog("char", String.valueOf(message));
     }
 
     public static void log(String message) {
-        log("string",String.valueOf(message));
+        mainLog("string", String.valueOf(message));
     }
 
     public static void log(boolean message) {
-        log("primitive",String.valueOf(message));
+        mainLog("primitive", String.valueOf(message));
     }
 
     public static void log(Object message) {
-        if(message != null)
-            log("reference",message.toString());
+        if(message != null) {
+            mainLog("reference", message.toString());
+        }
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        mainLog("primitive",String.valueOf(message));
     }
 }
