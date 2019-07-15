42a796d4f5f3b4bebeed4a983f91030e31dd5aa9
moroele <lenok-tver@yandex.ru>
2019-07-15 14:28:20 +0300

    Logger it1

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,5 +1,7 @@
 package com.acme.edu;
 
+import javax.lang.model.type.PrimitiveType;
+
 public class Logger {
     public static void log(int message) {
         System.out.println("primitive: " + message);
@@ -7,5 +9,28 @@ public class Logger {
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+
+    public static void log(String message) {
+        System.out.println("string: " + message);
+    }
+
+    public static void log(boolean message) {
+        System.out.println("primitive: " + message);
+    }
+
+    // public static void log(char message) {
+    //    System.out.println("char: " + message);
+
+    // }
+    public static void log(Object message) {
+        //System.out.println(message.getClass().getName() + ": " + message);
+        if (message == null) {
+            System.out.println("reference: null");
+            return;
+        }
+        System.out.println("reference: " + message);
+
+
     }
 }
--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -52,8 +52,6 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
     }
 
-    /*
-    TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogChar() throws IOException {
@@ -68,6 +66,8 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         assertSysoutContains("b");
         //endregion
     }
+/*
+    TODO: implement Logger solution to match specification as tests*/
 
     @Test
     public void shouldLogString() throws IOException {
@@ -108,6 +108,4 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         assertSysoutContains("@");
         //endregion
     }
-
-    */
 }
