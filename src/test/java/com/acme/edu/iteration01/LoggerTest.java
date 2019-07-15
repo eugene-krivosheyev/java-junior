bd94c8cf8d0fb1c200bf2fbed6c1b2e3dc4d2a27
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-07-15 18:27:30 +0300

    Fix tests.

--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -53,8 +53,6 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     }
 
     /*
-    TODO: implement Logger solution to match specification as tests
-
     @Test
     public void shouldLogChar() throws IOException {
         //region when
