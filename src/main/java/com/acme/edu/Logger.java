d25d3e11a08d9643ee5d488dcef03389243daf70
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-07-15 18:24:34 +0300

    Add demo for global named constant.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,8 +1,21 @@
 package com.acme.edu;
 
 public class Logger {
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        if (message == 0) return; //Guard clause
+
+        System.out.println(PRIMITIVE_PREFIX + message);
+
+
+        //========
+
+        if (message != 0) {
+            System.out.println("primitive: " + message);
+        } else {
+            return;
+        }
     }
 
     public static void log(byte message) {
