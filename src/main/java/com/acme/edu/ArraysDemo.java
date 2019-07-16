cc5c01d4b7d466fc61a04c568842294053e9bb5d
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-07-16 15:18:34 +0300

    Add vararg demo.

--- src/main/java/com/acme/edu/ArraysDemo.java
+++ src/main/java/com/acme/edu/ArraysDemo.java
@@ -1,16 +1,17 @@
 package com.acme.edu;
 
 public class ArraysDemo {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         String[][] array = new String[10][1];
         array[0] = new String[0];
         array[1] = new String[3];
         System.out.println(array[0][1]);
-
-        m(new int[] {1,2,3,4});
     }
 
-    private static void m(int[] params) {
+    private static void m(int... params) {
+
+    }
+    private static void m(int a, int... params) {
         for (int current : params) {
             System.out.println(current);
         }
