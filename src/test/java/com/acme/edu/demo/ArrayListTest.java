eca0bd20befdb16cdfb1e2f0444c9ab908896138
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-07-19 17:44:27 +0300

    Add Mockito stub demo.

--- src/test/java/com/acme/edu/demo/ArrayListTest.java
+++ src/test/java/com/acme/edu/demo/ArrayListTest.java
@@ -1,6 +1,12 @@
 package com.acme.edu.demo;
 
 import static junit.framework.Assert.assertEquals;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,10 +16,16 @@ import java.util.ArrayList;
  * Test Case
  */
 public class ArrayListTest {
+    private ArrayList<Object> sut;
+
+    @Before
+    public void setUp() {
+        sut = new ArrayList<>();
+    }
+
     @Test
     public void shouldElementExistsAndSizeIncrementedWhenElementNewAdded() {
         //region Fixture | Given
-        ArrayList<Object> sut = new ArrayList<>();
         Object dummy = new Object();
         //endregion
 
@@ -26,9 +38,32 @@ public class ArrayListTest {
         org.junit.Assert.assertTrue(sut.contains(dummy));
 
 //        assertThat(sut, Matcher) //Hamcrest
-        org.fest.assertions.Assertions.assertThat(sut) //Fluent Interface | API
+        assertThat(sut) //Fluent Interface | API
                 .hasSize(1)
                 .contains(dummy);
         //endregion
     }
+
+    @Test
+    public void shouldUseElementsToStringWhenToString() {
+        Object stub1 = mock(Object.class);
+        when(stub1.toString())
+                .thenReturn("test string 1")
+                .thenReturn("test string 2");
+
+        Object stub2 = mock(Object.class);
+        when(stub2.toString()).thenReturn("test string 3"); //any(Saver.class) //anyString()
+
+        sut.add(stub1);
+        sut.add(stub2);
+
+        String result = sut.toString();
+
+        assertThat(result)
+                .contains("test string 1")
+                .contains("test string 3");
+
+    }
+
+
 }
