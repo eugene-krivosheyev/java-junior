4831e13695b29b1c6285abea93b76400e3b18864
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-07-23 12:27:38 +0300

    Add Method Inner Class Demo.

--- src/main/java/com/acme/edu/functional/InnerClassesDemo.java
+++ src/main/java/com/acme/edu/functional/InnerClassesDemo.java
@@ -3,7 +3,7 @@ package com.acme.edu.functional;
 public class InnerClassesDemo {
     public static void main(String[] args) {
         Outer outer = new Outer();
-        Outer.Inner oi = outer.new Inner();
+        ChangeAndGetStateable result = outer.m();
     }
 }
 
@@ -14,16 +14,28 @@ class Outer {
     private static int staticOuterState = 0;
     private int instanceOuterState = 0;
 
-    class Inner {
-        private int innerState = 0;
+    public ChangeAndGetStateable m() {
+        int localVar = 0;
 
-        public int changeAndGetState() {
-            innerState = 1;
-            staticOuterState = 1;
-            Outer.this.instanceOuterState = 1;
-            return innerState;
+        class Inner implements ChangeAndGetStateable {
+            private int innerState = 0;
+
+            public int changeAndGetState() {
+                innerState = 1;
+                staticOuterState = 1;
+                Outer.this.instanceOuterState = 1;
+
+                System.out.println(localVar); //!!
+
+                return innerState;
+            }
         }
+
+        return new Inner();
     }
 }
 
+interface ChangeAndGetStateable {
+    int changeAndGetState();
+}
 
