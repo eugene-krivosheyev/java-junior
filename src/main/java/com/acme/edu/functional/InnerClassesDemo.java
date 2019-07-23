61a5ad57100cb31032d1c03786a5f44a0ea6dabe
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-07-23 12:20:11 +0300

    Add Inner Class Demo.

--- src/main/java/com/acme/edu/functional/InnerClassesDemo.java
+++ src/main/java/com/acme/edu/functional/InnerClassesDemo.java
@@ -2,8 +2,8 @@ package com.acme.edu.functional;
 
 public class InnerClassesDemo {
     public static void main(String[] args) {
-        Outer.Inner io = new Outer.Inner();
-        io.changeAndGetState();
+        Outer outer = new Outer();
+        Outer.Inner oi = outer.new Inner();
     }
 }
 
@@ -14,12 +14,13 @@ class Outer {
     private static int staticOuterState = 0;
     private int instanceOuterState = 0;
 
-    static class Inner {
+    class Inner {
         private int innerState = 0;
 
         public int changeAndGetState() {
             innerState = 1;
             staticOuterState = 1;
+            Outer.this.instanceOuterState = 1;
             return innerState;
         }
     }
