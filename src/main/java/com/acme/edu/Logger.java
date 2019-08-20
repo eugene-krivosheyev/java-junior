fbb1d51d48fabc0b6576e96c5026f604de59a761
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-08-20 15:16:59 +0300

    Add Singleton demo and FIXME usage.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,46 @@
 package com.acme.edu;
 
+//Single line
+/*
+Multi-line comment
+ */
+
+import static com.acme.edu.Logger.*;
+
+/**
+ * JavaDoc comment
+ * Class Logger has unnecessarily comment.
+ * <strong>bold</strong>
+ * @see java.lang.String#String()
+ */
 public class Logger {
+    static {
+        System.out.println("AAaaaa!!!!! WTF!!!!!!");
+    }
+
     public static void log(int message) {
+        //region Main
         System.out.println("primitive: " + message);
+        //endregion
     }
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
     }
+
+    public static void main(String[] args) {
+        log(0);
+    }
+}
+
+class Superman {
+    private static Superman theOne;
+
+    public static Superman getTheOne() {
+        return theOne;
+    }
+
+    //=========
+
+    private Superman() { }
 }
