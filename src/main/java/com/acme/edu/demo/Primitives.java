092d0a50df8970e6cdda1073d0295ca83ea8d942
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-08-20 17:30:35 +0300

    Add primitive types demo.

--- src/main/java/com/acme/edu/demo/Primitives.java
+++ src/main/java/com/acme/edu/demo/Primitives.java
@@ -21,5 +21,25 @@ public class Primitives {
         int charCode = c;
         //endregion
         //endregion
+
+        byte bb = 0;
+        int ii = bb; //auto casting
+        int iii = 1_000;
+        byte bbb = (byte) iii; //explicit casting
+
+        //arithmetic promotion: -> int, +long
+        int result = bb + bb;
+        int iiiii = (ii + ii);
+
+        //Type Overflow: BigInteger
+        byte counter = Byte.MIN_VALUE;
+        for (int j = Byte.MIN_VALUE; j < (Byte.MAX_VALUE + 4); j++) {
+            counter++;
+        }
+        byte newCounter = counter;
+        System.out.println(counter);
+
+        //Precision Loss:
+        System.out.println(.1 + .2);
     }
 }
