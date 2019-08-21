24c9c21a6c7e9b609c9061135257509b3806cf30
kate-c <kate-c@bk.ru>
2019-08-21 14:51:49 +0300

    Add modifications in iteration1

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,27 +1,42 @@
 package com.acme.edu;
+import com.acme.edu.Printer;
+
+import java.util.Objects;
 
 public class Logger {
+
+    private final static String PRIMITIVE_CONST_MESSAGE = "primitive: ";
+    private final static String CHAR_CONST_MESSAGE = "char: ";
+    private final static String STRING_CONST_MESSAGE = "string: ";
+    private final static String REFERENCE_CONST_MESSAGE = "reference: ";
+
+    private static void logPrimitive(String message) {
+        Printer.print(PRIMITIVE_CONST_MESSAGE + message);
+    }
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        logPrimitive(String.valueOf(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
-    }
-
-    public static void log(char message) {
-        System.out.println("char: " + message);
-    }
-
-    public static void log(String message) {
-        System.out.println("string: " + message);
+        logPrimitive(String.valueOf(message));
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        logPrimitive(String.valueOf(message));
+    }
+
+    public static void log(char message) {
+        Printer.print(CHAR_CONST_MESSAGE + message);
+    }
+
+    public static void log(String message) {
+        Printer.print(STRING_CONST_MESSAGE + message);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        Printer.print(REFERENCE_CONST_MESSAGE + message);
     }
+
+
 }
---
+++ src/main/java/com/acme/edu/Printer.java
@@ -0,0 +1,10 @@
+package com.acme.edu;
+
+/**
+ * Created by kate-c on 21/08/2019.
+ */
+public class Printer {
+    public  static void print(String message) {
+        System.out.println(message);
+    }
+}
