8469715f0414cc1fd23ed6546fee219478db034c
kate-c <kate-c@bk.ru>
2019-08-21 16:18:33 +0300

    Add fixes in iteration1

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,18 +1,11 @@
 package com.acme.edu;
-import com.acme.edu.Printer;
-
-import java.util.Objects;
 
 public class Logger {
 
-    private final static String PRIMITIVE_CONST_MESSAGE = "primitive: ";
-    private final static String CHAR_CONST_MESSAGE = "char: ";
-    private final static String STRING_CONST_MESSAGE = "string: ";
-    private final static String REFERENCE_CONST_MESSAGE = "reference: ";
-
-    private static void logPrimitive(String message) {
-        Printer.print(PRIMITIVE_CONST_MESSAGE + message);
-    }
+    private final static String PRIMITIVE_PREFIX = "primitive: ";
+    private final static String CHAR_PREFIX = "char: ";
+    private final static String STRING_PREFIX = "string: ";
+    private final static String REFERENCE_PREFIX = "reference: ";
 
     public static void log(int message) {
         logPrimitive(String.valueOf(message));
@@ -27,16 +20,50 @@ public class Logger {
     }
 
     public static void log(char message) {
-        Printer.print(CHAR_CONST_MESSAGE + message);
+        logChar(String.valueOf(message));
     }
 
     public static void log(String message) {
-        Printer.print(STRING_CONST_MESSAGE + message);
+        logString(message);
     }
 
     public static void log(Object message) {
-        Printer.print(REFERENCE_CONST_MESSAGE + message);
+        logObject(String.valueOf(message));
     }
 
+    // region Decorators
+    private static String decoratePrimitive(String message) {
+        return PRIMITIVE_PREFIX + message;
+    }
 
+    private static String decorateString(String message) {
+        return STRING_PREFIX + message;
+    }
+
+    private static String decorateReference(String message) {
+        return REFERENCE_PREFIX + message;
+    }
+
+    private static String decorateChar(String message) {
+        return CHAR_PREFIX + message;
+    }
+    // endregion
+
+    // region Loggers for each type
+    private static void logPrimitive(String message) {
+        Printer.print(decoratePrimitive(message));
+    }
+
+    private static void logChar(String message) {
+        Printer.print(decorateChar(message));
+    }
+
+    private static void logString(String message) {
+        Printer.print(decorateString(message));
+    }
+
+    private static void logObject(String message) {
+        Printer.print(decorateReference(message));
+    }
+    // endregion
 }
