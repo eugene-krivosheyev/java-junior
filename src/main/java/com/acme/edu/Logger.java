2d72ac0d98e6a127d2af89bf7d3af239e4aafe3e
blazzen <pavel.zhukov.official@gmail.com>
2019-08-21 15:16:06 +0300

    Extract constant for primitive type

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,12 +1,19 @@
 package com.acme.edu;
 
 public class Logger {
+
+    private static final String PRIMITIVE = "primitive: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE + message);
+    }
+
+    public static void log(boolean message) {
+        System.out.println(PRIMITIVE + message);
     }
 
     public static void log(char message) {
@@ -15,10 +22,6 @@ public class Logger {
 
     public static void log(String message) {
         System.out.println("string: " + message);
-    }
-
-    public static void log(boolean message) {
-        System.out.println("primitive: " + message);
     }
 
     public static void log(Object message) {
