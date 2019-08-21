7594bbef8040053bfc6cd84f744e9d149045d01e
Vlad Mashurin <27vl@mail.ru>
2019-08-21 15:15:51 +0300

    Remove TODO

--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -53,9 +53,6 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
     }
 
-    /*
-    TODO: implement Logger solution to match specification as tests
-     */
 
     @Test
     public void shouldLogChar() throws IOException {
