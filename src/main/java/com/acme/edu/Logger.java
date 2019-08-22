dec636f92f429dfb381cfb58d89bebb2690ccb42
kate-c <kate-c@bk.ru>
2019-08-22 13:09:50 +0300

    Add iteration2

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,5 +1,7 @@
 package com.acme.edu;
 
+
+
 public class Logger {
 
     private final static String PRIMITIVE_PREFIX = "primitive: ";
@@ -7,28 +9,113 @@ public class Logger {
     private final static String STRING_PREFIX = "string: ";
     private final static String REFERENCE_PREFIX = "reference: ";
 
+    private static enum Type {
+        UNDEFINED,
+        INTEGER,
+        BYTE,
+        BOOLEAN,
+        CHAR,
+        STRING,
+        OBJECT
+    }
+
+    private static Type currentType = Type.UNDEFINED;
+
+    private static String stringBuffer = null;
+    private static int stringCounter = 0;
+
+    private static long numberBuffer = 0;
+
+    public static void log(String message) {
+        flashAndChangeTypeIfNeeded(Type.STRING);
+        accumulate(message);
+    }
+
     public static void log(int message) {
-        logPrimitive(String.valueOf(message));
+        flashAndChangeTypeIfNeeded(Type.INTEGER);
+        accumulate(message);
     }
 
     public static void log(byte message) {
-        logPrimitive(String.valueOf(message));
+        flashAndChangeTypeIfNeeded(Type.BYTE);
+        accumulate(message);
     }
 
     public static void log(boolean message) {
+        flashAndChangeTypeIfNeeded(Type.BOOLEAN);
         logPrimitive(String.valueOf(message));
     }
 
     public static void log(char message) {
+        flashAndChangeTypeIfNeeded(Type.CHAR);
         logChar(String.valueOf(message));
     }
 
-    public static void log(String message) {
-        logString(message);
+    public static void log(Object message) {
+        flashAndChangeTypeIfNeeded(Type.OBJECT);
+        logObject(String.valueOf(message));
     }
 
-    public static void log(Object message) {
-        logObject(String.valueOf(message));
+    private static void flashAndChangeTypeIfNeeded(Type type) {
+        final boolean shouldTypeBeChanged = (type != currentType);
+        if (shouldTypeBeChanged) {
+            flush();
+            currentType = type;
+        }
+    }
+
+    public static void flush() {
+        switch (currentType) {
+            case UNDEFINED:
+                break;
+            case INTEGER:
+            case BYTE:
+                logPrimitive(String.valueOf(numberBuffer));
+                numberBuffer = 0;
+                break;
+            case BOOLEAN:
+                break;
+            case CHAR:
+                break;
+            case STRING:
+                if (stringBuffer == null) break;
+                String message = stringBuffer;
+                if (stringCounter > 1) {
+                    message += " (x" + stringCounter + ")";
+                }
+                logString(message);
+                stringBuffer = null;
+                break;
+            case OBJECT:
+                break;
+        }
+    }
+
+    private static void accumulate(String message) {
+        if (stringBuffer == null) {
+            stringBuffer = message;
+            stringCounter = 1;
+        } else if (stringBuffer.equals(message)) {
+            stringCounter++;
+        } else {
+            flush();
+            stringBuffer = message;
+            stringCounter = 1;
+        }
+    }
+
+    private static void accumulate(int number) {
+        if (numberBuffer + number >= Integer.MAX_VALUE) {
+            flush();
+        }
+        numberBuffer += number;
+    }
+
+    private static void accumulate(byte number) {
+        if (numberBuffer + number >= Byte.MAX_VALUE) {
+            flush();
+        }
+        numberBuffer += number;
     }
 
     // region Decorators
--- src/test/java/com/acme/edu/iteration02/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration02/LoggerTest.java
@@ -23,9 +23,6 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     //endregion
 
 
-    /*
-    TODO: implement Logger solution to match specification as tests
-
     @Test
     public void shouldLogSequentIntegersAsSum() throws IOException {
         //region when
@@ -34,38 +31,41 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         Logger.log(2);
         Logger.log("str 2");
         Logger.log(0);
+        Logger.flush();
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "3\n" +
-            "str 2\n" +
-            "0\n"
-        );
+        assertSysoutContains("str 1\n");
+        assertSysoutContains("3\n");
+        assertSysoutContains("str 2\n");
+        assertSysoutContains("0\n");
         //endregion
     }
+
 
     @Test
     public void shouldLogCorrectlyIntegerOverflowWhenSequentIntegers() {
         //region when
         Logger.log("str 1");
+        Logger.log(1);
         Logger.log(10);
         Logger.log(Integer.MAX_VALUE);
+        Logger.log(15);
         Logger.log("str 2");
         Logger.log(0);
+        Logger.flush();
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "10\n" +
-            Integer.MAX_VALUE + "\n" +
-            "str 2\n" +
-            "0\n"
-        );
+        assertSysoutContains("str 1\n");
+        assertSysoutContains("11\n");
+        assertSysoutContains(Integer.MAX_VALUE + "\n");
+        assertSysoutContains("15\n");
+        assertSysoutContains("str 2\n");
+        assertSysoutContains("0\n");
         //endregion
     }
+
 
     @Test
     public void shouldLogCorrectlyByteOverflowWhenSequentBytes() {
@@ -75,18 +75,18 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         Logger.log((byte)Byte.MAX_VALUE);
         Logger.log("str 2");
         Logger.log(0);
+        Logger.flush();
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "10\n" +
-            Byte.MAX_VALUE + "\n" +
-            "str 2\n" +
-            "0\n"
-        );
+        assertSysoutContains("str 1\n");
+        assertSysoutContains("10\n");
+        assertSysoutContains(Byte.MAX_VALUE + "\n");
+        assertSysoutContains("str 2\n");
+        assertSysoutContains("0\n");
         //endregion
     }
+
 
     @Test
     public void shouldLogSameSubsequentStringsWithoutRepeat() throws IOException {
@@ -99,18 +99,16 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         Logger.log("str 3");
         Logger.log("str 3");
         Logger.log("str 3");
+        Logger.flush();
         //endregion
 
         //region then
-        assertSysoutEquals(
-            "str 1\n" +
-            "str 2 (x2)\n" +
-            "0\n" +
-            "str 2\n" +
-            "str 3 (x3)\n"
-        );
+        assertSysoutContains("str 1\n");
+        assertSysoutContains("str 2 (x2)\n");
+        assertSysoutContains("0\n");
+        assertSysoutContains("str 2\n");
+        assertSysoutContains("str 3 (x3)\n");
         //endregion
     }
 
-    */
 }
