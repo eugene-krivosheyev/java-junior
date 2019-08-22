e4203fabd44b1d49fbd6bd132332b6814ee9e7f1
Viktor Vishnevskiy <vik_96@inbox.ru>
2019-08-22 10:43:51 +0300

    Add prefixes.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,28 +2,33 @@ package com.acme.edu;
 
 public class Logger {
 
+    private final static String PRIMITIVE_PREFIX = "primitive: ";
+    private final static String CHAR_PREFIX = "char: ";
+    private final static String STRING_PREFIX = "string: ";
+    private final static String REFERENCE_PREFIX = "reference: ";
+    
     //Bad code style
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        System.out.println(CHAR_PREFIX + message);
     }
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        System.out.println(STRING_PREFIX + message);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        System.out.println(REFERENCE_PREFIX + message);
     }
 }
