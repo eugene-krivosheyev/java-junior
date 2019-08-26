ac2b087e75102a036f4465a77b390ecee10539bb
vsavinova <vsavinova31@gmail.com>
2019-08-26 15:59:18 +0300

    Move to oop. Finished. :>

--- src/main/java/com/acme/edu/Constants.java
+++ src/main/java/com/acme/edu/Constants.java
@@ -1,11 +1,11 @@
 package com.acme.edu;
 
 public class Constants {
-    static final String PRIMITIVE_PREFIX = "primitive: ";
-    static final String CHAR_PREFIX = "char: ";
-    static final String STRING_PREFIX = "string: ";
-    static final String OBJECT_PREFIX = "reference: ";
-    static final String ARRAY_PREFIX = "primitives array: ";
-    static final String MATRIX_PREFIX = "primitives matrix: ";
-    static final String MULTIMATRIX_PREFIX = "primitives multimatrix: ";
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+    public static final String CHAR_PREFIX = "char: ";
+    public static final String STRING_PREFIX = "string: ";
+    public static final String OBJECT_PREFIX = "reference: ";
+    public static final String ARRAY_PREFIX = "primitives array: ";
+    public static final String MATRIX_PREFIX = "primitives matrix: ";
+    public static final String MULTIMATRIX_PREFIX = "primitives multimatrix: ";
 }
