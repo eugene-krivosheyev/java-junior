ddd87bbe9cbbc9b38b4e6825e71e3b9cf500f323
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-08-27 17:11:36 +0300

    Add lambda demo.

--- src/main/java/com/acme/edu/demo/InnerClassesDemo.java
+++ src/main/java/com/acme/edu/demo/InnerClassesDemo.java
@@ -1,46 +1,14 @@
 package com.acme.edu.demo;
 
 import com.acme.edu.Logger;
-import com.acme.edu.ooaddemo2.Command;
-import com.acme.edu.ooaddemo2.LoggerController;
-import com.acme.edu.ooaddemo2.SuperFilter;
-import com.acme.edu.ooaddemo2.SuperSaver;
+import com.acme.edu.ooaddemo2.*;
 
 public class InnerClassesDemo {
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        System.out.println(outer.m(5).toString());
-
+        SuperConsoleSaver s = new SuperConsoleSaver();
         new LoggerController(
-                new SuperFilter() {
-                    @Override
-                    public boolean isFiltered(Command command) {
-                        return false;
-                    }
-                },
-
-                new SuperSaver() {
-                    @Override
-                    public void save(String message) {
-
-                    }
-                }
+                command -> "WTF".equals(command.getMessage()),
+                s::save
         );
-    }
-}
-
-class Outer {
-    private static int staticState = 1;
-    private int instanceState = 2;
-
-    public Object m(int param) {
-        int localVar = param;
-
-        return new Object() {
-            @Override
-            public String toString() {
-                return staticState + " " + instanceState + " " + localVar;
-            }
-        };
     }
 }
