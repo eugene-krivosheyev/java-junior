c8717b788cbdf1bf6b93a1f826bfc3ad7ac21583
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-08-27 16:25:04 +0300

    Add non-static inner class demo.

--- src/main/java/com/acme/edu/demo/InnerClassesDemo.java
+++ src/main/java/com/acme/edu/demo/InnerClassesDemo.java
@@ -2,20 +2,25 @@ package com.acme.edu.demo;
 
 public class InnerClassesDemo {
     public static void main(String[] args) {
-        Outer.Inner object = new Outer.Inner();
-        object.m();
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+
+        Outer.Inner inn = new Outer().new Inner();
+
     }
 }
 
 class Outer {
     private static int staticState = 1;
-    private int instanceState = 2;
+    private int shadedInstanceState = 2;
 
-    public static class Inner {
-        public void m() {
+    public class Inner {
+        public void m(int shadedInstanceState) {
             staticState = 2;
             System.out.println(staticState);
             new Outer();
+
+            Outer.this.shadedInstanceState = 3;
         }
     }
 }
