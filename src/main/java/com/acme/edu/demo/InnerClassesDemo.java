3eb43c7dd83e8eb0a6b8ff9be2c642d4cdb4e05d
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-08-27 16:53:07 +0300

    Add anonymous inner class demo.

--- src/main/java/com/acme/edu/demo/InnerClassesDemo.java
+++ src/main/java/com/acme/edu/demo/InnerClassesDemo.java
@@ -1,9 +1,31 @@
 package com.acme.edu.demo;
 
+import com.acme.edu.Logger;
+import com.acme.edu.ooaddemo2.Command;
+import com.acme.edu.ooaddemo2.LoggerController;
+import com.acme.edu.ooaddemo2.SuperFilter;
+import com.acme.edu.ooaddemo2.SuperSaver;
+
 public class InnerClassesDemo {
     public static void main(String[] args) {
         Outer outer = new Outer();
         System.out.println(outer.m(5).toString());
+
+        new LoggerController(
+                new SuperFilter() {
+                    @Override
+                    public boolean isFiltered(Command command) {
+                        return false;
+                    }
+                },
+
+                new SuperSaver() {
+                    @Override
+                    public void save(String message) {
+
+                    }
+                }
+        );
     }
 }
 
@@ -14,13 +36,11 @@ class Outer {
     public Object m(int param) {
         int localVar = param;
 
-        class Inner extends Object {
+        return new Object() {
             @Override
             public String toString() {
                 return staticState + " " + instanceState + " " + localVar;
             }
-        }
-
-        return new Inner();
+        };
     }
 }
