9c4f4f33bc209381f68259c5e1082854252e596b
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-09-02 18:02:21 +0300

    Add naive solution for TX.

--- src/main/java/com/acme/edu/concurrency/ConcurrencyDemo.java
+++ src/main/java/com/acme/edu/concurrency/ConcurrencyDemo.java
@@ -7,17 +7,20 @@ import java.util.concurrent.TimeUnit;
 
 public class ConcurrencyDemo {
     public static void main(String[] args) throws InterruptedException {
-        final Counter sharedData = new Counter();
+        final Account account1 = new Account(100_000);
+        final Account account2 = new Account(0);
+        final Transaction tx = new Transaction();
         final ExecutorService pool = Executors.newFixedThreadPool(10);
 
         for (int i = 0; i < 100_000; i++) {
-            pool.submit(sharedData::increment);
+            pool.submit(() -> tx.transaction(account1, account2, 1));
         }
 
         pool.shutdown();
         pool.awaitTermination(10, TimeUnit.SECONDS);
 
-        System.out.println(sharedData.getCounter());
+        System.out.println(account1.getAmount());
+        System.out.println(account2.getAmount());
 
     }
 }
@@ -44,5 +47,35 @@ class Counter {
         synchronized (this.monitor) {
             return counter;
         }
+    }
+}
+
+class Transaction {
+    /**
+     * Ok for consistency, but performance fail
+     */
+    public synchronized void transaction(Account a1, Account a2, int amount) {
+        a1.withdraw(amount);
+        a2.credit(amount);
+    }
+}
+
+class Account {
+    private int amount;
+
+    public Account(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void withdraw(int amount) {
+        this.amount -= amount;
+    }
+
+    public void credit(int amount) {
+        this.amount += amount;
     }
 }
