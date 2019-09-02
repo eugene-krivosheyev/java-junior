ec9842d394836ceacc6a605d5011b3af3aa2e4c3
blazzen <pavel.zhukov.official@gmail.com>
2019-09-02 08:23:18 +0300

    Rework StringCommand

--- src/main/java/com/acme/edu/commands/StringCommand.java
+++ src/main/java/com/acme/edu/commands/StringCommand.java
@@ -4,41 +4,42 @@ import com.acme.edu.savers.Saver;
 
 import java.util.Objects;
 
-// TODO: Rework whole class!
 public class StringCommand implements Command, AccumulableCommand {
     public static final String STRING_PREFIX = "string: ";
 
     private int counter = 1;
-    private String buffer = "";
     private String message;
 
     public StringCommand(String message) {
         this.message = message;
     }
 
-    private StringCommand(String buffer, String message) {
-        this.buffer = buffer;
+    private StringCommand(String message, int counter) {
         this.message = message;
-    }
-
-    private StringCommand(int counter, String buffer, String message) {
         this.counter = counter;
-        this.buffer = buffer;
-        this.message = message;
     }
 
     @Override
     public String getDecorated() {
-        return buffer.replaceFirst("\n", "") + decorate(message, counter);
+        String result = STRING_PREFIX + message;
+        if (counter > 1) {
+            result += " (x" + counter + ")";
+        }
+        return result;
     }
 
     @Override
     public StringCommand accumulate(Command other, Saver saver) {
+        if (!(other instanceof StringCommand)) {
+            throw new IllegalArgumentException("Can't accumulate StringCommand with other Command subclass");
+        }
+
         String otherMessage = ((StringCommand) other).getMessage();
+
         if (Objects.equals(message, otherMessage)) {
             return getIncrementedCopy();
         } else {
-            return getCopyWithResolvedBuffer(otherMessage);
+            return flushPreviousStringAndGetNew(otherMessage, saver);
         }
     }
 
@@ -47,18 +48,11 @@ public class StringCommand implements Command, AccumulableCommand {
     }
 
     private StringCommand getIncrementedCopy() {
-        return new StringCommand(this.counter + 1, this.buffer, this.message);
+        return new StringCommand(this.message, this.counter + 1);
     }
 
-    private StringCommand getCopyWithResolvedBuffer(String newMessage) {
-        return new StringCommand(this.buffer + decorate(this.message, this.counter), newMessage);
-    }
-
-    private static String decorate(String message, int counter) {
-        String result = "\n" + STRING_PREFIX + message;
-        if (counter > 1) {
-            result += " (x" + counter + ")";
-        }
-        return result;
+    private StringCommand flushPreviousStringAndGetNew(String newMessage, Saver saver) {
+        saver.save(getDecorated());
+        return new StringCommand(newMessage, 1);
     }
 }
