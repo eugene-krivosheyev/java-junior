7ebe354d289b07f6f4c1194fed9ce935f366937c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-09-02 11:18:06 +0300

    Pool and Callable & Futures Demo.

--- src/main/java/com/acme/edu/threadingdemo/ThreadsDemo.java
+++ src/main/java/com/acme/edu/threadingdemo/ThreadsDemo.java
@@ -1,18 +1,29 @@
 package com.acme.edu.threadingdemo;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ThreadsDemo {
     public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            while (true) {
-                System.out.println("Thread-1");
-            }
+        ExecutorService pool = Executors.newFixedThreadPool(8);
+        pool.execute(() -> System.out.println("HW1"));
+        pool.execute(() -> System.out.println("HW2"));
+        pool.execute(() -> System.out.println("HW3"));
+        pool.execute(() -> System.out.println("HW4"));
+
+        final Future<Integer> futureInt = pool.submit(() -> {
+            return 2 + 2;
         });
-        thread.start();
+
+//        futureInt.get();
+        futureInt.isDone();
+        futureInt.cancel(true);
+
+        pool.shutdown();
 
 
-        while (true) {
-            System.out.println("main");
-        }
 
 
     }
