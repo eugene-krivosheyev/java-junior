15700b7244e9c1a879cdbe01b29fce94c7965628
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-09-02 22:37:52 +0300

    Add concurrency issue demo with transactions.

--- src/main/java/com/acme/edu/concurrency/ConcurrencyDemo.java
+++ src/main/java/com/acme/edu/concurrency/ConcurrencyDemo.java
@@ -1,6 +1,5 @@
 package com.acme.edu.concurrency;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -10,25 +9,23 @@ public class ConcurrencyDemo {
         final Account account1 = new Account(100_000);
         final Account account2 = new Account(0);
         final Transaction tx = new Transaction();
+
         final ExecutorService pool = Executors.newFixedThreadPool(1000);
-
-        for (int i = 0; i < 50_000; i++) {
-            pool.submit(() -> tx.transaction(account1, account2, 1));
-            pool.submit(() -> tx.transaction(account2, account1, 2));
+        for (int i = 0; i < 100_000; i++) {
+            pool.submit(() -> tx.noLockingTransaction(account1, account2, 1));
         }
-
         pool.shutdown();
         pool.awaitTermination(10, TimeUnit.SECONDS);
 
-        System.out.println(account1.getAmount());
-        System.out.println(account2.getAmount());
+        System.out.println(account1.getAmount() + " + " + account2.getAmount() + " == ");
+        System.out.println(account1.getAmount() + account2.getAmount());
 
     }
 }
 
 class Counter {
     private int counter;
-    private Object monitor = new Object();
+    private final Object monitor = new Object();
 
     /**
      * Data Race
@@ -52,20 +49,29 @@ class Counter {
 }
 
 class Transaction {
-    /**
-     * Ok for consistency, but performance fail
-     * a1 = id01
-     * a2 = id02
-     * tx(01, 02, 1)
-     * tx(02, 01, 1)
-     */
-    public synchronized void transaction(Account a1, Account a2, int amount) {
+    public void syncWithFileLevelLockingTransaction(Account a1, Account a2, int amount) {
         synchronized (a1) {
             synchronized (a2) {
-                a1.withdraw(amount);
-                a2.credit(amount);
+                int amount1 = a1.getAmount();
+                a1.setAmount(amount1 - amount);
+                int amount2 = a2.getAmount();
+                a2.setAmount(amount2 + amount);
             }
         }
+    }
+
+    public synchronized void syncWithCoarseLevelLockingTransaction(Account a1, Account a2, int amount) {
+        int amount1 = a1.getAmount();
+        a1.setAmount(amount1 - amount);
+        int amount2 = a2.getAmount();
+        a2.setAmount(amount2 + amount);
+    }
+
+    public void noLockingTransaction(Account a1, Account a2, int amount) {
+        int amount1 = a1.getAmount();
+        a1.setAmount(amount1 - amount);
+        int amount2 = a2.getAmount();
+        a2.setAmount(amount2 + amount);
     }
 }
 
@@ -80,11 +86,7 @@ class Account {
         return amount;
     }
 
-    public synchronized void withdraw(int amount) {
-        this.amount -= amount;
-    }
-
-    public synchronized void credit(int amount) {
-        this.amount += amount;
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 }
