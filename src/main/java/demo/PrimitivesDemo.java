ff3474fb597662286b9feb627486beeb745a9001
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-02 18:00:31 +0300

    Add primitives demo: part 1.

---
+++ src/main/java/demo/PrimitivesDemo.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class PrimitivesDemo {
+    public static void main(String[] args) {
+        //region Целые
+        byte b = 07; //0b100
+        short s = 1_000;
+        int i = 0xDEADCAFE;
+        long l = 999999999999999999L;
+        //Byte.MAX_VALUE
+        //endregion
+    }
+}
