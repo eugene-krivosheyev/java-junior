50923c900782bcf74ce0c459fb7436ce3a543b96
keleborn <ivanenko-aleksei@yandex.ru>
2020-09-02 13:33:34 +0300

    It works. Team 1.

--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -32,7 +32,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
 
         //region then
         assertSysoutContains("primitive: ");
-        assertSysoutEquals("primitive: 1\nprimitive: 0\nprimitive: -1\n");
+        assertSysoutEquals("primitive: 1" + System.lineSeparator() + "primitive: 0"+ System.lineSeparator() +"primitive: -1"+System.lineSeparator());
         //endregion
     }
 
