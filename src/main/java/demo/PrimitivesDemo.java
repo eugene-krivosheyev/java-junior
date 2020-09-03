9f058e88456c3781384ddded36ec1199480d24b7
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-03 10:51:38 +0300

    Add primitives demo: part 2 + type casting.

--- src/main/java/demo/PrimitivesDemo.java
+++ src/main/java/demo/PrimitivesDemo.java
@@ -9,5 +9,39 @@ public class PrimitivesDemo {
         long l = 999999999999999999L;
         //Byte.MAX_VALUE
         //endregion
+
+        //region Дробные
+        float f = .0F; //32 bit
+        double d = 2.1e3; //64 bit
+        //Float.MAX_VALUE
+        //endregion
+
+        //region Логический
+        boolean bb = true; //false
+        //bb = (boolean) 5;
+        //endregion
+
+        //region Символьный
+        char c = 'ざ'; //int charCode = c;
+        System.out.println( (char)0xABCD );
+        System.out.println('\n');
+        //endregion
+
+
+        //region Приведение типов
+        //region Arithmetic promotions
+        byte b1 = 1, b2 = 2;
+        int byteResult = b1 + b2;
+        long intLongResult = 1 + 1L;
+        //endregion
+        //region Auto casting, неявное приведение
+        short shortResult = b1;
+        //endregion
+        //region Явное приведение
+        int smallIntValue = Byte.MAX_VALUE + 1;
+        byte smallByteValue = (byte) smallIntValue;
+        System.out.println(smallByteValue);
+        //endregion
+        //endregion
     }
 }
