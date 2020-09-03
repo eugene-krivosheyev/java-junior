26f0386c6d9216f2454547479898a01e0e184a70
anharusp <nuro4ka2011@yandex.ru>
2020-09-03 15:53:52 +0300

    Refactored method for function creation

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -6,21 +6,20 @@ public class Logger {
     public static final String STRING_PREFIX = "string: ";
     public static final String REFERENCE_PREFIX = "reference: ";
 
-    public static void log(int message) { createAndPrintMessage(PRIMITIVE_PREFIX, message); }
+    public static void log(int message) { print(createMessage(PRIMITIVE_PREFIX, message)); }
 
-    public static void log(byte message) { createAndPrintMessage(PRIMITIVE_PREFIX, message); }
+    public static void log(byte message) { print(createMessage(PRIMITIVE_PREFIX, message)); }
 
-    public static void log(char message) { createAndPrintMessage(CHAR_PREFIX, message); }
+    public static void log(char message) { print(createMessage(CHAR_PREFIX, message)); }
 
-    public static void log(String message) { createAndPrintMessage(STRING_PREFIX, message); }
+    public static void log(String message) { print(createMessage(STRING_PREFIX, message)); }
 
-    public static void log(boolean message) { createAndPrintMessage(PRIMITIVE_PREFIX, message); }
+    public static void log(boolean message) { print(createMessage(PRIMITIVE_PREFIX, message)); }
 
-    public static void log(Object message) { createAndPrintMessage(REFERENCE_PREFIX, message); }
+    public static void log(Object message) { print(createMessage(REFERENCE_PREFIX, message)); }
 
-    private static void createAndPrintMessage(String nameType, Object message) {
-        String fullMessage = nameType + message;
-        print(fullMessage);
+    private static String createMessage(String nameType, Object message) {
+        return nameType + message;
     }
 
     private static void print(String message) { System.out.println(message); }
