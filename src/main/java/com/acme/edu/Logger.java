8deb934de5d2ab0de061a88b5401458a3056e41a
evgeniask <evisunny@yandex.ru>
2020-09-03 14:58:31 +0300

    Implemented functionality to log different message types

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,38 @@
 package com.acme.edu;
 
 public class Logger {
+
+    static String primitivePrefix = "primitive";
+    static String charPrefix = "char";
+    static String stringPrefix = "string";
+    static String objPrefix = "reference";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        printLogMessage(primitivePrefix, message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printLogMessage(primitivePrefix, message);
     }
+
+    public static void log(char message) {
+        printLogMessage(charPrefix, message);
+    }
+
+    public static void log(String message) {
+        printLogMessage(stringPrefix, message);
+    }
+
+    public static void log(boolean message) {
+        printLogMessage(primitivePrefix, message);
+    }
+
+    public static void log(Object message) {
+        printLogMessage(objPrefix, message);
+    }
+
+    public static void printLogMessage(String prefix, Object message) {
+        System.out.println(prefix + ": " + message.toString());
+    }
+
 }
