9cdab2eb8b67ec613fa3824bf3713855db6b5a66
Anastasiya Tiunova <a_tiunova99@mail.ru>
2020-09-03 15:42:50 +0300

    Prefixes & writeMessage

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,26 +1,37 @@
 package com.acme.edu;
 
 public class Logger {
+
+    public static final String PREFIX_PRIMITIVE = "primitive: ";
+    public static final String PREFIX_CHAR = "char: ";
+    public static final String PREFIX_STRING = "string: ";
+    public static final String PREFIX_REFERENCE = "reference: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        writeMessage(PREFIX_PRIMITIVE + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        writeMessage(PREFIX_PRIMITIVE + message);
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        writeMessage(PREFIX_CHAR + message);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        writeMessage(PREFIX_PRIMITIVE + message);
     }
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        writeMessage(PREFIX_STRING + message);
     }
+
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        writeMessage(PREFIX_REFERENCE + message);
+    }
+
+    private static void writeMessage(String message) {
+        System.out.println(message);
     }
 }
