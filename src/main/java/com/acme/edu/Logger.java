aed70577109d850b27eb4f4eba6d3c28e0e6855a
anharusp <nuro4ka2011@yandex.ru>
2020-09-03 14:51:43 +0300

    Updated code reuse and static variables for Logger

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,26 +1,27 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void print(String nameType, Object message) { System.out.println(nameType + message); }
+    public static final String PRIMITIVE = "primitive: ";
+    public static final String CHAR = "char: ";
+    public static final String STRING = "string: ";
+    public static final String REFERENCE = "reference: ";
 
-    public static void log(int message) { print("primitive: ", message); }
+    public static void log(int message) { createAndPrintMessage(PRIMITIVE, message); }
 
-    public static void log(byte message) { print("primitive: ", message); }
+    public static void log(byte message) { createAndPrintMessage(PRIMITIVE, message); }
 
-    public static void log(char message) {
-        print("char: ", message);
+    public static void log(char message) { createAndPrintMessage(CHAR, message); }
+
+    public static void log(String message) { createAndPrintMessage(STRING, message); }
+
+    public static void log(boolean message) { createAndPrintMessage(PRIMITIVE, message); }
+
+    public static void log(Object message) { createAndPrintMessage(REFERENCE, message); }
+
+    private static void createAndPrintMessage(String nameType, Object message) {
+        String fullMessage = nameType + message;
+        print(fullMessage);
     }
 
-    public static void log(String message) {
-        print("string: ", message);
-    }
-
-    public static void log(boolean message) {
-        print("primitive: ", message);
-    }
-
-    public static void log(Object message) {
-        print("reference: ", message);
-    }
-
+    private static void print(String message) { System.out.println(message); }
 }
