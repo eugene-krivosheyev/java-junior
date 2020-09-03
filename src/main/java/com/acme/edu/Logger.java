c93ddd4cc7b906764276ad1530f3bcd2cceae52a
Alexander Senin <aaasenin@gmail.com>
2020-09-03 14:56:33 +0300

    final refactoring

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,27 +1,32 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
-    }
+    private static String PRIMITIVE = "primitive: ";
+    private static String CHAR = "char: ";
+    private static String STRING = "string: ";
+    private static String REFERENCE = "reference: ";
+
+    private static void print(String prefix, Object postfix) { System.out.println(prefix + postfix); }
+
+    public static void log(int message) { print(PRIMITIVE, message); }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        print(PRIMITIVE, message);
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        print(CHAR, message);
     }
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        print(STRING, message);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        print(PRIMITIVE, message);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        print(REFERENCE, message);
     }
 }
