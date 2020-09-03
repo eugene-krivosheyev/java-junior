e37d4cd100f5853cdf2d8a42b08a0bc1f3e08675
ArseniyIFMO <vatrushka.96@mail.ru>
2020-09-03 14:49:48 +0300

    Add Constants

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,31 +1,40 @@
 package com.acme.edu;
 
 public class Logger {
+
+    private static String primitiveType = "primitive: ";
+
+    private static String charType = "char: ";
+
+    private static String stringType = "string: ";
+
+    private static String referenceType = "reference: ";
+
     private static void printMessage(String message) {
         System.out.println(message);
     }
 
     public static void log(int message) {
-        printMessage("primitive: " + message);
+        printMessage(primitiveType + message);
     }
 
     public static void log(byte message) {
-        printMessage("primitive: " + message);
+        printMessage(primitiveType + message);
     }
 
     public static void log(char message) {
-        printMessage("char: " + message);
+        printMessage(charType + message);
     }
 
     public static void log(String message) {
-        printMessage("string: " + message);
+        printMessage(stringType + message);
     }
 
     public static void log(boolean message) {
-        printMessage("primitive: " + message);
+        printMessage(primitiveType + message);
     }
 
     public static void log(Object message) {
-        printMessage("reference: " + message);
+        printMessage(referenceType + message);
     }
 }
