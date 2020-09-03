f4011462149227039f7f3aac5a1ccaf5b2eed9b1
anharusp <nuro4ka2011@yandex.ru>
2020-09-03 15:18:50 +0300

    Updated variable naming with PREFIX

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,22 +1,22 @@
 package com.acme.edu;
 
 public class Logger {
-    public static final String PRIMITIVE = "primitive: ";
-    public static final String CHAR = "char: ";
-    public static final String STRING = "string: ";
-    public static final String REFERENCE = "reference: ";
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+    public static final String CHAR_PREFIX = "char: ";
+    public static final String STRING_PREFIX = "string: ";
+    public static final String REFERENCE_PREFIX = "reference: ";
 
-    public static void log(int message) { createAndPrintMessage(PRIMITIVE, message); }
+    public static void log(int message) { createAndPrintMessage(PRIMITIVE_PREFIX, message); }
 
-    public static void log(byte message) { createAndPrintMessage(PRIMITIVE, message); }
+    public static void log(byte message) { createAndPrintMessage(PRIMITIVE_PREFIX, message); }
 
-    public static void log(char message) { createAndPrintMessage(CHAR, message); }
+    public static void log(char message) { createAndPrintMessage(CHAR_PREFIX, message); }
 
-    public static void log(String message) { createAndPrintMessage(STRING, message); }
+    public static void log(String message) { createAndPrintMessage(STRING_PREFIX, message); }
 
-    public static void log(boolean message) { createAndPrintMessage(PRIMITIVE, message); }
+    public static void log(boolean message) { createAndPrintMessage(PRIMITIVE_PREFIX, message); }
 
-    public static void log(Object message) { createAndPrintMessage(REFERENCE, message); }
+    public static void log(Object message) { createAndPrintMessage(REFERENCE_PREFIX, message); }
 
     private static void createAndPrintMessage(String nameType, Object message) {
         String fullMessage = nameType + message;
