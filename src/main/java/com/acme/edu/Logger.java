0a7d2226a306f6831448007b3b643cc68f94da7c
keleborn <ivanenko-aleksei@yandex.ru>
2020-09-03 15:39:45 +0300

    Prefix

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,37 @@
 package com.acme.edu;
 
 public class Logger {
+    final static String
+            PREFIX_CHAR = "char: ",
+            PREFIX_STRING = "string: ",
+            PREFIX_REFERENCE = "reference: ",
+            PREFIX_PRIMITIVE = "primitive: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        directToOutput(PREFIX_PRIMITIVE + message);
+    }
+
+    public static void log(char message) {
+        directToOutput(PREFIX_CHAR + message);
+    }
+
+    public static void log(boolean message) {
+        directToOutput(PREFIX_PRIMITIVE + message);
+    }
+
+    public static void log(Object message) {
+        directToOutput(PREFIX_REFERENCE + message);
+    }
+
+    public static void log(String message) {
+        directToOutput(PREFIX_STRING + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        directToOutput(PREFIX_PRIMITIVE + message);
+    }
+
+    static void directToOutput (String outputString){
+        System.out.println(outputString);
     }
 }
