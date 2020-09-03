7e7963c744aede1f50083ca0fb06777eae205d97
Alexander Senin <aaasenin@gmail.com>
2020-09-03 14:59:49 +0300

    codestyle fixes

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,32 +1,22 @@
 package com.acme.edu;
 
 public class Logger {
-    private static String PRIMITIVE = "primitive: ";
-    private static String CHAR = "char: ";
-    private static String STRING = "string: ";
-    private static String REFERENCE = "reference: ";
+    private static final String PRIMITIVE = "primitive: ";
+    private static final String CHAR = "char: ";
+    private static final String STRING = "string: ";
+    private static final String REFERENCE = "reference: ";
 
     private static void print(String prefix, Object postfix) { System.out.println(prefix + postfix); }
 
     public static void log(int message) { print(PRIMITIVE, message); }
 
-    public static void log(byte message) {
-        print(PRIMITIVE, message);
-    }
+    public static void log(byte message) { print(PRIMITIVE, message); }
 
-    public static void log(char message) {
-        print(CHAR, message);
-    }
+    public static void log(char message) { print(CHAR, message); }
 
-    public static void log(String message) {
-        print(STRING, message);
-    }
+    public static void log(String message) { print(STRING, message); }
 
-    public static void log(boolean message) {
-        print(PRIMITIVE, message);
-    }
+    public static void log(boolean message) { print(PRIMITIVE, message); }
 
-    public static void log(Object message) {
-        print(REFERENCE, message);
-    }
+    public static void log(Object message) { print(REFERENCE, message); }
 }
