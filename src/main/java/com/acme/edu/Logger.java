5e1677a088753229c7bdef83b8f6d74ed3d3631a
gumerova-albina <gumerova.albina38@gmail.com>
2020-09-03 15:00:01 +0300

    Add messageType enum and separator var

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,31 +1,49 @@
 package com.acme.edu;
 
 public class Logger {
+    private static String separator = ": ";
+
     public static void log(int message) {
-        printMessageWithType("primitive: " + message);
+        printMessageWithType(MessageType.PRIMITIVE.getType() + separator + message);
     }
 
     public static void log(byte message) {
-        printMessageWithType("primitive: " + message);
+        printMessageWithType(MessageType.PRIMITIVE.getType() + separator + message);
     }
 
     public static void log(char message) {
-        printMessageWithType("char: " + message);
+        printMessageWithType(MessageType.CHAR.getType() + separator + message);
     }
 
     public static void log(String message) {
-        printMessageWithType("string: " + message);
+        printMessageWithType(MessageType.STRING.getType() + separator + message);
     }
 
-    public static void log(boolean message){
-        printMessageWithType("primitive: " + message);
+    public static void log(boolean message) {
+        printMessageWithType(MessageType.PRIMITIVE.getType() + separator + message);
     }
 
-    public static void log(Object message){
-        printMessageWithType("reference: " + message);
+    public static void log(Object message) {
+        printMessageWithType(MessageType.REFERENCE.getType() + separator + message);
     }
 
-    private static void printMessageWithType(String message){
+    private static void printMessageWithType(String message) {
         System.out.println(message);
+    }
+}
+
+enum MessageType {
+    PRIMITIVE("primitive"),
+    CHAR("char"),
+    STRING("string"),
+    REFERENCE("reference");
+
+    private String type;
+    MessageType(String type) {
+        this.type = type;
+    }
+
+    String getType() {
+        return type;
     }
 }
