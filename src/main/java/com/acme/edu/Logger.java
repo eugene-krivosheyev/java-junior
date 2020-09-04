0e7e25ee93642a3503777fd506bc80669fc2a43c
keleborn <ivanenko-aleksei@yandex.ru>
2020-09-04 09:21:20 +0300

    first test ok (try to understand how to catch the last log)

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,14 +1,40 @@
 package com.acme.edu;
 
 public class Logger {
-    private final static String
-            PREFIX_CHAR = "char: ",
-            PREFIX_STRING = "string: ",
-            PREFIX_REFERENCE = "reference: ",
-            PREFIX_PRIMITIVE = "primitive: ";
+    private final static String PREFIX_CHAR = "char ";
+    private final static String PREFIX_STRING = "";
+    private final static String PREFIX_REFERENCE = "reference ";
+    private final static String PREFIX_PRIMITIVE = "";
+    private static String lastStringValue = "";
+    private static int lastIntValue;
+    private static String lastType = "";
+    private static int counter = 1;
 
+    private static boolean isEqualLastType(String type){
+        return type.equals(lastType);
+    }
     public static void log(int message) {
-        writeMessage(PREFIX_PRIMITIVE + message);
+        if(Integer.MAX_VALUE == message){
+            if(isEqualLastType("int")){
+                writeMessage(PREFIX_PRIMITIVE + lastIntValue);
+            }
+            System.out.println(message);
+            lastIntValue = 0;
+            lastType = "";
+        }
+        else if(isEqualLastType("int")){
+            lastIntValue += message;
+        }
+        else{
+//            if(counter != 1){
+//                lastStringValue += " (x" + counter + ")";
+//            }
+//            if(!lastStringValue.equals("")){
+//                writeMessage(lastStringValue);
+//            }
+            lastIntValue = message;
+            lastType = "int";
+        }
     }
 
     public static void log(char message) {
@@ -19,11 +45,15 @@ public class Logger {
         writeMessage(PREFIX_PRIMITIVE + message);
     }
 
-    public static void log(Object message) {
-        writeMessage(PREFIX_REFERENCE + message);
-    }
-
     public static void log(String message) {
+        if(!isEqualLastType("str")){
+//            lastStringValue += message;
+//        }
+//        else {
+            if(isEqualLastType("int")){
+                writeMessage(PREFIX_PRIMITIVE + lastIntValue);
+            }
+        }
         writeMessage(PREFIX_STRING + message);
     }
 
@@ -31,7 +61,17 @@ public class Logger {
         writeMessage(PREFIX_PRIMITIVE + message);
     }
 
+    public static void log(Object message) {
+        writeMessage(PREFIX_REFERENCE + message);
+    }
+
+
     static void writeMessage(String outputString){
         System.out.println(outputString);
     }
+
+    private static boolean isFirstValue(String  message) {
+        return message.equals("");
+    }
+
 }
