8995a9b1a1a87b3b6ea0bf8764a999bc0d6566e6
keleborn <ivanenko-aleksei@yandex.ru>
2020-09-04 10:42:26 +0300

    String and byte adding (test works only with special end-logging)

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -6,9 +6,11 @@ public class Logger {
     private final static String PREFIX_REFERENCE = "reference ";
     private final static String PREFIX_PRIMITIVE = "";
     private static String lastStringValue = "";
-    private static int lastIntValue;
+    private static String countedStringValue = "";
+    private static int lastIntValue = 0;
     private static String lastType = "";
     private static int counter = 1;
+    private static byte lastByteValue = 0;
 
     private static boolean isEqualLastType(String type){
         return type.equals(lastType);
@@ -16,7 +18,7 @@ public class Logger {
     public static void log(int message) {
         if(Integer.MAX_VALUE == message){
             if(isEqualLastType("int")){
-                writeMessage(PREFIX_PRIMITIVE + lastIntValue);
+                writeMessage(lastType);
             }
             System.out.println(message);
             lastIntValue = 0;
@@ -26,12 +28,6 @@ public class Logger {
             lastIntValue += message;
         }
         else{
-//            if(counter != 1){
-//                lastStringValue += " (x" + counter + ")";
-//            }
-//            if(!lastStringValue.equals("")){
-//                writeMessage(lastStringValue);
-//            }
             lastIntValue = message;
             lastType = "int";
         }
@@ -46,32 +42,68 @@ public class Logger {
     }
 
     public static void log(String message) {
-        if(!isEqualLastType("str")){
-//            lastStringValue += message;
-//        }
-//        else {
-            if(isEqualLastType("int")){
-                writeMessage(PREFIX_PRIMITIVE + lastIntValue);
+        if(isEqualLastType("str")){
+            if(countedStringValue.equals(message)){
+                counter++;
+                if(counter == 2 && !countedStringValue.equals(lastStringValue)) {
+                        writeMessage(lastType);
+                        lastStringValue = countedStringValue;
+                }
+            }
+            else{
+                if(counter == 1){
+                    if(!countedStringValue.equals(lastStringValue)){
+                        lastStringValue += countedStringValue;
+                    }
+                }
+                else{
+                    lastStringValue += " (x" + counter + ")";
+                    writeMessage(lastType);
+                    lastStringValue = message;
+                    counter = 1;
+                }
+                countedStringValue = message;
             }
         }
-        writeMessage(PREFIX_STRING + message);
+        else {
+            counter = 1;
+            writeMessage(lastType);
+            countedStringValue = message;
+            lastStringValue = message;
+        }
     }
 
     public static void log(byte message) {
-        writeMessage(PREFIX_PRIMITIVE + message);
+        if(Byte.MAX_VALUE == message){
+            if(isEqualLastType("byte")){
+                writeMessage(PREFIX_PRIMITIVE + lastByteValue);
+            }
+            System.out.println(message);
+            lastByteValue = 0;
+            lastType = "";
+        }
+        else if(isEqualLastType("byte")){
+            lastByteValue += message;
+        }
+        else{
+            writeMessage(lastType);
+            lastByteValue = message;
+            lastType = "byte";
+        }
     }
 
     public static void log(Object message) {
         writeMessage(PREFIX_REFERENCE + message);
     }
 
-
-    static void writeMessage(String outputString){
+    static void writeMessage(String lastType){
+        String outputString = "";
+        switch (lastType){
+            case "int": outputString += lastIntValue; break;
+            case "byte": outputString += lastByteValue; break;
+            case "str": outputString = lastStringValue; break;
+        }
         System.out.println(outputString);
-    }
-
-    private static boolean isFirstValue(String  message) {
-        return message.equals("");
     }
 
 }
