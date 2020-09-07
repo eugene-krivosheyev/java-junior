ccf009f6a560b4e830c7f367e9ec182103425c57
alengul <alengul1@mail.ru>
2020-09-08 00:14:11 +0300

    changed stringMessage and added blank Char/Reference

---
+++ src/main/java/com/acme/edu/message/CharMessage.java
@@ -0,0 +1,12 @@
+package com.acme.edu.message;
+
+import java.util.ArrayList;
+
+public class CharMessage implements AbstractMessage{
+    static public String PREFIX = "char: ";
+
+    @Override
+    public void prepareMessage(ArrayList<Object> listOfLog) {
+
+    }
+}
---
+++ src/main/java/com/acme/edu/message/ReferenceMessage.java
@@ -0,0 +1,12 @@
+package com.acme.edu.message;
+
+import java.util.ArrayList;
+
+public class ReferenceMessage implements AbstractMessage{
+    static public String PREFIX = "reference: ";
+
+    @Override
+    public void prepareMessage(ArrayList<Object> listOfLog) {
+
+    }
+}
