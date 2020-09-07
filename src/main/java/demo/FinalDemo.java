5315159acc4b382d3f44b6355b04f0b2c58ca818
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-07 10:56:36 +0300

    Add demo for final variables.

---
+++ src/main/java/demo/FinalDemo.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class FinalDemo {
+    public static final String MY_CONST;
+    public final String objectConst; //blank final
+
+    static {
+        MY_CONST = "1";
+    }
+
+    public FinalDemo() {
+        this.objectConst = "objectConst";
+    }
+
+    public static void main(final String[] args) {
+        final int intConst = 1;
+        //intConst++;
+    }
+}
