6080e62f2b0c022fabd8db3546b5f601d53fc37c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-08 10:51:33 +0300

    Add polymorphic collection demo.

--- src/main/java/demo/PolymorphismDemo.java
+++ src/main/java/demo/PolymorphismDemo.java
@@ -5,13 +5,20 @@ public class PolymorphismDemo {
         Object o = new Insure(); // new -> creational components
         System.out.println(o.toString());
 
-
+        Object[] array = {
+                new String("abc"),
+                new Double("4.0"),
+                new Insure()
+        };
+        for (Object current : array) {
+            System.out.println(current.toString());
+        }
     }
 }
 
 class Insure {
     @Override
     public String toString() {
-        return "Я же говорил!!!!!";
+        return "AAAAA!!!!! Осень!!!!!!";
     }
 }
