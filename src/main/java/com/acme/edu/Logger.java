d41ab0c6c80bed1950f21e26ac7e187b45094699
ArseniyIFMO <vatrushka.96@mail.ru>
2020-09-08 16:14:43 +0300

    iteration2 fix

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -4,69 +4,10 @@ import com.acme.edu.message.BoolMessage;
 import com.acme.edu.message.Message;
 
 public class Logger {
-     static LoggerController loggerController = new LoggerController();
-
-
-    private static void printMessage(Message message) {
-        System.out.println(message);
-    }
-
-    private static void printBuffer() {
-        /*
-        switch (last) {
-            case INTEGER:
-                printMessage(primitiveType + savedInt);
-                break;
-            case BYTE:
-                printMessage(primitiveType + savedByte);
-                break;
-            case STRING:
-                printMessage(stringType + savedString);
-                if(stringCounter != 1){
-                    System.out.print(" (x" + stringCounter + ")");
-                }
-                break;
-        }
-        System.out.println();
-         */
-    }
-
-    private static void resetBuffer() {
-        savedString = "";
-        savedInt = 0;
-        savedByte = 0;
-        stringCounter = 0;
-        last = null;
-    }
-
-    private static  String getStringFromArray(int [] arr) {
-        StringBuilder strForPrint = new StringBuilder("{");
-        for (int i = 0; i < arr.length; i++) {
-            strForPrint.append(arr[i]);
-            if (i< arr.length -1) { strForPrint.append(", ");}
-        }
-        strForPrint.append("}");
-        return strForPrint.toString();
-    }
-    static int savedInt = 0;
-    static String savedString = "";
-    static byte savedByte = 0;
-    static types last = null;
-    static int stringCounter = 0;
-
-    enum types {
-        INTEGER ,
-        BYTE ,
-        STRING
-    }
-
-    public static void main(String[] args) {
-
-    }
+    static LoggerController loggerController = new LoggerController();
 
     public static void flush() {
-        printBuffer();
-        resetBuffer();
+        loggerController.flush();
     }
 
     public static void log(int message) {
