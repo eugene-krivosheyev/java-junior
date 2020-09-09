88ed0d47c311bfe2c2af943065d634c9bb31b772
Your Name <you@example.com>
2020-09-09 15:22:56 +0300

    unit tests for IntCommand

---
+++ src/test/java/com/acme/edu/command/IntCommandTest.java
@@ -0,0 +1,62 @@
+package com.acme.edu.command;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IntCommandTest{
+
+    private IntCommand stateCommand;
+
+    @Before
+    public void setUp() throws Exception {
+        stateCommand = new IntCommand(1);
+    }
+
+    @Test
+    public void shouldAllowFlushWhenAnotherType() {
+        //region given
+        StringCommand otherCommand = new StringCommand("str");
+        //endregion
+
+        //region when
+        boolean checkResult = stateCommand.checkFlush(otherCommand);
+        //endregion
+
+        //region then
+        assertThat(checkResult).isEqualTo(true);
+        //endregion
+    }
+
+    @Test
+    public void shouldAllowFlushWhenIntegerOverflow() {
+        //region given
+        IntCommand otherCommand = new IntCommand(Integer.MAX_VALUE);
+        //endregion
+
+        //region when
+        boolean checkResult = stateCommand.checkFlush(otherCommand);
+        //endregion
+
+        //region then
+        assertThat(checkResult).isEqualTo(true);
+        //endregion
+    }
+
+    @Test
+    public void shouldNotAllowFlushWhenNoIntegerOverflow() {
+        //region given
+        IntCommand otherCommand = new IntCommand(0);
+        //endregion
+
+        //region when
+        boolean checkResult = stateCommand.checkFlush(otherCommand);
+        //endregion
+
+        //region then
+        assertThat(checkResult).isEqualTo(false);
+        //endregion
+    }
+}
