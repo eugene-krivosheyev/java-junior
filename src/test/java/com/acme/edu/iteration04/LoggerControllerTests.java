ee4afed5413dfbdaadc9c57449135cfa6b094530
linlit <alinkondakova@gmail.com>
2020-09-09 16:30:59 +0300

    added saver call test

--- src/test/java/com/acme/edu/iteration04/LoggerControllerTests.java
+++ src/test/java/com/acme/edu/iteration04/LoggerControllerTests.java
@@ -1,19 +1,17 @@
-package com.acme.edu.iteration05;
+package com.acme.edu.iteration04;
 
 import com.acme.edu.LoggerController;
 import com.acme.edu.SysoutCaptureAndAssertionAbility;
 import com.acme.edu.message.AbstractMessage;
-import com.acme.edu.message.IntMessage;
 import com.acme.edu.saver.LogSaver;
 import com.acme.edu.saver.Saver;
-import demo.ooad.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
-public class LoggerFacadeTests implements SysoutCaptureAndAssertionAbility {
+public class LoggerControllerTests implements SysoutCaptureAndAssertionAbility {
 
     private LoggerController sut;
 
@@ -33,10 +31,25 @@ public class LoggerFacadeTests implements SysoutCaptureAndAssertionAbility {
     @Test
     public void shouldSaveMessageWhenFlushCalled() {
         AbstractMessage mock = mock(AbstractMessage.class);
+
         when(mock.getPreparedMessage()).thenReturn("hahaha");
+
         sut.log(mock);
         sut.flushStart();
+
         assertSysoutContains("hahaha");
+    }
+
+    @Test
+    public void shouldCallSaveWhenFlushCalled() {
+        AbstractMessage messageMock = mock(AbstractMessage.class);
+        Saver saverMock = mock(LogSaver.class);
+
+        sut = new LoggerController(saverMock);
+        sut.log(messageMock);
+        sut.flushStart();
+
+        verify(saverMock).save(messageMock);
     }
 
 }
