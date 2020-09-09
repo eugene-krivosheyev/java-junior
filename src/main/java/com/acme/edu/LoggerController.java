ca418911460cf23799789c0ca46c5675843bea69
Dasha <darya_gavrilova1999@mail.ru>
2020-09-09 11:27:38 +0300

    Global refactoring part 4 Iter01 tests  passed.

--- src/main/java/com/acme/edu/LoggerController.java
+++ src/main/java/com/acme/edu/LoggerController.java
@@ -6,7 +6,7 @@ import com.acme.edu.save.Saver;
 public class LoggerController {
 
     private final Saver saver;
-    private Command currentCommand = new Command() {
+    private Command currentCommand; /* = new Command() {
         @Override
         public String decorate() {
             return null;
@@ -19,39 +19,24 @@ public class LoggerController {
         public Command reduce(Command cmd) {
             return cmd;
         }
-    };
+    };*/
 
     public LoggerController(Saver saver) {
         this.saver = saver;
     }
 
     public void flushBuffers() {
-        /*switch (PREVIOUS_TYPE) {
-            case "String":
-                if (STRING_BUFFER > 1) {
-                    printLogMessage(StringCommand.STRING_PREFIX + PREVIOUS_STRING + " (x" + STRING_BUFFER + ")");
-                } else
-                    printLogMessage(StringCommand.STRING_PREFIX + PREVIOUS_STRING);
-                flushString();
-                break;
-            case "Integer":
-                printLogMessage(PrimitiveCommand.PRIMITIVE_PREFIX + INT_BUFFER);
-                INT_BUFFER = 0;
-                break;
-            case "Byte":
-                printLogMessage(PrimitiveCommand.PRIMITIVE_PREFIX + BYTE_BUFFER);
-                BYTE_BUFFER = 0;
-                break;
-        }*/
         saver.save(currentCommand);
+        currentCommand = null;
     }
 
     public void log(Command cmd) {
-        if (currentCommand.isSameType(cmd)) {
+        if (currentCommand != null && currentCommand.isSameType(cmd)) {
             currentCommand = currentCommand.reduce(cmd);
         }
         else {
-            saver.save(currentCommand);
+            if (currentCommand != null)
+                saver.save(currentCommand);
             currentCommand = cmd;
         }
     }
