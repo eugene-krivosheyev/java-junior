7c49a9babce4db69f49b3588e6631255fe573239
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-10 11:53:48 +0300

    Add nested class demo.

--- src/main/java/demo/innernestedlocal/InnerNestedLocalDemo.java
+++ src/main/java/demo/innernestedlocal/InnerNestedLocalDemo.java
@@ -7,17 +7,35 @@ public class InnerNestedLocalDemo {
         Outer.StaticInner o1 = new Outer.StaticInner();
         System.out.println(o1.mutateAndGetOuterStaticState());
         System.out.println(o1.mutateAndGetOuterStaticState());
+
+        final Outer outer = new Outer();
+        Outer.Nested nested = outer.new Nested();
+        outer.new Nested();
+        outer.new Nested();
+
+        final Outer.Nested nested1 = new Outer().new Nested();
+
     }
 }
 
 class Outer {
     private static int outerStaticState;
+    private int outerObjectState;
 
     static class StaticInner {
         public int mutateAndGetOuterStaticState() {
             int outerStaticState = 0;
             Outer.outerStaticState++;
             return outerStaticState;
+        }
+    }
+
+    class Nested {
+        private int nestedObjectState;
+
+        public int mutateAndGetOuterObjectState() {
+            int outerObjectState = 0;
+            return Outer.this.outerObjectState++;
         }
     }
 }
