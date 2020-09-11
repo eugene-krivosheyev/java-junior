f33934efe5c8ec8f6394a7835b5c0b0c3614a616
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-11 17:57:45 +0300

    Add wrappers demo.

---
+++ src/main/java/demo/WrapperDemo.java
@@ -0,0 +1,33 @@
+package demo;
+
+import com.sun.xml.internal.xsom.XSUnionSimpleType;
+
+public class WrapperDemo {
+    public static void main(String[] args) {
+        String s = "abc"; //
+        Integer i = new Integer(1); // auto boxing
+        Integer ii = 1;
+        Integer iii = 1;
+        System.out.println(i + "  " + ii + " " + iii);
+        System.out.println(i == ii);
+        System.out.println(ii == iii);
+
+        Integer i1 = new Integer(iii); //127; //new In
+        Integer i2 = 127;
+        System.out.println(i1 == i2);
+
+        Integer i3 = 9;
+        int i4 = i3;
+        i1.longValue();
+
+        Integer.parseInt("123");
+        Double.parseDouble("1.1");
+        new Integer("123");
+        new Double("1.1");
+
+        System.out.println(i1 + i2);
+        while (true) {
+            i1++;
+        }
+    }
+}
