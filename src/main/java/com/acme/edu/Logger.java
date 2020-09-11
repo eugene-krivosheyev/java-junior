f52208bb1e33820427f64f875f920cdb188fc6eb
dfrant <defrant98@mail.ru>
2020-09-11 10:53:51 +0300

    Replace ConsoleSaver with lambda

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -3,12 +3,10 @@ package com.acme.edu;
 import com.acme.edu.command.IntCommand;
 import com.acme.edu.command.StringCommand;
 import com.acme.edu.controller.LoggerController;
-import com.acme.edu.saver.ConsoleSaver;
 
 public class Logger {
-    //private static byte byteAccumulator = (byte) 0;
 
-    private static LoggerController loggerController = new LoggerController(new ConsoleSaver());
+    private static final LoggerController loggerController = new LoggerController(System.out::println);
 
     public static void flush() {
         loggerController.flush();
@@ -22,94 +20,4 @@ public class Logger {
         loggerController.log(new StringCommand(message));
     }
 
-//TODO: Implement when customer will ask for these types
-//
-//    public static void log(byte message) {
-//        byte diff = (byte) checkNumberAccumulatorOverflow(Byte.MAX_VALUE, byteAccumulator, message);
-//        if (Byte.MAX_VALUE - byteAccumulator < message) {
-//            byteAccumulator += diff;
-//            message -= diff;
-//            flush();
-//        }
-//        byteAccumulator += message;
-//        if (previousType != null && !previousType.equals(Type.BYTE)) {
-//            flush();
-//        }
-//        setPreviousPrefixAndType(Prefix.PRIMITIVE_PREFIX, Type.BYTE);
-//    }
-
-//    public static void log(char message) {
-//        writeMessage(Prefix.CHAR_PREFIX + message);
-//    }
-
-//    public static void log(boolean message) {
-//        writeMessage(Prefix.PRIMITIVE_PREFIX + message);
-//    }
-//
-//    public static void log(Object message) {
-//        writeMessage(Prefix.REFERENCE_PREFIX + message);
-//    }
-//
-//    public static void log(int[] message) {
-//        writeArray(message);
-//    }
-//
-//    public static void log(int[][] message) {
-//        writeMatrix(message);
-//    }
-
-//    private static String clearStringBuffer() {
-//        String buffer = stringAccumulator;
-//        if (stringDuplicates != 0) {
-//            buffer += " (x" + ++stringDuplicates + ")";
-//        }
-//        stringAccumulator = "";
-//        stringDuplicates = 0;
-//        return buffer;
-//    }
-
-//    private static String clearByteBuffer() {
-//        String buffer = Byte.toString(byteAccumulator);
-//        byteAccumulator = (byte) 0;
-//        return buffer;
-//    }
-
-//    private static void writeMessage(String message) {
-//        System.out.println(message);
-//    }
-
-//    private static void writeArray(int[] array) {
-//        writeMessage(Prefix.ARRAY_PREFIX + buildArrayStr(array));
-//    }
-
-//    private static String buildArrayStr(int[] array) {
-//        StringBuilder stringBuilder = new StringBuilder("{");
-//        for (int i = 0; i < array.length - 1; i++) {
-//            stringBuilder.append(array[i] + ", ");
-//        }
-//        stringBuilder.append(array[array.length - 1]);
-//        stringBuilder.append("}");
-//        return stringBuilder.toString();
-//    }
-
-//    private static void writeMatrix(int[][] matrix) {
-//        writeMessage(Prefix.MATRIX_PREFIX + "{");
-//        for (int[] array : matrix) {
-//            writeMessage(buildArrayStr(array));
-//        }
-//        writeMessage("}");
-//    }
-//
-//    private static int checkNumberAccumulatorOverflow(int maxValue, int accumulator, int message) {
-//        int diff = 0;
-//        if (maxValue - accumulator < message) {
-//            diff = message - maxValue;
-//        }
-//        return diff;
-//    }
-
-//    private static void setPreviousPrefixAndType(String prefix, String type) {
-//        previousPrefix = prefix;
-//        previousType = type;
-//    }
 }
