cf39bb37a7b69332f85450c988c628996e2cdfa0
alengul <alengul1@mail.ru>
2020-09-11 11:05:16 +0300

    added Lambda and LoggerControllerException

---
+++ src/main/java/com/acme/edu/exception/AbstractException.java
@@ -0,0 +1,7 @@
+package com.acme.edu.exception;
+
+public abstract class AbstractException extends Exception {
+    public AbstractException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/acme/edu/exception/LoggerControllerException.java
@@ -0,0 +1,7 @@
+package com.acme.edu.exception;
+
+public class LoggerControllerException extends AbstractException {
+    public LoggerControllerException(String message) {
+        super(message);
+    }
+}
