d8a9142e9503d7951aa7e16f06bd13c46c664def
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-14 17:03:58 +0300

    Add stream API demo.

---
+++ src/main/java/demo/StreamApiDemo.java
@@ -0,0 +1,44 @@
+package demo;
+
+
+import com.sun.tools.javac.util.List;
+
+import java.util.stream.Collectors;
+
+public class StreamApiDemo {
+    public static void main(String[] args) {
+        //Arrays.asList(1,2,3,4,5)
+        //range(0, 5)
+        System.out.println(
+            List.of(1,2,3,4,5).stream()
+                .map(Dog::new)
+                .filter(dog -> dog.getId() > 2)
+                .sorted((d1,d2) -> d2.getId() - d1.getId())
+//                    .collect(Collectors.toList())
+//                    .forEach(System.out::println)
+//                    .reduce()
+//                    .max() // .min()
+                    .findAny() //findAny()
+//                        .get()
+//                        .orElse(new Dog(0))
+                        .orElseThrow(RuntimeException::new)
+        );
+    }
+}
+
+class Dog {
+    private int id;
+
+    public Dog(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(id);
+    }
+}
