250ebaed8f73276c79b80a86345e85ce873dee75
Albina Isakova <s-id@mail.ru>
2020-09-15 15:21:05 +0300

    Fix fileLoggerSaver

---
+++ src/test/java/com/acme/edu/test/FileLoggerSaverTest.java
@@ -0,0 +1,33 @@
+package com.acme.edu.test;
+
+import com.acme.edu.SysoutCaptureAndAssertionAbility;
+import com.acme.edu.exceptions.FileLoggerSaverException;
+import com.acme.edu.saver.ConsoleLoggerSaver;
+import com.acme.edu.saver.FileLoggerSaver;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileLoggerSaverTest implements SysoutCaptureAndAssertionAbility {
+    private FileLoggerSaver saver;
+
+    @Before
+    public void setUp() {
+        saver = new FileLoggerSaver();
+    }
+
+    @Test
+    public void messageIsPrintedToConsoleWhenSave() throws FileLoggerSaverException, IOException {
+        saver.save("H1!!!");
+
+        List<String> lines = Files.readAllLines(Paths.get("test.txt"));
+        lines.forEach(System.out::println);
+
+        assertSysoutEquals("H1!!!");
+    }
+
+}
