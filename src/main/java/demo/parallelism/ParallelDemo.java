40d4409cdff3b5c27db547f569a34054b2341c2e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-16 12:41:50 +0300

    Add class thread creation demo.

--- src/main/java/demo/parallelism/ParallelDemo.java
+++ src/main/java/demo/parallelism/ParallelDemo.java
@@ -2,7 +2,7 @@ package demo.parallelism;
 
 public class ParallelDemo {
     public static void main(String[] args) {
-        final Thread thread = new Worker(); //NEW
+        final Thread thread = new Thread(new Worker()); //NEW
         thread.start(); // -> run
         while (true) {
             System.out.println("main");
@@ -10,7 +10,7 @@ public class ParallelDemo {
     }
 }
 
-class Worker extends Thread {
+class Worker implements Runnable {
     @Override
     public void run() {
         while (true) {
