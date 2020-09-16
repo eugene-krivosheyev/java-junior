f6950964349eaa7b1a5013319ef1a14f20eb6fcb
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-16 14:59:25 +0300

    Add parallel demo.

--- src/main/java/demo/parallelism/ParallelDemo.java
+++ src/main/java/demo/parallelism/ParallelDemo.java
@@ -1,17 +1,48 @@
 package demo.parallelism;
 
-public class ParallelDemo {
-    public static void main(String[] args) {
-        final Thread thread = new Thread(() -> {
-            while (true) {
-                System.out.println("Thread-1");
-            }
-        }); //NEW
+import com.sun.codemodel.internal.JWhileLoop;
 
-        thread.start(); // -> run
-        while (true) {
-            System.out.println("main");
-        }
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ParallelDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread someThread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                System.out.println("thread-01");
+            }
+        });
+        someThread.setDaemon(true);
+        someThread.start();
+        someThread.getName(); //Thread-01
+        someThread.setPriority(Thread.MAX_PRIORITY);
+
+        someThread.isAlive();
+        someThread.getState();
+
+        someThread.suspend();
+        someThread.resume();
+        someThread.stop();
+        someThread.interrupt(); //someThread.isInterrupted();
+
+        someThread.join();
+        Thread.yield();
+
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        pool.execute(() -> {
+            System.out.println(Thread.currentThread());
+        });
+        pool.execute(() -> { /*while (true) ;*/ });
+        pool.execute(() -> System.out.println("thread-3"));
+        pool.execute(() -> System.out.println("thread-4"));
+        pool.execute(() -> System.out.println("thread-5"));
+
+        pool.shutdown();
+//        final List<Runnable> alreadyQueued = pool.shutdownNow();
+//        pool.awaitTermination(1, TimeUnit.SECONDS);
     }
 }
 
