8508597c73205d4db752b9ed269ec8867836b88c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-16 12:43:13 +0300

    Add anonymous class thread creation demo.

--- src/main/java/demo/parallelism/ParallelDemo.java
+++ src/main/java/demo/parallelism/ParallelDemo.java
@@ -2,7 +2,15 @@ package demo.parallelism;
 
 public class ParallelDemo {
     public static void main(String[] args) {
-        final Thread thread = new Thread(new Worker()); //NEW
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("Thread-1");
+                }
+            }
+        }); //NEW
+
         thread.start(); // -> run
         while (true) {
             System.out.println("main");
@@ -10,11 +18,3 @@ public class ParallelDemo {
     }
 }
 
-class Worker implements Runnable {
-    @Override
-    public void run() {
-        while (true) {
-            System.out.println("Thread-1");
-        }
-    }
-}
