b0240306491e3e088a72dbcd75610c14d8187bd7
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-09-16 15:54:58 +0300

    Add DATA RACE elimination demo.

---
+++ src/main/java/demo/parallelism/ConcurrencyDemo.java
@@ -0,0 +1,53 @@
+package demo.parallelism;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static java.util.stream.IntStream.range;
+
+public class ConcurrencyDemo {
+    public static void main(String[] args) {
+        final Counter counter = new Counter(0);
+        ExecutorService pool = newFixedThreadPool(100);
+        range(0, 100_000).forEach(e -> pool.submit(counter::increment));
+        pool.shutdown();
+
+        System.out.println(counter.getState());
+    }
+}
+
+class Counter {
+    private Object monitor = new Object();
+    private Lock lock = new ReentrantLock(false);
+    private int state;
+
+    public Counter(int state) {
+        this.state = state;
+    }
+
+    public void increment() {
+        synchronized (monitor) {
+            state++;
+        }
+    }
+
+    public int getState() {
+        int result;
+        lock.tryLock();
+
+        lock.lock();
+        try {
+            result = state;
+        } finally {
+            lock.unlock();
+        }
+
+        return result;
+    }
+}
