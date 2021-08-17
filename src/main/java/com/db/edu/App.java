71bfd6aa082149c54b4fd3d39bc3eab6d11821be
Nikita Baranov <baranovnikita18@gmail.com>
2021-08-17 17:28:18 +0300

    Minor fixes

--- src/main/java/com/db/edu/App.java
+++ src/main/java/com/db/edu/App.java
@@ -1,9 +1,5 @@
 package com.db.edu;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- src/test/java/com/db/edu/AppTest.java
+++ src/test/java/com/db/edu/AppTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
