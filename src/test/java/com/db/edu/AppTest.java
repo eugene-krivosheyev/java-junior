350042dc56975085382c783301039cf495346efb
Vsevolod Fedorov <fsdoc151@mail.ru>
2021-08-17 16:33:36 +0300

    Updated pom.xml + AppTest.java

--- src/test/java/com/db/edu/AppTest.java
+++ src/test/java/com/db/edu/AppTest.java
@@ -1,8 +1,6 @@
 package com.db.edu;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -14,5 +12,8 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    private void assertTrue(boolean b) {
     }
 }
