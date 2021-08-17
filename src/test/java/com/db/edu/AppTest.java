67ee8d3252e20299ea9f3d4dcdba647ffb328e2c
sofyaaleksandrova <asalektt@gmail.com>
2021-08-17 17:06:57 +0300

    Added a new pom.xml file; Fixed some issues with junit

--- src/test/java/com/db/edu/AppTest.java
+++ src/test/java/com/db/edu/AppTest.java
@@ -1,8 +1,7 @@
 package com.db.edu;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
