d0ea984ca3e6f3e83f4b4cb4cae008a5ce51d368
DlfMinn <dlfminn675@gmail.com>
2021-08-17 16:50:14 +0300

    JUnit4 updated to JUnit5

--- src/main/java/com/db/edu/App.java
+++ src/main/java/com/db/edu/App.java
@@ -1,6 +1,6 @@
 package com.db.edu;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ src/test/java/com/db/edu/AppTest.java
@@ -0,0 +1,12 @@
+package com.db.edu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest {
+    @Test
+    public void test(){
+        Assertions.assertEquals(true, true);
+    }
+}
