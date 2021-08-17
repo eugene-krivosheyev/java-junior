ba3aa39fadc866eef6cf483f0ecaca6edb55e163
DlfMinn <dlfminn675@gmail.com>
2021-08-17 16:04:41 +0300

    Re-do Maven Project

---
+++ src/test/java/com/db/edu/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.db.edu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void test(){
+        assertEquals(true, true);
+    }
+}
