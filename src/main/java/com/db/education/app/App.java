85d189702909ea560c7540a9e360b27943c718fe
lobakov <lobakov@gmail.com>
2021-08-17 16:47:05 +0300

    Added test

--- src/main/java/com/db/education/app/App.java
+++ src/main/java/com/db/education/app/App.java
@@ -1,10 +1,8 @@
 package com.db.education.app;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class App {
 
     public static void main(String[] args) {
-        assertTrue(true);
+        System.out.println("TRUE");
     }
 }
---
+++ src/test/java/com/db/education/app/AppTest.java
@@ -0,0 +1,13 @@
+package com.db.education.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void alwaysReturnTrue() {
+        assertTrue(true);
+    }
+}
