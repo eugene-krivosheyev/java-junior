18c1977ace6a5bfea39282bfd13945f5284c149b
mech-Maks <maksim_28_11_1999@mail.ru>
2021-08-17 16:41:29 +0300

    added junit 5

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,7 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
