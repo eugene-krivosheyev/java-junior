be023c95b5d55a0fcac5426ec37f430ab402d2b8
hexecute <myaremchuk@yandex.ru>
2021-08-17 16:37:47 +0300

    changed junit version

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,17 +1,15 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-  /**
-   * Rigorous Test :-)
-   */
   @Test
   public void shouldAnswerWithTrue() {
     assertTrue(true);
