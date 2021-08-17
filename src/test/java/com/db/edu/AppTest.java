92fea14efcc5f49f209b5d7993531cdbc0904944
Ivan Filimonov <ivanf2000@yandex.ru>
2021-08-17 16:50:08 +0300

    mvn

---
+++ src/test/java/com/db/edu/AppTest.java
@@ -0,0 +1,17 @@
+package com.db.edu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
+}
