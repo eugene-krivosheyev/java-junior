aac25407088a52855ea6089729e89764a6dacece
lobakov <lobakov@gmail.com>
2021-08-17 16:00:36 +0300

    Set up clean maven project

---
+++ src/main/java/com/db/education/app/App.java
@@ -0,0 +1,10 @@
+package com.db.education.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class App {
+
+    public static void main(String[] args) {
+        assertTrue(true);
+    }
+}
