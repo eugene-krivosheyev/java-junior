9f87e70b2dc901a41b04fc8c15a555647a197661
mech-Maks <maksim_28_11_1999@mail.ru>
2021-08-17 16:49:42 +0300

    added junit 5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,8 +4,7 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -6,8 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
