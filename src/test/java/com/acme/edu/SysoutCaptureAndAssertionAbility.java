0410cdcb89755871c9a0d40558b44f9377279937
Vasily Dubinin <vasily.dubinin@gmail.com>
2021-08-19 14:28:25 +0300

    Logger iteration1

---
+++ src/test/java/com/acme/edu/SysoutCaptureAndAssertionAbility.java
@@ -0,0 +1,26 @@
+package com.acme.edu;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.fest.assertions.Assertions.*;
+
+public interface SysoutCaptureAndAssertionAbility {
+    ByteArrayOutputStream OUT = new ByteArrayOutputStream();
+
+    default void captureSysout() {
+        System.setOut(new PrintStream(OUT));
+    }
+
+    default void assertSysoutEquals(String expected) {
+        assertThat(OUT.toString()).isEqualTo(expected);
+    }
+
+    default void assertSysoutContains(String expected) {
+        assertThat(OUT.toString()).contains(expected);
+    }
+
+    default void resetOut() {
+        OUT.reset();
+    }
+}
