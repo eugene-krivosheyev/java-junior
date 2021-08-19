98ff3d97eb940c62af05f1f546146b3407bbebb6
Nikita Baranov <baranovnikita18@gmail.com>
2021-08-19 14:38:00 +0300

    Test iteration1

--- src/main/java/com/db/edu/App.java
+++ src/main/java/com/db/edu/App.java
@@ -2,6 +2,8 @@ package com.db.edu;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Logger.log(1);
+        Logger.log(0);
+        Logger.log(-1);
     }
 }
