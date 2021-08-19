53ab1bf118f5953a63e64c5f3d71b68195fb2fa7
mech-Maks <maksim_28_11_1999@mail.ru>
2021-08-19 16:32:58 +0300

    Added vars for literals.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,27 +1,32 @@
 package com.acme.edu;
 
 public class Logger {
+    private static String PRIMITIVE_POSTFIX = "primitive: ";
+    private static String CHAR_POSTFIX = "char: ";
+    private static String STRING_POSTFIX = "string: ";
+    private static String REFERENCE_POSTFIX = "reference: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_POSTFIX + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_POSTFIX + message);
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        System.out.println(CHAR_POSTFIX + message);
     }
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        System.out.println(STRING_POSTFIX + message);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_POSTFIX + message);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        System.out.println(REFERENCE_POSTFIX + message);
     }
 }
