d78e63e8352f6c10cb515b97c324b3b3d32ed4c0
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-08-19 12:08:37 +0300

    Add demo for equals() type.

--- src/main/java/demo/StringDemo.java
+++ src/main/java/demo/StringDemo.java
@@ -1,8 +1,11 @@
 package demo;
 
 import java.util.Locale;
+import java.util.Objects;
 
 public class StringDemo {
+    private int i;
+
     public static void main(String[] args) {
         String str0 = new String("abc");
         String str1 = new String("abc");
@@ -16,7 +19,25 @@ public class StringDemo {
 
         StringBuffer sb1 = new StringBuffer("1"); // vs String
         StringBuilder sb2;
-        System.out.println("1" + ? + "3" + ? + "5");
-        sb1.append("2").append(?).append("3").append(?).append("5");
+
+        Integer integer = new Integer(5); // auto boxing + intern()
+        int i = integer.intValue(); //auto unboxing
+
+        Integer integer1 = 128;
+        Integer integer2 = 128; //-----new Integer()-----
+        System.out.println( integer1 == integer2 );
+
+        str1.equals(str2);
+        Objects.equals(integer1, integer2);
+
+        if (str1.equals("abc")) {
+
+        }
+
+        if ("abc".equals(str1)) {
+
+        }
     }
+
+
 }
