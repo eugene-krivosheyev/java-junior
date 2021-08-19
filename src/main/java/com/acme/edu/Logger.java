c223685d126a4ceb00f2ab19ff42aecbec8e393a
Vsevolod Fedorov <fsdoc151@mail.ru>
2021-08-19 15:56:32 +0300

    Code refactoring.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,16 +1,22 @@
 package com.acme.edu;
 
 public class Logger {
+
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
+    private static final String CHAR_PREFIX = "char: ";
+    private static final String STRING_PREFIX = "string: ";
+    private static final String OBJECT_PREFIX = "reference: ";
+
     private static String getMessage(Object message) {
         return getMessagePrefix(message) + message + "\n";
     }
 
     private static String getMessagePrefix(Object message) {
         return switch (message.getClass().getSimpleName()) {
-            case "Integer", "Byte", "Boolean" -> "primitive: ";
-            case "String" -> "string: ";
-            case "Character" -> "char: ";
-            default -> "reference: ";
+            case "Integer", "Byte", "Boolean" -> PRIMITIVE_PREFIX;
+            case "String" -> STRING_PREFIX;
+            case "Character" -> CHAR_PREFIX;
+            default -> OBJECT_PREFIX;
         };
     }
 
