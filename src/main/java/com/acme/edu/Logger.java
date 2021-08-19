f035ce8e729928cecbfccc3f25440dc3a98342ee
Vsevolod Fedorov <fsdoc151@mail.ru>
2021-08-19 15:52:26 +0300

    Code refactoring.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,39 +1,21 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) {
-        log(Integer.valueOf(message));
+    private static String getMessage(Object message) {
+        return getMessagePrefix(message) + message + "\n";
     }
 
-    public static void log(byte message) {
-        log(Byte.valueOf(message));
-    }
-
-    public static void log(char message) {
-        log(Character.valueOf(message));
-    }
-
-    public static void log(boolean message) {
-        log(Boolean.valueOf(message));
+    private static String getMessagePrefix(Object message) {
+        return switch (message.getClass().getSimpleName()) {
+            case "Integer", "Byte", "Boolean" -> "primitive: ";
+            case "String" -> "string: ";
+            case "Character" -> "char: ";
+            default -> "reference: ";
+        };
     }
 
     public static void log(Object message) {
-
-        String prefix;
-
-        if ( message instanceof Integer ||
-             message instanceof Byte    ||
-             message instanceof Boolean     ) {
-            prefix = "primitive: ";
-        } else if ( message instanceof String ) {
-            prefix = "string: ";
-        } else if ( message instanceof Character ) {
-            prefix = "char: ";
-        } else {
-            prefix = "reference: ";
-        }
-
-        System.out.print(prefix + message + "\n");
+        System.out.print(getMessage(message));
     }
 }
 
