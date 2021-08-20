d660a056a1d2f62718f27de89a4fbbf568c837bd
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-08-20 09:31:26 +0300

    Add demo for refactorings: introduce const and extract method.

--- src/test/java/com/acme/edu/iteration01/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration01/LoggerTest.java
@@ -8,7 +8,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.*;
 
+import static java.lang.System.lineSeparator;
+
 public class LoggerTest implements SysoutCaptureAndAssertionAbility {
+    public static final String PRIMITIVE_PREFIX = "primitive:";
+
     //region given
     @BeforeEach
     public void setUpSystemOut() throws IOException {
@@ -31,8 +35,10 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
 
         //region then
-        assertSysoutContains("primitive: ");
-        assertSysoutEquals("primitive: 1\nprimitive: 0\nprimitive: -1\n");
+        assertSysoutContains(PRIMITIVE_PREFIX + " ");
+
+        String s = lineSeparator();
+        assertSysoutEquals(PRIMITIVE_PREFIX + " 1" + s + PRIMITIVE_PREFIX + " 0\nprimitive: -1\n");
         //endregion
     }
 
@@ -45,15 +51,19 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
 
         //region then
-        assertSysoutContains("primitive: ");
-        assertSysoutContains("1");
-        assertSysoutContains("0");
-        assertSysoutContains("-1");
+        assertSysoutContains(PRIMITIVE_PREFIX + " ");
+        extracted("1", "0", "-1");
         //endregion
     }
 
-    /*
-    TODO: implement Logger solution to match specification as tests
+    private void extracted(String s, String s2, String s3) {
+        assertSysoutContains(s);
+        assertSysoutContains(s2);
+        assertSysoutContains(s3);
+    }
+
+
+    //TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogChar() throws IOException {
@@ -63,9 +73,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
 
         //region then
-        assertSysoutContains("char: ");
-        assertSysoutContains("a");
-        assertSysoutContains("b");
+        extracted("char: ", "a", "b");
         //endregion
     }
 
@@ -77,9 +85,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
 
         //region then
-        assertSysoutContains("string: ");
-        assertSysoutContains("test string 1");
-        assertSysoutContains("other str");
+        extracted("string: ", "test string 1", "other str");
         //endregion
     }
 
@@ -92,9 +98,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         //endregion
 
         //region then
-        assertSysoutContains("primitive: ");
-        assertSysoutContains("true");
-        assertSysoutContains("false");
+        extracted(PRIMITIVE_PREFIX + " ", "true", "false");
         //endregion
     }
 
