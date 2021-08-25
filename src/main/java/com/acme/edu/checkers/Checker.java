5c8c35f5f66932123209ec00b6c608d17aa5ecf9
mech-Maks <maksim_28_11_1999@mail.ru>
2021-08-25 09:53:21 +0300

    added structure of abstract class

---
+++ src/main/java/com/acme/edu/checkers/Checker.java
@@ -0,0 +1,5 @@
+package com.acme.edu.checkers;
+
+public abstract class Checker {
+    public static void check(Object message) {};
+}
---
+++ src/main/java/com/acme/edu/checkers/IntSequenceChecker.java
@@ -0,0 +1,4 @@
+package com.acme.edu.checkers;
+
+public class IntSequenceChecker extends Checker {
+}
