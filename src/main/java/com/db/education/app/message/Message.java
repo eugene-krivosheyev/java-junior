b416b67f57646fdfccdf7d96c61715ab4a0cd8fd
lobakov <lobakov@gmail.com>
2021-08-29 16:19:12 +0300

    doc: fixed javadoc for Message class

--- src/main/java/com/db/education/app/message/Message.java
+++ src/main/java/com/db/education/app/message/Message.java
@@ -6,7 +6,7 @@ public interface Message {
 
     /**
      * @param message message to be accumulated with
-     * @return false for messages that shouldn't be accumulated, true otherwise
+     * @return accumulated or current message if shouldn't be accumulated
      */
     Message accumulate(Message message);
 
