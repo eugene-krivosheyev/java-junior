850e1634c02530f1d506ead019678ddd9f6a3b89
Andrew <rakovandrew812@gmail.com>
2021-08-31 08:05:34 +0300

    [iteration8] - added additional unit tests

--- src/main/java/com/db/edu/saver/ConsoleSaver.java
+++ src/main/java/com/db/edu/saver/ConsoleSaver.java
@@ -3,6 +3,7 @@ package com.db.edu.saver;
 import com.db.edu.messagepack.message.Message;
 
 public class ConsoleSaver implements Saver {
+    @Override
     public void save(Message message) {
         System.out.println(message.getDecoratedMessage());
     }
