065ad71041baa60121c85be325f4fae3e2213e7f
mech-Maks <maksim_28_11_1999@mail.ru>
2021-09-01 13:33:16 +0300

    new FileSaver with charset and Exceptions

--- src/main/java/com/acme/edu/savers/FileSaver.java
+++ src/main/java/com/acme/edu/savers/FileSaver.java
@@ -4,13 +4,22 @@ import com.acme.edu.exceptions.FileSaverException;
 import java.io.*;
 
 public class FileSaver extends AbstractSaver {
+    private String charsetCode = "Windows-1251";
+
     @Override
     public void save(String message) throws FileSaverException {
-        try (FileWriter writer = new FileWriter("results.txt", true)) {
-            writer.write(message + System.lineSeparator());
+        try {
+            File fileOutput = new File("results.txt");
+            FileOutputStream outStream = new FileOutputStream(fileOutput, true);
+
+            outStream.write(message.getBytes(charsetCode));
+            outStream.write(System.lineSeparator().getBytes(charsetCode));
+        }
+        catch (FileNotFoundException e) {
+            throw new FileSaverException("File not found", e);
         }
         catch (IOException e) {
-            throw new FileSaverException("error with file", e);
+            throw new FileSaverException("Error while writing in file", e);
         }
     }
 }
