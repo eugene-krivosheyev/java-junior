2fa01cbb49e3342a9a82913a516beb6767071488
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-09-01 11:29:14 +0300

    Add demo for simple blocking I/O.

--- src/main/java/demo/IoDemo.java
+++ src/main/java/demo/IoDemo.java
@@ -1,9 +1,6 @@
 package demo;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.nio.file.*;
 import java.util.List;
 
@@ -13,18 +10,21 @@ import static java.nio.file.StandardOpenOption.*;
 public class IoDemo {
     public static void main(String[] args) {
         //region must have
+        /*
         File fileInfo1 = new File("file.txt");
         Path fileInfo2 = Paths.get("file2.txt");
         fileInfo2.toFile();
 
-        /*
+
         try (RandomAccessFile raf = new RandomAccessFile(fileInfo1.getAbsolutePath(), "rw")) {
+            raf.writeDouble(1.0);
+            raf.writeByte(1);
             raf.readUTF();
             raf.seek(1_000);
         } catch (IOException e) {
             e.printStackTrace();
         }
-         */
+
 
 
         try {
@@ -43,10 +43,45 @@ public class IoDemo {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
         //endregion
 
-        //region Blocking I/O
+        //region (Classic) Blocking I/O API
+        // Legacy -> many libs
+        // DP: Decorator
+        // Simpler then NIO -> network app -> distributed systems design
 
+        File source = new File("data.txt");
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        new BufferedInputStream(
+                                new FileInputStream(source)),"windows-1251"))) {
+
+            String readLine;
+            while((readLine = in.readLine()) != null) {
+                System.out.println(readLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        /*
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            String readLine;
+            while((readLine = reader.readLine()) != null) {
+                System.out.println(readLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+        //endregion
+
+        //region NIO2
+        // + Async/non-blocking
+        // + Native
         //endregion
     }
 }
