10dbde7db757b74179a7985b4f461d8c9ba06f1f
Alexandr Zakaluzhskii <zakaluga@gmail.com>
2021-10-14 13:58:24 +0300

    fixed functions order

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,10 +1,26 @@
 package com.acme.edu;
 
 public class Logger {
+    public static void log(int message) {
+        print(int.class.getTypeName(),String.valueOf(message));
+    }
 
+    public static void log(char message) {
+        print(char.class.getTypeName(),String.valueOf(message));
+    }
 
-    private static void print(String type,String message) {
-        System.out.println(prefix(type) + message);
+    public static void log(byte message) {
+        print(byte.class.getTypeName(),String.valueOf(message));
+    }
+
+    public static void log(String message) {
+        print(String.class.getTypeName(),String.valueOf(message));
+    }
+    public static void log(Boolean message) {
+        print(Boolean.class.getTypeName(),String.valueOf(message));
+    }
+    public static void log(Object message) {
+        print(Object.class.getTypeName(),String.valueOf(message));
     }
 
     private static String prefix(String type) {
@@ -19,24 +35,7 @@ public class Logger {
         }
     }
 
-    public static void log(int message) {
-        print(int.class.getTypeName(),String.valueOf(message));
-    }
-
-    public static void log(char message) {
-        print(char.class.getTypeName(),String.valueOf(message));
-    }
-
-    public static void log(byte message) {
-        print(byte.class.getTypeName(),String.valueOf(message));
-    }
-    public static void log(String message) {
-        print(String.class.getTypeName(),String.valueOf(message));
-    }
-    public static void log(Boolean message) {
-        print(Boolean.class.getTypeName(),String.valueOf(message));
-    }
-    public static void log(Object message) {
-        print(Object.class.getTypeName(),String.valueOf(message));
+    private static void print(String type,String message) {
+        System.out.println(prefix(type) + message);
     }
 }
