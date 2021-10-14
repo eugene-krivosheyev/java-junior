a84026cc0c3fef2a0f57e37aaa6852128a4ca31f
ignat-mishura <ignat.mishura@gmail.com>
2021-10-14 14:08:36 +0300

    Logger Refactoring

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -5,30 +5,30 @@ public class Logger {
     public static final String CHAR_PREFIX = "char: ";
     public static final String STRING_PREFIX = "string: ";
     public static final String OBJECT_PREFIX = "reference: ";
+    public static final OutputMethod OUTPUT_METHOD = OutputMethod.TERMINAL;
 
-    public static void log(int message) {
-        output('c',PRIMITIVE_PREFIX, message);
-    }
+    public static void log(int message) { output(OUTPUT_METHOD,PRIMITIVE_PREFIX, message); }
 
-    public static void log(byte message) {
-        output('c',PRIMITIVE_PREFIX, message);
-    }
+    public static void log(byte message) { output(OUTPUT_METHOD,PRIMITIVE_PREFIX, message); }
 
-    public static void log(char message) { output('c', CHAR_PREFIX, message); }
+    public static void log(char message) { output(OUTPUT_METHOD, CHAR_PREFIX, message); }
 
-    public static void log(String message) { output('c',STRING_PREFIX, message); }
+    public static void log(String message) { output(OUTPUT_METHOD,STRING_PREFIX, message); }
 
-    public static void log(boolean message) { output('c',PRIMITIVE_PREFIX, message); }
+    public static void log(boolean message) { output(OUTPUT_METHOD,PRIMITIVE_PREFIX, message); }
 
+    public static void log(Object message) { output(OUTPUT_METHOD,OBJECT_PREFIX, message); }
 
-    public static void log(Object message) { output('c',OBJECT_PREFIX, message); }
-
-    private static void output(char output_method, String prefix, Object message){
-        if (output_method == 'c'){
+    private static void output(OutputMethod outputMethod, String prefix, Object message) {
+        if (outputMethod == OutputMethod.TERMINAL) {
             System.out.println(prefix + message);
         }
-        else if(output_method == 'f'){
+        else if(outputMethod == OutputMethod.FILE) {
             //write to file
         }
+    }
+
+    enum OutputMethod {
+        TERMINAL, FILE
     }
 }
