489bdc940e7ec838685e1650a9d632b0af42ee15
y_xab <y_xab@mail.ru>
2021-10-14 12:12:48 +0300

    add tests

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -8,4 +8,20 @@ public class Logger {
     public static void log(byte message) {
         System.out.println("primitive: " + message);
     }
+
+    public static void log(char message) {
+        System.out.println("char: " + message);
+    }
+
+    public static void log(String message) {
+        System.out.println("string: " + message);
+    }
+
+    public static void log(boolean message) {
+        System.out.println("primitive: " + message);
+    }
+
+    public static void log(Object message) {
+        System.out.println("reference: " + message);
+    }
 }
