1b0a9da141e5bf7bc95765f5fb2bf8eb81c54a4f
Alexandr Zakaluzhskii <zakaluga@gmail.com>
2021-10-14 12:56:30 +0300

    fix for Logger

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,23 +1,42 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
+
+
+    private static void print(String type,String message) {
+        System.out.println(prefix(type) + message);
     }
+
+    private static String prefix(String type) {
+        if (String.class.getTypeName() == type){
+            return "string: ";
+        } else if (char.class.getTypeName() == type){
+            return "char: ";
+        } else if (Object.class.getTypeName() == type){
+            return "reference: ";
+        } else {
+            return "primitive: ";
+        }
+    }
+
+    public static void log(int message) {
+        print(int.class.getTypeName(),String.valueOf(message));
+    }
+
     public static void log(char message) {
-        System.out.println("char: " + message);
+        print(char.class.getTypeName(),String.valueOf(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        print(byte.class.getTypeName(),String.valueOf(message));
     }
     public static void log(String message) {
-        System.out.println("string: " + message);
+        print(String.class.getTypeName(),String.valueOf(message));
     }
     public static void log(Boolean message) {
-        System.out.println("primitive: " + message);
+        print(Boolean.class.getTypeName(),String.valueOf(message));
     }
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        print(Object.class.getTypeName(),String.valueOf(message));
     }
 }
