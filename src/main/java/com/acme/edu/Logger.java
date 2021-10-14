56c9b5239ac7d8edb167b0bab63acb7b77ab5372
Alexandr Zakaluzhskii <zakaluga@gmail.com>
2021-10-14 14:07:37 +0300

    fixed style

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,40 +2,42 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-        print(int.class.getTypeName(),String.valueOf(message));
+        print(int.class.getTypeName(), String.valueOf(message));
     }
 
     public static void log(char message) {
-        print(char.class.getTypeName(),String.valueOf(message));
+        print(char.class.getTypeName(), String.valueOf(message));
     }
 
     public static void log(byte message) {
-        print(byte.class.getTypeName(),String.valueOf(message));
+        print(byte.class.getTypeName(), String.valueOf(message));
     }
 
     public static void log(String message) {
-        print(String.class.getTypeName(),String.valueOf(message));
+        print(String.class.getTypeName(), String.valueOf(message));
     }
+
     public static void log(Boolean message) {
-        print(Boolean.class.getTypeName(),String.valueOf(message));
+        print(Boolean.class.getTypeName(), String.valueOf(message));
     }
+
     public static void log(Object message) {
-        print(Object.class.getTypeName(),String.valueOf(message));
+        print(Object.class.getTypeName(), String.valueOf(message));
     }
 
     private static String prefix(String type) {
-        if (String.class.getTypeName() == type){
+        if (String.class.getTypeName() == type) {
             return "string: ";
-        } else if (char.class.getTypeName() == type){
+        } else if (char.class.getTypeName() == type) {
             return "char: ";
-        } else if (Object.class.getTypeName() == type){
+        } else if (Object.class.getTypeName() == type) {
             return "reference: ";
         } else {
             return "primitive: ";
         }
     }
 
-    private static void print(String type,String message) {
+    private static void print(String type, String message) {
         System.out.println(prefix(type) + message);
     }
 }
