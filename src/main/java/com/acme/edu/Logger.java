60286163ce1eb7903dc6f569b18fe666d28c3ded
aleksandra <adlovlac@gmail.com>
2021-10-14 12:43:53 +0300

    Moved printing to console to separate method

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -2,26 +2,30 @@ package com.acme.edu;
 
 public class Logger {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        printToConsole("primitive: " + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printToConsole("primitive: " + message);
     }
 
     public static void log(char message) {
-        System.out.println("char: " + message);
+        printToConsole("char: " + message);
     }
 
     public static void log(String message) {
-        System.out.println("string: " + message);
+        printToConsole("string: " + message);
     }
 
     public static void log(boolean message) {
-        System.out.println("boolean: " + message);
+        printToConsole("boolean: " + message);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message.toString());
+        printToConsole("reference: " + message.toString());
+    }
+
+    private static void printToConsole(String message) {
+        System.out.println(message);
     }
 }
