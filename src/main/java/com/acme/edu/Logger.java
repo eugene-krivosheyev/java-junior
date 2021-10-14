0cf77c4aad98a72fdc15452d0e541304fae021e4
Che1012 <t-minibaev@mail.ru>
2021-10-14 13:04:48 +0300

    Refactor print

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -7,32 +7,32 @@ public class Logger {
     private static final String stringPrefix = "string: ";
     private static final String referencePrefix = "reference: ";
 
-    private static void print(String prefix, String message) {
-        System.out.println(prefix + message);
+    private static void print(String message) {
+        System.out.println(message);
     }
 
     public static void log(int message) {
-        print(primitivePrefix, String.valueOf(message));
+        print(primitivePrefix + message);
     }
 
     public static void log(byte message) {
-        print(primitivePrefix, String.valueOf(message));
+        print(primitivePrefix + message);
     }
 
     public static void log(char message) {
-        print(charPrefix, String.valueOf(message));
+        print(charPrefix + message);
     }
 
     public static void log(String message) {
-        print(stringPrefix, message);
+        print(stringPrefix + message);
     }
 
     public static void log(boolean message) {
-        print(primitivePrefix, String.valueOf(message));
+        print(primitivePrefix + message);
     }
 
     public static void log(Object message) {
-        print(referencePrefix, message.toString());
+        print(referencePrefix + message);
     }
 
 }
