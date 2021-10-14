9158f88458eba127813da487a09b0a8563af6e6c
verdoga55 <spaceivanoff@yandex.ru>
2021-10-14 12:01:00 +0300

    [Logger-test] Char_log

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,15 @@
 package com.acme.edu;
 
+import java.math.BigDecimal;
+
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
-    }
+
+    public static void log(int message) { System.out.println("primitive: " + message); }
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
     }
+
+    public static void log(char message) { System.out.println("char: " + message); }
+
 }
