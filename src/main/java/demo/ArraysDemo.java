099b472c36258298254d2b75a500767d18af9246
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-10-15 16:16:10 +0300

    Add demo for varargs.

--- src/main/java/demo/ArraysDemo.java
+++ src/main/java/demo/ArraysDemo.java
@@ -1,7 +1,7 @@
 package demo;
 
 public class ArraysDemo {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         int[] array = new int[10]; //Collections API
         System.out.println(array[0]);
         array[0] = 1;
@@ -21,11 +21,23 @@ public class ArraysDemo {
         m(hardcoded);
 
         m(new int[] {1,3});
+
+        mWithVararg();
+        mWithVararg(new int[] {1,3});
+        mWithVararg(1,2);
+        mWithVararg(1,2,3,4,5,6,7,8);
     }
 
     private static void m(int[] arg) {
         for (int current : arg) {
             System.out.println(current);
+        }
+    }
+
+    private static void mWithVararg(int... arg) {
+        int first = arg[0];
+        for (int current : arg) {
+            System.out.println(arg);
         }
     }
 }
