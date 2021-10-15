0b2f450df02cd5c0f6062bbc64ab5d644760f722
Alexandr Zakaluzhskii <zakaluga@gmail.com>
2021-10-15 14:14:08 +0300

    fix style for new functionality

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -12,9 +12,6 @@ public class Logger {
 
     public static void setSumming(boolean isSum){isSumming = isSum;}
 
-    public static void separate(){
-    }
-
     public static void log(int message) {
         prepare(int.class.getTypeName(), String.valueOf(message));
     }
@@ -37,6 +34,16 @@ public class Logger {
 
     public static void log(Object message) {
         prepare(Object.class.getTypeName(), String.valueOf(message));
+    }
+
+    public static void endLog(){
+        if(String.class.getTypeName() == collectingType){
+            if(stringCounter != 0){
+                print(stringMsg(bufferString,stringCounter));
+            }
+        } else if (int.class.getTypeName() == collectingType){
+            print(bufferInteger.toString());
+        }
     }
 
     private static String prefix(String type) {
@@ -62,63 +69,81 @@ public class Logger {
         return message;
     }
 
-    public static void endLog(){
-        if(String.class.getTypeName() == collectingType){
-            if(stringCounter != 0){
-                print(stringMsg(bufferString,stringCounter));
-            }
-        } else if (int.class.getTypeName() == collectingType){
-            print(bufferInteger.toString());
-        }
-    }
-
     private static void prepare(String classType, String message) {
         if (!isSumming) {
             print(formatMsg(classType, message));
         } else {
-            if (Objects.equals(classType,collectingType)){
-                if(String.class.getTypeName() == classType){
-                    if(Objects.equals(message,bufferString)){
-                        stringCounter++;
-                    } else {
-                        print(stringMsg(bufferString,stringCounter));
-                        stringCounter = 1;
-                        bufferString = message;
-                    }
-                } else if (int.class.getTypeName() == classType){
-                    if ((Integer.MAX_VALUE - bufferInteger < Integer.valueOf(message).intValue())
-                    ||  (Integer.MIN_VALUE - bufferInteger > Integer.valueOf(message).intValue())){
-                        print(bufferInteger.toString());
-                        bufferInteger = 0;
-                    }
-                    bufferInteger = (bufferInteger + Integer.valueOf(message).intValue());
-                } else {
-                    print(message);
-                }
-            } else {
-                if(String.class.getTypeName() == collectingType){
-                    if(stringCounter != 0){
-                        print(stringMsg(bufferString,stringCounter));
-                        stringCounter = 0;
-                    }
-                } else if (int.class.getTypeName() == collectingType){
-                    print(bufferInteger.toString());
-                    bufferInteger = 0;
-                } else {
-                    bufferString = "";
-                    stringCounter = 0;
-                    bufferInteger = 0;
-                }
-                collectingType = classType;
-                if(String.class.getTypeName() == classType){
-                    bufferString = message;
-                    stringCounter = 1;
-                } else if (int.class.getTypeName() == classType){
-                    bufferInteger = Integer.valueOf(message);
-                } else {
-                    print(message);
-                }
+            workWithSumming(classType, message);
+        }
+    }
+
+    private static void workWithSumming(String classType, String message) {
+        if (Objects.equals(classType,collectingType)){
+            workWithSameType(classType, message);
+        } else {
+            workWithAnotherType(classType, message);
+        }
+    }
+
+    private static void workWithAnotherType(String classType, String message) {
+        endWorkWithPreviosType();
+        startWorkWithNewType(classType, message);
+    }
+
+    private static void startWorkWithNewType(String classType, String message) {
+        collectingType = classType;
+        if(String.class.getTypeName() == classType){
+            bufferString = message;
+            stringCounter = 1;
+        } else if (int.class.getTypeName() == classType){
+            bufferInteger = Integer.valueOf(message);
+        } else {
+            print(message);
+        }
+    }
+
+    private static void endWorkWithPreviosType() {
+        if(String.class.getTypeName() == collectingType){
+            if(stringCounter != 0){
+                print(stringMsg(bufferString,stringCounter));
+                stringCounter = 0;
             }
+        } else if (int.class.getTypeName() == collectingType){
+            print(bufferInteger.toString());
+            bufferInteger = 0;
+        } else {
+            bufferString = "";
+            stringCounter = 0;
+            bufferInteger = 0;
+        }
+    }
+
+    private static void workWithSameType(String classType, String message) {
+        if(String.class.getTypeName() == classType){
+            workWithString(message);
+        } else if (int.class.getTypeName() == classType){
+            workWithInteger(message);
+        } else {
+            print(message);
+        }
+    }
+
+    private static void workWithInteger(String message) {
+        if ((Integer.MAX_VALUE - bufferInteger < Integer.valueOf(message).intValue())
+        ||  (Integer.MIN_VALUE - bufferInteger > Integer.valueOf(message).intValue())){
+            print(bufferInteger.toString());
+            bufferInteger = 0;
+        }
+        bufferInteger = (bufferInteger + Integer.valueOf(message).intValue());
+    }
+
+    private static void workWithString(String message) {
+        if(Objects.equals(message,bufferString)){
+            stringCounter++;
+        } else {
+            print(stringMsg(bufferString,stringCounter));
+            stringCounter = 1;
+            bufferString = message;
         }
     }
 }
