eb7d540468e60c0ce2daff9355ee2c4daf1f7ddb
darik333 <darik93@yandex.ru>
2021-10-18 12:03:43 +0300

    New fun processing

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,15 +1,14 @@
 package com.acme.edu;
 
-import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class Logger {
 
-    private static int state;
-    private static String state_string = null;
-    private static int counter = 1;
-    private static String previous_type = "start";
+    private static int bufferSum;
+    private static String bufferString = null;
+    private static int stringCounter = 1;
+    private static String previousType = "start";
     private static String messagePrefix = "";
 
     public static void log(String... args) {
@@ -21,12 +20,29 @@ public class Logger {
 
     public static void log(int... args) {
         messagePrefix = "";
-        print(formatMessage(messagePrefix, sumOfArray(args)));
+        print(formatMessage(sumOfArray(args)));
     }
 
     public static void log(int[][] arr) {
         messagePrefix = "";
-        print(formatMessage(messagePrefix, sumOfArray2D(arr)));
+        print(formatMessage(sumOfArray2D(arr)));
+    }
+
+    private static void processingForIntAndByte(String type, int message) {
+        messagePrefix = "primitive: ";
+        int maxValue = (type == "int") ? Integer.MAX_VALUE : Byte.MAX_VALUE;
+        if (type != previousType) {
+            flush();
+            previousType = type;
+            bufferSum += message;
+        } else {
+            if ((Long.valueOf(bufferSum) + Long.valueOf(message)) < maxValue) {
+                bufferSum += message;
+            } else {
+                System.out.println(maxValue);
+                bufferSum = message - (maxValue - bufferSum);
+            }
+        }
     }
 
     public static void log(int message) {
@@ -35,82 +51,58 @@ public class Logger {
          * @param message
          */
         final String type = "int";
-        messagePrefix = "primitive: ";
-        if (type != previous_type) {
-            flush();
-            previous_type = type;
-            state += message;
-        } else {
-            if ((Long.valueOf(state) + Long.valueOf(message)) < Integer.MAX_VALUE) {
-                state += message;
-            } else {
-                System.out.println(Integer.MAX_VALUE);
-                state = message - (Integer.MAX_VALUE - state);
-            }
-        }
+        processingForIntAndByte(type, message);
     }
 
     public static void log(byte message) {
         final String type = "byte";
-        messagePrefix = "primitive: ";
-        if (type != previous_type) {
-            flush();
-            previous_type = type;
-            state += message;
-        } else {
-            if ((Integer.valueOf(state) + Integer.valueOf(message)) < Byte.MAX_VALUE) {
-                state += message;
-            } else {
-                System.out.println(Byte.MAX_VALUE);
-                state = message - (Byte.MAX_VALUE - state);
-            }
-        }
+        processingForIntAndByte(type, message);
     }
 
     public static void log(char message) {
         messagePrefix = "char: ";
-        print(formatMessage(messagePrefix, message));
+        print(formatMessage(message));
     }
 
     public static void log(String message) {
         final String type = "str";
-        if (type != previous_type) {
+        if (type != previousType) {
             flush();
         } else {
-            if (Objects.equals(message, state_string)) {
-                counter++;
+            if (Objects.equals(message, bufferString)) {
+                stringCounter++;
             } else {
                 flush();
-                counter = 1;
+                stringCounter = 1;
             }
         }
         messagePrefix = "string: ";
-        previous_type = type;
-        state = 0;
-        state_string = message;
+        previousType = type;
+        bufferSum = 0;
+        bufferString = message;
     }
 
     public static void log(boolean message) {
         messagePrefix = "primitive: ";
-        print(formatMessage(messagePrefix, message));
+        print(formatMessage(message));
     }
 
     public static void log(Object message) {
         messagePrefix = "reference: ";
-        print(formatMessage(messagePrefix, message));
+        print(formatMessage(message));
     }
 
     public static void flush() {
-        if (previous_type != "start" && (previous_type == "int" || previous_type == "byte")) {
-            print(formatMessage(messagePrefix, state));
-        } else if (previous_type != "start" && previous_type == "str") {
-            if (counter == 1) print(formatMessage(messagePrefix, state_string));
+        if (previousType != "start" && (previousType == "int" || previousType == "byte")) {
+            print(formatMessage(bufferSum));
+        } else if (previousType != "start" && previousType == "str") {
+            if (stringCounter == 1) print(formatMessage(bufferString));
             else {
-                System.out.println(state_string + " (x" + counter + ")");
+                System.out.println(bufferString + " (x" + stringCounter + ")");
             }
         }
-        state = 0;
-        counter = 1;
+        bufferSum = 0;
+        stringCounter = 1;
     }
 
     public static void mass(int[] a) {
@@ -119,7 +111,7 @@ public class Logger {
         }
     }
 
-    private static String formatMessage(String messagePrefix, Object message) {
+    private static String formatMessage(Object message) {
         return messagePrefix + message;
     }
 
