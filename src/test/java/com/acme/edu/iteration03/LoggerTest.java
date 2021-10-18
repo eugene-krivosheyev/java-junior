77bd3ec4401f55c451e95949f19465cd3a0ec333
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-10-18 10:56:19 +0300

    Fix requirements for iteration 03.

--- src/test/java/com/acme/edu/iteration03/LoggerTest.java
+++ src/test/java/com/acme/edu/iteration03/LoggerTest.java
@@ -46,11 +46,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
 
         //region then
         assertSysoutEquals(
-            "primitives matrix: {\n" +
-                "{-1, 0, 1}\n" +
-                "{1, 2, 3}\n" +
-                "{-1, -2, -3}\n" +
-            "}\n"
+            "primitives matrix: 0\n"
         );
         //endregion
     }
