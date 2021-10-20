a44512ff14aaa870ac576f0b396590e4bf1edd8a
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-10-20 12:27:23 +0300

    Add demo for annotations.

--- src/main/java/demo/ooad/FileSaver.java
+++ src/main/java/demo/ooad/FileSaver.java
@@ -1,21 +1,24 @@
 package demo.ooad;
 
+import java.util.Date;
+
 public class FileSaver extends ValidatingSaver {
     private final String FILE_NAME;
 
+    @SuppressWarnings("")
     public FileSaver(String fileName) {
         FILE_NAME = fileName;
     }
 
-    //overriding
+    @Override
     public Long save(String message) {
         super.save(message);
         System.out.println("write " + message + " to file " + FILE_NAME);
         return 0L;
     }
 
-    @Override
+    @Deprecated
     public void close() {
-
+        this.save("");
     }
 }
--- src/main/java/demo/ooad/Logger.java
+++ src/main/java/demo/ooad/Logger.java
@@ -16,6 +16,7 @@ public class Logger {
      * 1 MSLoC
      * @param DTO | Command
      */
+    @Deprecated(since = "5.12.9", forRemoval = true)
     public void log(Message message) {
         if (!filter.filter(message)) { // [GoF]: Visitor
             Number number = saver.save(message.getBody());
