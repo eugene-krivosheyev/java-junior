b096b188af25ace29301074409dcff844b98c0c7
aleksandra <adlovlac@gmail.com>
2021-10-21 16:17:43 +0300

    Removed redundant methods

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -46,20 +46,7 @@ public class Logger {
     }
     public static void log(String... array) {
         for (String value: array) {
-            log(value);
+            controller.log(new StringMessage(value));
         }
     }
-
-    private static int getSumOfArray(int... array) {
-        int sum = 0;
-        for (int value: array) {
-            sum += value;
-        }
-        return sum;
-    }
-
-    private static void printToOutput(String message) {
-        out.println(message);
-    }
-
 }
