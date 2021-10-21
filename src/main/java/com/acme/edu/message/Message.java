ddcf764a58c37923757146be6e7a41bfc28197df
Alexandr Zakaluzhskii <zakaluga@gmail.com>
2021-10-21 16:00:36 +0300

    fixed

--- src/main/java/com/acme/edu/message/Message.java
+++ src/main/java/com/acme/edu/message/Message.java
@@ -6,34 +6,33 @@ public abstract class Message {
 
     protected static final String SEP = lineSeparator();
 
-    protected Object  data;
-    protected boolean isSumming = false;
-    protected boolean isEndLogging = false;
+    protected Object data;
+    private boolean isSumming = false;
+    private boolean isEndLogging = false;
     public Object getData() {
         return data;
-    }
-    public boolean isSum(){return isSumming;}
-
-    public abstract void setMsg(Object message);
-
-    public abstract void setMsg(Object message, boolean isSum);
-    public abstract Message add(Message message);
-
-    public abstract boolean isSameType(Message message);
-
-    public boolean isSame(Message message){
-        return this.isSameType(message) && (this.data.equals(message.data));
     }
 
     public boolean isEmpty(){
         return this instanceof EmptyMessage;
     }
+    public boolean isEndLogging() {
+        return isEndLogging;
+    }
+    public boolean isSum(){return isSumming;}
+    public boolean isSame(Message message){
+        return this.isSameType(message) && (this.data.equals(message.data));
+    }
+    public abstract boolean isSameType(Message message);
+
+    public abstract void setMsg(Object message);
+
+    public abstract void setMsg(Object message, boolean isSum);
+
+    public abstract Message add(Message message);
 
     public void setEndLogging(boolean isEnd) {
         isEndLogging = isEnd;
-    }
-    public boolean isEndLogging() {
-        return isEndLogging;
     }
 
     public abstract String toString();
