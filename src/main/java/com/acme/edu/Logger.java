ef0f16b363d9dead59a79169d68c1906e44aa914
Егор <sainstrow.pro26@gmail.com>
2021-10-25 16:51:22 +0300

    Changed back to static

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -6,53 +6,53 @@ import com.acme.edu.model.MessageContainer;
 
 
 public class Logger {
-    private MessageContainer container;
+    private static MessageContainer container;
 
     public Logger() {
         container = new MessageContainer();
     }
 
-    public void log(int message) {
+    public static void log(int message) {
         container.addMessage(new IntMessage(message));
     }
 
-    public void log(byte message) {
+    public static void log(byte message) {
         container.addMessage(new ByteMessage(message));
     }
 
-    public void log(char message) {
+    public static void log(char message) {
         container.addMessage(new CharMessage(message));
     }
 
 
-    public void log(String message) {
+    public static void log(String message) {
         container.addMessage(new StringMessage(message));
     }
 
-    public void log(boolean message) {
+    public static void log(boolean message) {
         container.addMessage(new BooleanMessage(message));
     }
 
-    public void log(Object message) {
+    public static void log(Object message) {
         container.addMessage(new ObjectMessage(String.valueOf(message)));
     }
 
-    public void log(int... messages) {
+    public static void log(int... messages) {
             container.addMessage(new ArrayMessage(messages));
     }
 
-    public void log(int[][] messages) {
+    public static void log(int[][] messages) {
         int[] formattedMessage = new int[messages.length * messages[0].length];
         container.addMessage(new MatrixMessage(formattedMessage));
     }
 
-    public void log(String... messages) {
+    public static void log(String... messages) {
         for (String message : messages) {
             container.addMessage(new StringMessageWithConcatenation(message));
         }
     }
 
-    public void flush() {
+    public static void flush() {
         System.out.print(container.getText());
         container = new MessageContainer();
     }
