2468de4f3ae32378b0aa7f454fd17493d376b280
DoolyEvgesha <oparina.dk@phystech.edu>
2021-10-25 17:32:33 +0300

    add test for Controller

--- src/test/java/com/acme/edu/integration/ControllerTest.java
+++ src/test/java/com/acme/edu/integration/ControllerTest.java
@@ -5,30 +5,46 @@ import com.acme.edu.Flusher;
 import com.acme.edu.StatesDTO;
 import com.acme.edu.TypeCodeEnum;
 import com.acme.edu.message.StringMessage;
-import com.acme.edu.saver.Saver;
 import com.acme.edu.saver.SystemOutSaver;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
 public class ControllerTest {
+    private final SystemOutSaver saverStub = mock(SystemOutSaver.class);
+    private final Controller controller = new Controller(new StatesDTO(new Flusher(saverStub)), new Flusher(saverStub));
 
     @Test
     public void shouldFlushWhenLogTwoStringMessage() {
         StringMessage firstStringMessage = mock(StringMessage.class);
         StringMessage secondStringMessage = mock(StringMessage.class);
-        SystemOutSaver saverStub = mock(SystemOutSaver.class);
 
         when(firstStringMessage.getMessage()).thenReturn("Hello world first");
         when(secondStringMessage.getMessage()).thenReturn("Hello world again");
         when(firstStringMessage.getCode()).thenReturn(TypeCodeEnum.STRING);
         when(secondStringMessage.getCode()).thenReturn(TypeCodeEnum.STRING);
 
-        final Controller controller = new Controller(new StatesDTO(new Flusher(saverStub)), new Flusher(saverStub));
         controller.log(firstStringMessage);
         controller.log(secondStringMessage);
 
         verify(saverStub).save("string: Hello world first");
     }
 
+    @Test
+    @Ignore
+    public void shouldSetByteSumWhenLogByte() {
+        StringMessage firstStringMessage = mock(StringMessage.class);
+        StringMessage secondStringMessage = mock(StringMessage.class);
+
+        when(firstStringMessage.getMessage()).thenReturn("Hello world first");
+        when(secondStringMessage.getMessage()).thenReturn("Hello world again");
+        when(firstStringMessage.getCode()).thenReturn(TypeCodeEnum.STRING);
+        when(secondStringMessage.getCode()).thenReturn(TypeCodeEnum.STRING);
+
+        controller.log(firstStringMessage);
+        controller.log(secondStringMessage);
+
+        verify(saverStub).save("string: Hello world first");
+    }
 }
