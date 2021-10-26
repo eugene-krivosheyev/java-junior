f83dc194f3c5afb0a6d89461b4b2f787bced3561
aleksandra <adlovlac@gmail.com>
2021-10-26 17:59:54 +0300

    Added test for stringMessage

---
+++ src/test/java/com/acme/edu/messages/StringMessageTest.java
@@ -0,0 +1,39 @@
+package com.acme.edu.messages;
+
+import com.acme.edu.common.Message;
+import com.acme.edu.common.Printer;
+import com.acme.edu.messages.IntMessage;
+import com.acme.edu.messages.StringMessage;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class StringMessageTest {
+    @Test
+    public void shouldThrowExceptionWhenAccumulateWithAnotherType() {
+        // Given
+        StringMessage firstMessage = new StringMessage("default str");
+        IntMessage anotherTypeMessage = mock(IntMessage.class);
+
+        // Then
+        assertThrows(IllegalArgumentException.class,
+                () -> firstMessage.accumulate(anotherTypeMessage));
+    }
+    @Test
+    public void shouldReturnPreviousMessageWhenAccumulateDifferentValue() {
+        // Given
+        StringMessage firstMessage = new StringMessage("default str");
+        StringMessage secondMessage = new StringMessage("another str");
+
+        // When
+        Message previousMessage = firstMessage.accumulate(secondMessage);
+
+        // Then
+        StringMessage expectedMessage = new StringMessage("default str");
+        assertEquals(expectedMessage, previousMessage);
+    }
+}
